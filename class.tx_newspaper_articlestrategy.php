6875102ad6a6111f07446a98aac1162b73f0bd6f
Lene Preuss <lene.preuss@gmail.com>
2008-11-19 18:00:42 +0000

    Added doxygen documentation

--- class.tx_newspaper_articlestrategy.php
+++ class.tx_newspaper_articlestrategy.php
@@ -6,59 +6,92 @@
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
 
-class tx_newspaper_ArticleStrategy {
+/// Strategy class to factor out code common to more or less all Article implementations
+/** I made this class implement the Article interface. I'm not sure if this is a
+ *  bright idea, but I think that as every function in an Article implementation
+ *  just calls the corresponding function in the Strategy class, this class must
+ *  implement every function in the interface, right? So I just as well might 
+ *  make the Strategy class conform to the interface. As an added bonus, I get
+ *  doxygen documentation for every function for free.
+ * 
+ *  The catch here is that
+ *  -# The ArticleStrategy IS NOT A Article
+ *  -# Maybe there are methods which are not delegated to the Strategy.
+ * 
+ *  I'll just see how this approach works out. Maybe I'll change it later.
+ */ 
+class tx_newspaper_ArticleStrategy implements tx_newspaper_Article {
+	
+	/** \param $parent The tx_newspaper_Article object using this Strategy
+	 */ 
 	public function __construct(tx_newspaper_Article $parent) {
 		$this->parent = $parent;
 	}
 	
 	public function render($template) {
-		throw new NotYetImplementedException("ArticleImpl::render()");
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::render()");
 	}
 	public function importieren() {
-		throw new NotYetImplementedException("ArticleImpl::importieren()");
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::importieren()");
 	}
 	public function exportieren() {
-		throw new NotYetImplementedException("ArticleImpl::exportieren()");
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::exportieren()");
 	}
 	public function laden() {
-		throw new NotYetImplementedException("ArticleImpl::laden()");
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::laden()");
 	}
 	public function speichern() {
-		throw new NotYetImplementedException("ArticleImpl::speichern()");
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::speichern()");
 	}
 	public function vergleichen() {
-		throw new NotYetImplementedException("ArticleImpl::vergleichen()");
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::vergleichen()");
 	}
 	public function extraAnlegen() {
-		throw new NotYetImplementedException("ArticleImpl::extraAnlegen()");
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::extraAnlegen()");
 	}
 
 	/// returns an actual member (-> Extra)
 	function getAttribute($attribute) {
-		return $this->attributes[$attribute];
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
 	}
 
 	/// sets a member (-> Extra)
 	function setAttribute($attribute, $value) {
-		$this->attributes[$attribute] = $value;
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
 	}
 
 	/// Get the list of Extra s associated with this Article
-	function getExtras() { return $this->extras; }
-
-	function addExtra() {
-		throw new NotYetImplementedException("ArticleImpl::addExtra()");
+	function getExtras() {
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
 	}
 
-	function getSource() { return $this->source; }
+	function addExtra() {
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
+	}
 
-	function setSource(tx_newspaper_Source $source) {$this->source = $source; }
+	function getSource() {
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
+	}
 
-	function getUid() { return $this->uid; }
-	function setUid($uid) { $this->uid = $uid; }
+	function setSource(tx_newspaper_Source $source) {
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
+	}
+
+	function getUid() {
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
+	}
+	function setUid($uid) {
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
+	}
 	
-	static function getAttributeList() { return self::$attribute_list; }
+	static function getAttributeList() {
+		throw new NotYetImplementedException("tx_newspaper_ArticleStrategy::addExtra()");
+	}
 
+	/** Usage:
+	 *  \code return tx_newspaper_ArticleStrategy::mapFieldToSourceField(
+	 *		$fieldname, $source, self::$mapFieldsToSourceFields); \endcode
+	 */
 	static function mapFieldToSourceField($fieldname, 
 										  tx_newspaper_Source $source, 
 										  array $mapFieldsToSourceFields) {
@@ -71,6 +104,10 @@ class tx_newspaper_ArticleStrategy {
 		return $mapping[$fieldname];
 	}
 	
+	/** Usage:
+	 * \code return tx_newspaper_ArticleStrategy::sourceTable(
+	 * 		$source, self::$table); \endcode
+	 */
 	static function sourceTable(tx_newspaper_Source $source, array $sourceTable) {
 		$table = $sourceTable[get_class($source)];
 
