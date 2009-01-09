e8cc5cfae501a97c77869898f03e15d4a84e36e9
Lene Preuss <lene.preuss@gmail.com>
2009-01-09 16:49:05 +0000

    edited with kickstarter, manually restored include line

--- tca.php
+++ tca.php
@@ -106,5 +106,86 @@ $TCA["tx_newspaper_extra_image"] = array (
 	)
 );
 
+
+
+$TCA["tx_newspaper_department"] = array (
+	"ctrl" => $TCA["tx_newspaper_department"]["ctrl"],
+	"interface" => array (
+		"showRecordFieldList" => "sys_language_uid,l18n_parent,l18n_diffsource,hidden,fe_group,department_name"
+	),
+	"feInterface" => $TCA["tx_newspaper_department"]["feInterface"],
+	"columns" => array (
+		'sys_language_uid' => array (		
+			'exclude' => 1,
+			'label'  => 'LLL:EXT:lang/locallang_general.xml:LGL.language',
+			'config' => array (
+				'type'                => 'select',
+				'foreign_table'       => 'sys_language',
+				'foreign_table_where' => 'ORDER BY sys_language.title',
+				'items' => array(
+					array('LLL:EXT:lang/locallang_general.xml:LGL.allLanguages', -1),
+					array('LLL:EXT:lang/locallang_general.xml:LGL.default_value', 0)
+				)
+			)
+		),
+		'l18n_parent' => array (		
+			'displayCond' => 'FIELD:sys_language_uid:>:0',
+			'exclude'     => 1,
+			'label'       => 'LLL:EXT:lang/locallang_general.xml:LGL.l18n_parent',
+			'config'      => array (
+				'type'  => 'select',
+				'items' => array (
+					array('', 0),
+				),
+				'foreign_table'       => 'tx_newspaper_department',
+				'foreign_table_where' => 'AND tx_newspaper_department.pid=###CURRENT_PID### AND tx_newspaper_department.sys_language_uid IN (-1,0)',
+			)
+		),
+		'l18n_diffsource' => array (		
+			'config' => array (
+				'type' => 'passthrough'
+			)
+		),
+		'hidden' => array (		
+			'exclude' => 1,
+			'label'   => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
+			'config'  => array (
+				'type'    => 'check',
+				'default' => '0'
+			)
+		),
+		'fe_group' => array (		
+			'exclude' => 1,
+			'label'   => 'LLL:EXT:lang/locallang_general.xml:LGL.fe_group',
+			'config'  => array (
+				'type'  => 'select',
+				'items' => array (
+					array('', 0),
+					array('LLL:EXT:lang/locallang_general.xml:LGL.hide_at_login', -1),
+					array('LLL:EXT:lang/locallang_general.xml:LGL.any_login', -2),
+					array('LLL:EXT:lang/locallang_general.xml:LGL.usergroups', '--div--')
+				),
+				'foreign_table' => 'fe_groups'
+			)
+		),
+		"department_name" => Array (		
+			"exclude" => 1,		
+			"label" => "LLL:EXT:newspaper/locallang_db.xml:tx_newspaper_department.department_name",		
+			"config" => Array (
+				"type" => "input",	
+				"size" => "40",	
+				"max" => "40",	
+				"eval" => "required",
+			)
+		),
+	),
+	"types" => array (
+		"0" => array("showitem" => "sys_language_uid;;;;1-1-1, l18n_parent, l18n_diffsource, hidden;;1, department_name")
+	),
+	"palettes" => array (
+		"1" => array("showitem" => "fe_group")
+	)
+);
+
 require_once(PATH_typo3conf . 'ext/newspaper/tca_addon.php');
 ?>
