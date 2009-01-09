71c23da6818c44dd6ae95e8ed24862fc0645fdeb
Lene Preuss <lene.preuss@gmail.com>
2009-01-09 16:50:51 +0000

    edited with kickstarter

--- ext_tables.sql
+++ ext_tables.sql
@@ -25,24 +25,31 @@ CREATE TABLE tx_newspaper_extra_image (
 
 
 #
+# Table structure for table 'tx_newspaper_department'
+#
+CREATE TABLE tx_newspaper_department (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l18n_parent int(11) DEFAULT '0' NOT NULL,
+	l18n_diffsource mediumblob NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	fe_group int(11) DEFAULT '0' NOT NULL,
+	department_name tinytext NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
+
+
+
+#
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
 	tx_newspaper_extra tinytext NOT NULL
-);
-
-
-#
-# Table structure for table 'tx_newspaper_content_extra_mm'
-# This table has to be added manually to this file after using the kickstarter
-#
-CREATE TABLE tx_newspaper_content_extra_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(30) DEFAULT '' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  extra_type varchar(30) DEFAULT '' NOT NULL,
-  sorting int(11) unsigned DEFAULT '0' NOT NULL,
-  conf text,
-  KEY uid_local (uid_local),
-  KEY uid_foreign (uid_foreign)
 );
