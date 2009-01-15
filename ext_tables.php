87dcc6997e139d83545592aeecdab65968f9cdaa
Lene Preuss <lene.preuss@gmail.com>
2009-01-15 18:01:17 +0000

    edited in kickstarter

--- ext_tables.php
+++ ext_tables.php
@@ -74,6 +74,34 @@ $TCA["tx_newspaper_section"] = array (
 	)
 );
 
+
+t3lib_extMgm::allowTableOnStandardPages('tx_newspaper_page');
+
+
+t3lib_extMgm::addToInsertRecords('tx_newspaper_page');
+
+$TCA["tx_newspaper_page"] = array (
+	"ctrl" => array (
+		'title'     => 'LLL:EXT:newspaper/locallang_db.xml:tx_newspaper_page',		
+		'label'     => 'uid',	
+		'tstamp'    => 'tstamp',
+		'crdate'    => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'sortby' => 'sorting',	
+		'delete' => 'deleted',	
+		'enablecolumns' => array (		
+			'disabled' => 'hidden',	
+			'starttime' => 'starttime',	
+			'endtime' => 'endtime',
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_newspaper_page.gif',
+	),
+	"feInterface" => array (
+		"fe_admin_fieldList" => "hidden, starttime, endtime, type_name, get_var, get_value",
+	)
+);
+
 $tempColumns = Array (
 	"tx_newspaper_extra" => Array (		
 		"exclude" => 1,		
@@ -112,7 +140,4 @@ $tempColumns = Array (
 t3lib_div::loadTCA("pages");
 t3lib_extMgm::addTCAcolumns("pages",$tempColumns,1);
 t3lib_extMgm::addToAllTCAtypes("pages","tx_newspaper_associated_section;;;;1-1-1");
-
-require_once(PATH_typo3conf . 'ext/newspaper/ext_tables_addon.php');
-
 ?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -33,17 +33,42 @@ CREATE TABLE tx_newspaper_section (
 	tstamp int(11) DEFAULT '0' NOT NULL,
 	crdate int(11) DEFAULT '0' NOT NULL,
 	cruser_id int(11) DEFAULT '0' NOT NULL,
-    sorting int(10) DEFAULT '0' NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
 	deleted tinyint(4) DEFAULT '0' NOT NULL,
 	hidden tinyint(4) DEFAULT '0' NOT NULL,
-    starttime int(11) DEFAULT '0' NOT NULL,
-    endtime int(11) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
 	fe_group int(11) DEFAULT '0' NOT NULL,
 	section_name tinytext NOT NULL,
 	
 	PRIMARY KEY (uid),
 	KEY parent (pid)
 );
+
+
+
+#
+# Table structure for table 'tx_newspaper_page'
+#
+CREATE TABLE tx_newspaper_page (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	type_name tinytext NOT NULL,
+	get_var varchar(10) DEFAULT '' NOT NULL,
+	get_value int(11) DEFAULT '0' NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
+
 
 
 #
@@ -61,19 +86,3 @@ CREATE TABLE tt_content (
 CREATE TABLE pages (
 	tx_newspaper_associated_section int(11) DEFAULT '0' NOT NULL
 );
-
-# Table structure for table 'tx_newspaper_content_extra_mm'
-# This table has to be added manually to this file after using the kickstarter
-#
-CREATE TABLE tx_newspaper_content_extra_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(30) DEFAULT '' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  extra_type varchar(30) DEFAULT '' NOT NULL,
-  sorting int(11) unsigned DEFAULT '0' NOT NULL,
-  conf text,
-  KEY uid_local (uid_local),
-  KEY uid_foreign (uid_foreign)
-);
-
-
