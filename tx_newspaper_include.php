81a92a73632aaf47bc314cc63eb529bdb49932e8
Lene Preuss <lene.preuss@gmail.com>
2009-01-19 16:27:29 +0000

    include page definition by default

--- tx_newspaper_include.php
+++ tx_newspaper_include.php
@@ -39,6 +39,7 @@
 	require_once(BASEPATH.'/typo3conf/ext/newspaper/interfaces/interface.tx_newspaper_article.php');
 	require_once(BASEPATH.'/typo3conf/ext/newspaper/interfaces/interface.tx_newspaper_source.php');
 	require_once(BASEPATH.'/typo3conf/ext/newspaper/classes/class.tx_newspaper_section.php');
+	require_once(BASEPATH.'/typo3conf/ext/newspaper/classes/class.tx_newspaper_page.php');
 //}
 
 ?>
