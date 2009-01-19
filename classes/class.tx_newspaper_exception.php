bf778c2e7e194b3fe450786642edc1c5ac173389
Lene Preuss <lene.preuss@gmail.com>
2009-01-19 15:08:48 +0000

    enable throwing generic exceptions with a message

--- classes/class.tx_newspaper_exception.php
+++ classes/class.tx_newspaper_exception.php
@@ -30,14 +30,26 @@
  */
 
 /// Base class for all exceptions thrown by this Typo3 extension
-class tx_newspaper_Exception extends Exception { }
+class tx_newspaper_Exception extends Exception { 
+	public function __construct($message, $code = 0) {
+        parent::__construct($message, $code);
+    }
+}
 
 /// This Exception is thrown when opening a Source fails
 /** I could have named it OpenSourceFailedException, but no way! Open Source rules! :-)) */
-class tx_newspaper_SourceOpenFailedException extends tx_newspaper_Exception { }
+class tx_newspaper_SourceOpenFailedException extends tx_newspaper_Exception { 
+	public function __construct($message = '', $code = 0) {
+        parent::__construct($message, $code);
+    }	
+}
 
 /// This Exception is thrown when a Source is asked to create a class that is not an Article or Extra
-class tx_newspaper_WrongClassException extends tx_newspaper_Exception { }
+class tx_newspaper_WrongClassException extends tx_newspaper_Exception {
+	public function __construct($message = '', $code = 0) {
+        parent::__construct($message, $code);
+    }
+}
 
 /// This Exception is thrown if a feature is not yet implemented
 class tx_newspaper_NotYetImplementedException extends tx_newspaper_Exception { 
