633333c92b11aa6c99602b777c99264b28981023
Lene Preuss <lene.preuss@gmail.com>
2009-01-21 12:28:42 +0000

    little optimizations

--- classes/extra/class.tx_newspaper_extra_image.php
+++ classes/extra/class.tx_newspaper_extra_image.php
@@ -10,8 +10,8 @@ class tx_newspaper_extra_image extends tx_newspaper_ExtraImpl {
 	public function render($template = '') {
 		$ret = '<h4>' . $this->getAttribute('title') . "</h4>\n" .
 		'<p>' .
-		'<img src="' . $this->OutputToDataURI() .
-		   '" alt="' . $this->getAttribute('caption') . '" />';
+		'<img src="data:image/png;base64,'.base64_encode($this->getAttribute('image')) .
+		   '" alt="' . $this->getAttribute('caption') . '" />' .
 		"</p>\n" . $this->getAttribute('caption') . "\n";
 		
 		return $ret;
@@ -32,16 +32,6 @@ class tx_newspaper_extra_image extends tx_newspaper_ExtraImpl {
 	static function getModuleName() {
 		return 'npe_image'; // this is the default folder for data associated with newspaper etxension, overwrite in conrete Extras
 	}
-
-	/// Print images inline as data: URI
-	/** Snatched from 
-	 *  http://www.sencer.de/article/1135/how-to-inline-your-sparklinesimages-in-php-with-data-uris
-	 */
-	private function OutputToDataURI() {
-//		header('Content-type: text/html');
-		return "data:image/png;base64,".base64_encode($this->getAttribute('image'));  
-	} // function OutputToDataURI
-
 }
 
 ?>
