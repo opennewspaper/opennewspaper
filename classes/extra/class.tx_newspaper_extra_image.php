15e787b37f802775f456c4cdffe5600b7baa264c
Lene Preuss <lene.preuss@gmail.com>
2009-01-21 11:19:59 +0000

    preliminary render() function for ExtraImage

--- classes/extra/class.tx_newspaper_extra_image.php
+++ classes/extra/class.tx_newspaper_extra_image.php
@@ -4,7 +4,16 @@ require_once(BASEPATH . '/typo3conf/ext/newspaper/classes/class.tx_newspaper_ext
 
 class tx_newspaper_extra_image extends tx_newspaper_ExtraImpl {
 
-
+	/** Just a quick hack to see anything
+	 *  \todo use smarty
+	 */
+	public function render() {
+		$ret = '<h4>' . $this->getAttribute('title') . "</h4>\n" .
+		'<p>' .
+		'<img src="' . $this->OutputToDataURI() .
+		   '" alt="' . $this->getAttribute('caption') . '">';
+		"</p>\n" . $this->getAttribute('caption') . "\n";
+	}
 
 
 // internal name
@@ -21,6 +30,15 @@ class tx_newspaper_extra_image extends tx_newspaper_ExtraImpl {
 	static function getModuleName() {
 		return 'npe_image'; // this is the default folder for data associated with newspaper etxension, overwrite in conrete Extras
 	}
+
+	/// Print images inline as data: URI
+	/** Snatched from 
+	 *  http://www.sencer.de/article/1135/how-to-inline-your-sparklinesimages-in-php-with-data-uris
+	 */
+	private function OutputToDataURI() {
+		header('Content-type: text/html');
+		return "data:image/png;base64,".base64_encode($this->getAttribute('image'));  
+	} // function OutputToDataURI
 
 }
 
