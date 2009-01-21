f452c06eac20a9901781401554b7e12cebec76cb
Lene Preuss <lene.preuss@gmail.com>
2009-01-21 13:55:45 +0000

    auto-fixed changes from kickstarter

--- ext_tables.sql
+++ ext_tables.sql
@@ -259,3 +259,17 @@ CREATE TABLE tt_content (
 CREATE TABLE pages (
 	tx_newspaper_associated_section int(11) DEFAULT '0' NOT NULL
 );
+#
+# Table structure for table 'tx_newspaper_content_extra_mm'
+# This table has to be added manually to this file after using the kickstarter
+#
+CREATE TABLE tx_newspaper_content_extra_mm (
+  uid_local int(11) DEFAULT '0' NOT NULL,
+  tablenames varchar(30) DEFAULT '' NOT NULL,
+  uid_foreign int(11) DEFAULT '0' NOT NULL,
+  extra_type varchar(30) DEFAULT '' NOT NULL,
+  sorting int(11) unsigned DEFAULT '0' NOT NULL,
+  conf text,
+  KEY uid_local (uid_local),
+  KEY uid_foreign (uid_foreign)
+);
