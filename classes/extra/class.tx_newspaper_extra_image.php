706e0f00c9c0696db7829072300025edb003da40
Lene Preuss <lene.preuss@gmail.com>
2009-01-21 11:42:09 +0000

    do inline images work without a header?

--- classes/extra/class.tx_newspaper_extra_image.php
+++ classes/extra/class.tx_newspaper_extra_image.php
@@ -11,7 +11,7 @@ class tx_newspaper_extra_image extends tx_newspaper_ExtraImpl {
 		$ret = '<h4>' . $this->getAttribute('title') . "</h4>\n" .
 		'<p>' .
 		'<img src="' . $this->OutputToDataURI() .
-		   '" alt="' . $this->getAttribute('caption') . '"/>';
+		   '" alt="' . $this->getAttribute('caption') . '" />';
 		"</p>\n" . $this->getAttribute('caption') . "\n";
 		
 		return $ret;
@@ -38,7 +38,7 @@ class tx_newspaper_extra_image extends tx_newspaper_ExtraImpl {
 	 *  http://www.sencer.de/article/1135/how-to-inline-your-sparklinesimages-in-php-with-data-uris
 	 */
 	private function OutputToDataURI() {
-		header('Content-type: text/html');
+//		header('Content-type: text/html');
 		return "data:image/png;base64,".base64_encode($this->getAttribute('image'));  
 	} // function OutputToDataURI
 
