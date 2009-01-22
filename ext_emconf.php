71590c140ae57d5eb56748e040a297eea0721733
Lene Preuss <lene.preuss@gmail.com>
2009-01-22 18:13:45 +0000

    edited in kickstarter - fixed record browsers

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "newspaper"
 #
-# Auto generated 22-01-2009 17:12
+# Auto generated 22-01-2009 19:13
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
