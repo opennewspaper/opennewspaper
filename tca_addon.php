480753e0ea6a6392b59357118683b707082d96e1
Lene Preuss <lene.preuss@gmail.com>
2009-01-28 13:54:15 +0000

    fix ranges artificially imposed by kickstarter

--- tca_addon.php
+++ tca_addon.php
@@ -27,4 +27,18 @@ $TCA['tx_newspaper_extra_image']['columns']['extra_field']['config']['type'] = '
 $TCA['tx_newspaper_extra_image']['columns']['extra_field']['config']['userFunc'] = 'tx_newspaper->getCodeForBackend';
 $TCA['tx_newspaper_extra_image']['columns']['extra_field']['config']['noTableWrapping'] = true;
 
+// fix ranges artificially imposed by kickstarter
+$TCA["tx_newspaper_extra"]["columns"]["extra_uid"]["config"]["range"] = array (
+	"upper" => "1000000000",
+	"lower" => "1"
+);
+$TCA["tx_newspaper_page"]["columns"]["get_value"]["config"]["range"] = array (
+	"upper" => "1000000000",
+	"lower" => "1"
+);
+$TCA["tx_newspaper_pagezone"]["columns"]["pagezone_uid"]["config"]["range"] = array (
+	"upper" => "1000000000",
+	"lower" => "1"
+);
+
 ?>
