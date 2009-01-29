9f757c857f1164866eec238989febbf291c1a574
Lene Preuss <lene.preuss@gmail.com>
2009-01-29 19:34:56 +0000

    include definition for class tx_newspaper

--- tx_newspaper_include.php
+++ tx_newspaper_include.php
@@ -34,6 +34,7 @@
 //	require_once(t3lib_extMgm::extPath('newspaper', 'interfaces/interface.tx_newspaper_article.php'));
 //	require_once(t3lib_extMgm::extPath('newspaper', 'interfaces/interface.tx_newspaper_source.php'));
 //} else {
+	require_once(BASEPATH.'/typo3conf/ext/newspaper/classes/class.tx_newspaper.php');
 	require_once(BASEPATH.'/typo3conf/ext/newspaper/classes/class.tx_newspaper_exception.php');
 	require_once(BASEPATH.'/typo3conf/ext/newspaper/interfaces/interface.tx_newspaper_extra.php');
 	require_once(BASEPATH.'/typo3conf/ext/newspaper/interfaces/interface.tx_newspaper_article.php');
