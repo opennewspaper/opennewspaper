d59daaf9b28da68fe1eea263720c3f2d408e2d8f
Lene Preuss <lene.preuss@gmail.com>
2009-01-29 21:41:15 +0000

    factored out functions which have to deal with sources to interface WithSource

--- interfaces/interface.tx_newspaper_extra.php
+++ interfaces/interface.tx_newspaper_extra.php
@@ -37,29 +37,6 @@
 	 */
 	public function setAttribute($attribute, $value);
 
-	/// \return The Source object associated with this Article (if any)
-	function getSource();
-	/// Set the Source object associated with this Article
-	/** \param $source The new Source */
-	function setSource(tx_newspaper_Source $source);
-
-	/// Definition of attributes and their mapping to implementation fields
-	/// for all supported Sources
-	/** \note For every new Source that is defined, if the Extra must support
-	 * 		  that Source, you must alter this function to contain the mapping
-	 * 		  to the new Source type. Usually that will require to add fields to
-	 * 		  a static array.
-	 *  \param $fieldname Name of the attribute which must be mapped
-	 *  \param $source The Source for which the mapping is wanted
-	 *  \return The name of attribute \p $fieldname in Source \p $source
-	 *  \throw WrongClassException If the mapping for class \p $source is not
-	 * 							   configured
-	 */
-	static function mapFieldToSourceField($fieldname, tx_newspaper_Source $source);
-
-	/// Additional info needed to instantiate an Extra, such as a SQL table
-	static function sourceTable(tx_newspaper_Source $source);
-
 	/// \return the name of the Extra = the name of the database table the Extra data is stored in
 	static function getName();
 
