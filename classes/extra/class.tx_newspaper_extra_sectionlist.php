697094c067f9c4d7a9f43b91ff13effaa5d322b3
Lene Preuss <lene.preuss@gmail.com>
2009-01-30 14:18:14 +0000

    added SectionList Extra

---
+++ classes/extra/class.tx_newspaper_extra_sectionlist.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(BASEPATH . '/typo3conf/ext/newspaper/classes/class.tx_newspaper_extraimpl.php');
+
+class tx_newspaper_extra_SectionList extends tx_newspaper_ExtraImpl {
+
+	public function __construct($uid = 0) { if ($uid) parent::__construct($uid); }
+	
+	/** Just a quick hack to see anything
+	 *  \todo everything
+	 */
+	public function render($template = '') {
+		return "<h1>Section List Plugin -coming soon to a store near you</h3>\n";
+	}
+
+
+	static function getName() {
+		return 'tx_newspaper_extra_sectionlist';
+	}
+
+	static function getTitle() {
+		return 'SectionList';
+	}
+
+	static function getModuleName() {
+		return 'npe_sect_l'; 
+	}
+}
+
+tx_newspaper_ExtraImpl::registerExtra(new tx_newspaper_extra_ArticleRenderer());
+
+?>
