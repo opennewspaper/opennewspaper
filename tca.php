928e25f41a8b5f4bcf85e24fd50c0012b13553d3
Lene Preuss <lene.preuss@gmail.com>
2009-01-30 15:17:39 +0000

    auto-fixed changes from kickstarter

--- tca.php
+++ tca.php
@@ -1229,4 +1229,5 @@ $TCA["tx_newspaper_articlelist_auto"] = array (
 		"1" => array("showitem" => "starttime, endtime, fe_group")
 	)
 );
+require_once(PATH_typo3conf . 'ext/newspaper/tca_addon.php');
 ?>
