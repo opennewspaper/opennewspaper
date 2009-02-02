cfdbf8f134d93df7845b32bb9583c4809fd7aa28
Lene Preuss <lene.preuss@gmail.com>
2009-02-02 11:09:50 +0000

    wrote readExtraItem() (not quite complete yet, pid and sorting are missing)

--- classes/class.tx_newspaper_extraimpl.php
+++ classes/class.tx_newspaper_extraimpl.php
@@ -14,6 +14,15 @@ require_once(BASEPATH.'/typo3conf/ext/newspaper/tx_newspaper_include.php');
  *  concrete Extra anyway. All Extras (except tx_newspaper_Article) inherit from
  *  this class.
  * 
+ *  abstract functions:
+ *  - static function getName()
+ *	- static function getTitle()
+ *	- static function getModuleName()
+ *
+ *  generic functions which should be overridden:
+ *  - __construct()
+ *  - render()
+ * 
  *  \todo Currently implements tx_newspaper_WithSource as well. I am not sure if
  *  this makes sense.
  */ 
@@ -109,7 +118,7 @@ abstract class tx_newspaper_ExtraImpl
 	public static function readExtraItem($uid, $table) {
 t3lib_div::devlog('Extra Image: readExtraItem - reached!', 'newspaper', 0);
 		
-		return tx_newspaper::selectOneRow('*', $table, "uid = $uid");
+		return tx_newspaper::selectOneRow('*', $table, 'uid = ' . intval($uid));
 	}
 	
 	/// Create the record for a concrete Extra in the table of abstract Extras
@@ -122,16 +131,51 @@ t3lib_div::devlog('Extra Image: readExtraItem - reached!', 'newspaper', 0);
 	 *  \param $table Table of concrete Extras. If empty, 
 	 */ 
 	public static function createExtraRecord($uid, $table) {
-		/// \todo check if record is already present in extra table
+		/// Check if record is already present in extra table
+		$row = tx_newspaper::selectZeroOrOneRows(
+			'uid', self::$table, 
+			'extra_table = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($table) .
+			' AND extra_uid = ' . intval($uid)	
+		);
+		if ($row['uid']) return;
 		
 		/// \todo read typo3 fields to copy into extra table
+		$row = tx_newspaper::selectOneRow(
+			implode(self::$fields_to_copy_into_extra_table),
+			$table,
+			'uid = ' . intval($uid)
+		);
 		
-		/// \todo write the uid and table into extra table, with the values read above
-		throw new tx_newspaper_NotYetImplementedException();
+		/// write the uid and table into extra table, with the values read above
+		$row['extra_uid'] = $uid;
+		$row['extra_table'] = $table;
+
+		/** \todo sorting, pid must be calculated somehow. it does not matter if 
+		 *  that computation is expensive, because it is executed only once per 
+		 *  extra, (hopefully) during its creation.
+		 */
+		
+		$query = $GLOBALS['TYPO3_DB']->INSERTquery(self::$table, $row);
+		$res = $GLOBALS['TYPO3_DB']->sql_query(self::$query);
+		tx_newspaper::$query = $query;
+				
+//		throw new tx_newspaper_NotYetImplementedException();
 	}
 	
 	private $attributes = array();				///< attributes of the extra
 	
 	private static $registeredExtra = array();	///< list of registered Extras
+	
+	/// Extra table must be defined here because tx_newspaper_Extra is an interface
+	/** \todo this table is defined in tx_newspaper_Extra_Factory too. decide
+	 * 		  on one class to store it!
+	 */
+	private static $table = 'tx_newspaper_extra';
+	
+	private static $fields_to_copy_into_extra_table = array(
+		'tstamp', 'crdate', 'cruser_id', 'deleted', 'hidden', 
+		'starttime', 'endtime', 'fe_group'
+	);
+	
 }
 ?>
