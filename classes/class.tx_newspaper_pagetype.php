0d3e10d648721fc15068e190775b4c9b116e5478
Lene Preuss <lene.preuss@gmail.com>
2009-02-06 15:45:42 +0000

    added classes for page type and pagezone type

---
+++ classes/class.tx_newspaper_pagetype.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *  \file class.tx_newspaper_pagetype.php
+ * 
+ *  This file is part of the TYPO3 extension "newspaper".
+ * 
+ *  Copyright notice
+ *
+ *  (c) 2008 Helge Preuss, Oliver Schroeder, Samuel Talleux <helge.preuss@gmail.com, oliver@schroederbros.de, samuel@talleux.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *  
+ *  \author Helge Preuss <helge.preuss@gmail.com>
+ *  \date Feb 6, 2009
+ */
+ 
+/// brief description
+/** long description
+ */
+class tx_newspaper_PageType {
+ 	
+ 	/// Construct a tx_newspaper_PageType given the UID of the SQL record
+ 	function __construct($uid) {
+		$this->attributes = tx_newspaper::selectOneRow(
+			'*', tx_newspaper::getTable($this), "uid = $uid"
+		);
+ 	}
+ 	
+}
+?>
