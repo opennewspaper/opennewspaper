c13f11372092fb02ff0131b47b98f58d23280673
Oliver Schröder <oliver@schroederbros.de>
2009-02-09 15:11:33 +0000

    added check, if newspaper sysfolder should be hidden

--- ext_tables_addon.php
+++ ext_tables_addon.php
@@ -11,4 +11,16 @@ require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper.php');
 $TCA['tt_content']['columns']['tx_newspaper_extra']['config']['type'] = 'user';
 $TCA['tt_content']['columns']['tx_newspaper_extra']['config']['userFunc'] = 'tx_newspaper->renderList';
 
+
+if (TYPO3_MODE == 'BE') {
+	$sysfolder = tx_newspaper_Sysfolder::getInstance();
+	if ($TYPO3_CONF_VARS['EXTCONF']['newspaper']['setup']['hideFolder']) {
+		$pid = $sysfolder->getPid(new tx_newspaper());
+/// \to do: append $pid (current version overwrites)
+		t3lib_extMgm::addUserTSConfig('
+			option.hideRecords.pages = ' . $pid . '
+		');
+	}
+}
+
 ?>
