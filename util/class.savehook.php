b0fd9f878dd26dcd6b66653801b77f86498357d2
Oliver Schröder <oliver@schroederbros.de>
2009-02-10 14:59:47 +0000

    devlog++

--- util/class.savehook.php
+++ util/class.savehook.php
@@ -2,9 +2,11 @@
 
 class user_savehook_newspaper {
 
-//TODO: check if needed
+
   function processDatamap_preProcessFieldArray($incomingFieldArray, $table, $id, $that) {
-#t3lib_div::devlog('save hook table', 'newspaper', 0, $table);
+t3lib_div::devlog('sh fields', 'newspaper', 0, $incomingFieldArray);
+t3lib_div::devlog('sh table', 'newspaper', 0, $table);
+t3lib_div::devlog('sh id', 'newspaper', 0, $id);
 
   }
 
