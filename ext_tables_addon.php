b3b8623b4c89b53065c8927f0246fbc0687546ea
Oliver Schröder <oliver@schroederbros.de>
2009-02-10 10:24:50 +0000

    devlog++

--- ext_tables_addon.php
+++ ext_tables_addon.php
@@ -17,7 +17,9 @@ if (TYPO3_MODE == 'BE') {
 	$pid = $sysfolder->getPid(new tx_newspaper());
 	
 	$tsconfig = t3lib_BEfunc::getPagesTSconfig($pid);
-t3lib_div::devlog('tsc', 'newspaper', 0, $tsconfig);
+t3lib_div::devlog('pts', 'newspaper', 0, $tsconfig);
+t3lib_div::devlog('uts', 'newspaper', 0, $GLOBALS['BE_USER']->userTS);
+
 
 	// read other pages to be hidden
 	if (isset($tsconfig['options.']['hideRecords.']['pages']) && $tsconfig['tx_newspaper.']['showFolder']) {
