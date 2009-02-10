46d1ae2b3cb044eafa634ada21801ddecd34ca00
Oliver Schröder <oliver@schroederbros.de>
2009-02-10 13:24:27 +0000

    test+

--- ext_tables_addon.php
+++ ext_tables_addon.php
@@ -17,6 +17,10 @@ $TCA['pages']['columns']['module']['config']['items'][] = array('Newspaper', 'ne
 
 if (TYPO3_MODE == 'BE') {
 /// \to do: hide sysfolder (with user tsconfig): options.hideRecords.pages	
+
+$s = tx_newspaper_Sysfolder.getInstance();
+$s->getPid(new tx_newspaper_Extra_Image());
+
 }
 
 ?>
