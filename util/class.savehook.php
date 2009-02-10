6bc857c0e0e29018b843dd87c778584f12582268
Oliver Schröder <oliver@schroederbros.de>
2009-02-10 17:25:51 +0000

    using getMainFields_preProcess to change pid for np records

--- util/class.savehook.php
+++ util/class.savehook.php
@@ -5,22 +5,9 @@ class user_savehook_newspaper {
 	function getMainFields_preProcess($table, &$row, $that) {
 t3lib_div::devlog('sh mainfields table', 'newspaper', 0, $table);
 t3lib_div::devlog('sh mainfields row', 'newspaper', 0, $row);
-		
-	}
-
-
-  function processDatamap_preProcessFieldArray(&$incomingFieldArray, $table, $id, $that) {
-t3lib_div::devlog('sh datamap fields', 'newspaper', 0, $incomingFieldArray);
-t3lib_div::devlog('sh datamap table', 'newspaper', 0, $table);
-t3lib_div::devlog('sh datamap id', 'newspaper', 0, $id);
-
-#t3lib_div::devlog('classes', 'newspaper', 0, get_declared_classes());
-
 
 /// \todo: remove by-pass after article class and table have the same name
 $class_bypass = ($table == 'tx_newspaper_article')? 'tx_newspaper_ArticleImpl' : $table;
-
-
 
 	/// check if a newspaper record is saved and make sure it's stored in the appropriate sysfolder
 	if (class_exists($class_bypass)) { ///<newspaper specification: table name = class name
@@ -29,11 +16,21 @@ $class_bypass = ($table == 'tx_newspaper_article')? 'tx_newspaper_ArticleImpl' :
 			/// tx_newspaper_InSysFolder is implemented, so record is to be stored in a special sysfolder
 			$sf = tx_newspaper_Sysfolder::getInstance();
 			$pid = $sf->getPid($np_obj);
-#t3lib_div::devlog('sh datamap pid', 'newspaper', 0, $pid);
-			$incomingFieldArray['pid'] = $pid;
-t3lib_div::devlog('sh datamap fields modified', 'newspaper', 0, $incomingFieldArray);
+			$row['pid'] = $pid;
 		}
 	}
+
+		
+	}
+
+
+  function processDatamap_preProcessFieldArray(&$incomingFieldArray, $table, $id, $that) {
+#t3lib_div::devlog('sh datamap fields', 'newspaper', 0, $incomingFieldArray);
+#t3lib_div::devlog('sh datamap table', 'newspaper', 0, $table);
+#t3lib_div::devlog('sh datamap id', 'newspaper', 0, $id);
+#t3lib_div::devlog('classes', 'newspaper', 0, get_declared_classes());
+
+
 
 
 
