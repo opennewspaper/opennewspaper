3027540f36e362d4d92c2d6b1b9bb188cef86f0f
Oliver Schröder <oliver@schroederbros.de>
2009-02-10 16:09:02 +0000

    bypass implmented

--- util/class.savehook.php
+++ util/class.savehook.php
@@ -11,9 +11,14 @@ t3lib_div::devlog('sh id', 'newspaper', 0, $id);
 t3lib_div::devlog('classes', 'newspaper', 0, get_declared_classes());
 
 
+/// \todo: remove by-pass after article class and table have the same name
+$class_bypass = ($table == 'tx_newspaper_article')? 'tx_newspaper_ArticleImpl' : $table;
+
+
+
 	/// check if a newspaper record is saved and make sure it's stored in the appropriate sysfolder
-	if (class_exists($table)) { ///<newspaper specification: table name = class name
-		$np_obj = new $table();
+	if (class_exists($class_bypass)) { ///<newspaper specification: table name = class name
+		$np_obj = new $class_bypass();
 		if (in_array("tx_newspaper_InSysFolder", class_implements($np_obj))) { 
 			/// tx_newspaper_InSysFolder is implemented, so record is to be stored in a special sysfolder
 			$pid = $np_obj->getModuleName();
