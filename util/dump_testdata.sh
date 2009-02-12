549a1a65bb3c04ea922099f12f8479a01f4807fd
Lene Preuss <lene.preuss@gmail.com>
2009-02-12 17:03:17 +0000

    new DB dump

--- util/dump_testdata.sh
+++ util/dump_testdata.sh
@@ -22,7 +22,7 @@ db_password=axtimwalde
 # DB to dump - adjust on a per-user basis
 db=onlinetaz_2_hel
 # change this if you want to call the script from somewhere else than the
-# tx_newspaper base dir
+# tx_newspaper base dir. trailing '/' is required.
 path_prefix=
 # subdirectory in which to store the dump. should stay the same.
 tests_dir=tests
@@ -35,13 +35,13 @@ dump_filename=testdata_newspaper.sql
 
 tables=''
 for i in $(echo show tables\; | \
-		   mysql -u onlinetaz --password=axtimwalde onlinetaz_2_hel | \
-		   grep tx_newspaper); do 
+	   mysql -u ${db_user} --password=${db_password} ${db} | \
+	   grep tx_newspaper); do 
 	tables="$tables $i"
 done
 
 mysqldump -u ${db_user} --password=${db_password} ${db} ${tables} > \
-	${path_prefix}/${tests_dir}/${dump_filename}
+	${path_prefix}${tests_dir}/${dump_filename}
 
 # that's all folks. actually a one liner if you call it from the shell, but i
 # wanted it to be as transparent as possible :-)
