1a9ec8ddff5f52fa2e9d4dae2fc8cc468565376d
Lene Preuss <lene.preuss@gmail.com>
2009-02-12 17:31:43 +0000

    reactivated save hook

--- util/class.savehook.php
+++ util/class.savehook.php
@@ -3,7 +3,6 @@
 class user_savehook_newspaper {
 
 	function processDatamap_postProcessFieldArray($status, $table, $id, &$fieldArray, $that) {
-		return;
 #t3lib_div::devlog('sh post status', 'newspaper', 0, $status);
 #t3lib_div::devlog('sh post table', 'newspaper', 0, $table);
 #t3lib_div::devlog('sh post id', 'newspaper', 0, $id);
