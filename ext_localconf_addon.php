41c8eba5e336da27661b7572564c723ba0f051ab
Oliver Schröder <oliver@schroederbros.de>
2009-02-13 16:27:21 +0000

    tx_newspaper_include.php included

--- ext_localconf_addon.php
+++ ext_localconf_addon.php
@@ -10,5 +10,5 @@
 	// register hook to add javascript and css to BE (loaded to top)
 	$GLOBALS['TYPO3_CONF_VARS']['typo3/backend.php']['additionalBackendItems'][] = PATH_typo3conf . 'ext/newspaper/util/additionalBackendItems.php';
 
-	require_once('tx_newspaper_include.php');
+	require_once(t3lib_extMgm::extPath('newspaper', 'tx_newspaper_include.php'));
 ?>
