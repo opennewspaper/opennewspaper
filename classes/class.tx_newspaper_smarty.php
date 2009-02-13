3263f1460de38c47208476e222aaf070a452cb43
Lene Preuss <lene.preuss@gmail.com>
2009-02-13 15:40:37 +0000

    try t3lib_extMgm::extPath() again (just for kicks)

--- classes/class.tx_newspaper_smarty.php
+++ classes/class.tx_newspaper_smarty.php
@@ -29,8 +29,8 @@
  *  \date Feb 13, 2009
  */
  
-//require_once(t3lib_extMgm::extPath('smarty', 'Smarty.class.php'));
-require_once(BASEPATH.'/typo3conf/ext/smarty/Smarty.class.php');
+require_once(t3lib_extMgm::extPath('smarty', 'Smarty.class.php'));
+//require_once(BASEPATH.'/typo3conf/ext/smarty/Smarty.class.php');
 
 /// Smarty rendering engine with configurable template directory
 /** Smarty suffers from the limitation that you can only have \em one folder to
