cc907a57bea1ea6a052eb69cd55d6a62311c1330
Oliver Schröder <oliver@schroederbros.de>
2009-02-16 17:03:53 +0000

    devlog++

--- classes/class.tx_newspaper_extra_be.php
+++ classes/class.tx_newspaper_extra_be.php
@@ -187,6 +187,7 @@ t3lib_div::devlog('renderListItem item', 'newspaper', 0, $item);
 		/// read tsconfig for Extra data
 		$tsconfig = t3lib_BEfunc::getPagesTSconfig($sf->getPidRootfolder());
 t3lib_div::devlog('be mode tsc', 'newspaper', 0, $tsconfig);
+t3lib_div::devlog('user ts?', 'newspaper', 0, $GLOBALS['BE_USER']->userTS);
 
 		if (isset($tsconfig['tx_newspaper.']['extra_mode'])) {
 			switch(trim(strtolower($tsconfig['tx_newspaper.']['extra_mode']))) {
