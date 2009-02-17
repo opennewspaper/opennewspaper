0fa840b2d5cf8af611305e27c73d3b30864fde05
Lene Preuss <lene.preuss@gmail.com>
2009-02-17 20:45:55 +0000

    debug++

--- classes/class.tx_newspaper_taz_redsyssource.php
+++ classes/class.tx_newspaper_taz_redsyssource.php
@@ -131,7 +131,7 @@ class tx_newspaper_taz_RedsysSource implements tx_newspaper_Source {
     	/// Check if a .pag file exists. if so, we're in a current production seitenbereich
     	$pathname = array_pop(explode('/', $path));
     	if (file_exists(red_get_var($this->red_private, 'TxtBaseDir')."/$path/$pathname.pag")) {
-    		t3lib_div::debug(file_get_contents(red_get_var($this->red_private, 'TxtBaseDir')."/$path/$pathname.pag"));
+//    		t3lib_div::debug(file_get_contents(red_get_var($this->red_private, 'TxtBaseDir')."/$path/$pathname.pag"));
     		$text = red_text_open($this->red_private, "$path/$pathname.pag");
 			t3lib_div::debug(red_text_get($text, 'TxtList'));	
     	}
