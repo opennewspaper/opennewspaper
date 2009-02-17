670e7ffeec2828ed6f5dfeefea8affd21903b26a
Oliver Schröder <oliver@schroederbros.de>
2009-02-17 18:52:33 +0000

    testing new main module

--- ext_tables.php
+++ ext_tables.php
@@ -15,7 +15,7 @@ if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]
 
 
 if (TYPO3_MODE == 'BE')	{
-		
+	t3lib_extMgm::addModule('txnewspaperMmain','','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');	
 	t3lib_extMgm::addModule('web','txnewspaperM1','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
 }
 
