a051a229674216db1b67aea675d51e6a90f61a20
Lene Preuss <lene.preuss@gmail.com>
2009-02-17 20:10:59 +0000

    test only every 10th article

--- tests/class.RedsysSource_testcase.php
+++ tests/class.RedsysSource_testcase.php
@@ -157,11 +157,10 @@ class test_RedsysSource_testcase extends tx_phpunit_testcase {
 				$this->assertTrue(sizeof($articles) > 0, 
 								  "you should find at least one article in $day. " );
 
-#				set_time_limit(40);			//	allow 40 seconds more for the next loop
 				$count = 0;
 				foreach ($articles as $article_path) {
 					$count++;
-					if ($count%3 == 0) {
+					if ($count%10 == 0) {
 						$article = $this->source->readArticle('tx_newspaper_ArticleImpl', $article_path);
 						$this->doTestIfArticleValid($article, "source->browse() with path $article_path", array('teaser', 'ressort'));
 					}
