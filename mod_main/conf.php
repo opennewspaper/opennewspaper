d944f803c71ae0830156761f4e38ac3811c5db51
Oliver Schröder <oliver@schroederbros.de>
2009-02-17 18:49:46 +0000

    Mein module for newspaper ext - initial upload

---
+++ mod_main/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/taz_modul/mod_main/');
+$BACK_PATH='../../../../typo3/';
+$MCONF['name']='txtazmodulMmain';
+
+	
+$MCONF['access']='user,group';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:taz_modul/mod_main/locallang_mod.xml';
+?>
