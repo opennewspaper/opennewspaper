1a5b9c103b7574d574534902bb88316795cbd757
Oliver Schröder <oliver@schroederbros.de>
2009-02-20 15:28:17 +0000

    kickstarter: is_article added to page zone type

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "newspaper"
 #
-# Auto generated 20-02-2009 16:21
+# Auto generated 20-02-2009 16:28
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
