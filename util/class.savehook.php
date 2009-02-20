db61235ecc030e5b3c6c443bf29a397dca213be2
Oliver Schröder <oliver@schroederbros.de>
2009-02-20 17:50:59 +0000

    save hook: make sure no other page zone type with is_article flag set exists

--- util/class.savehook.php
+++ util/class.savehook.php
@@ -11,6 +11,29 @@ class user_savehook_newspaper {
 /// \todo: remove by-pass after article class and table have the same name
 $class_bypass = ($table == 'tx_newspaper_article')? 'tx_newspaper_ArticleImpl' : $table;
 
+
+		/// check if a page zone type with is_article fals set is allowed
+		$pzt = new tx_newspaper_PageZoneType(); 
+		if  ($table == $pzt->getTable() && 
+			isset($fieldArray['is_article']) && 
+			$fieldArray['is_article'] == 1 &&
+			($status = 'new' || $status == 'update')
+		) {
+			/// make sure no other page zone type with is_article flag set exists
+			$sf = tx_newspaper_Sysfolder::getInstance();
+			$pid = $sf->getPid($pzt);
+			$row = tx_newspaper::selectRows(
+				'uid, name',
+				$pzt->getTable(),
+				'pid=' . $pid . ' AND uid !=' . $id . ' AND deleted=0 AND is_article=1'
+			);
+#t3lib_div::devlog('pzt: # is_article', 'newspaper', 0, count($row));
+			if (count($row) > 0) {
+				die('Fatal error: Only one page zone type can have the "is article" flag set. You change was not saved.<br /><br /><a href="javascript:history.back();">Click here to retry</a>');
+			}
+		}
+
+
 		/// check if a newspaper record is saved and make sure it's stored in the appropriate sysfolder
 		if (class_exists($class_bypass)) { ///<newspaper specification: table name = class name
 			$np_obj = new $class_bypass();
