14099dce0a748965186c1b17ef0c2dc7bfd11db8
Oliver Schröder <oliver@schroederbros.de>
2009-02-23 12:44:21 +0000

    removed selectRowsNotEmpty() selectRows() can return an emtpy result

--- classes/class.tx_newspaper.php
+++ classes/class.tx_newspaper.php
@@ -101,35 +101,14 @@ t3lib_div::devlog('tx_newspaper->renderList pa', 'newspaper', 0, $PA);
 		self::$query = $GLOBALS['TYPO3_DB']->SELECTquery(
 			$fields, $table, $where, $groupBy, $orderBy, $limit);
 		$res = $GLOBALS['TYPO3_DB']->sql_query(self::$query);
-		
-		$rows = array();
+
 		if ($res) {        
+	        $rows = array();
 	        while($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))
 	        	$rows[] = $row;
-		} 
-		return $rows;		
-	}
-	
-
-	/// Execute a SELECT query, check the result, return all records, if any; exception if no records are found
-	/** \param $fields Fields to SELECT
-	 *  \param $table Table to SELECT FROM
-	 *  \param $where WHERE-clause (defaults to selecting all records)
-	 *  \param $groupBy Fields to GROUP BY
-	 *  \param $orderBy Fields to ORDER BY
-	 *  \param $limit Maximum number of records to SELECT
-	 */
-	public static function selectRowsNotEmpty($fields, $table, $where = '1',
-									$groupBy = '', $orderBy = '', $limit = '') {
-		$rows = self::selectRows($fields, $table, $where, 
-			$groupBy, $orderBy , $limit);
-		if (count($rows) > 0)
 			return $rows;
-		else
-			throw new tx_newspaper_NoResException(self::$query);		
-	}
-
-
+		} else throw new tx_newspaper_NoResException(self::$query);		
+	}	
 
 	
 	/// inserts a record using T3 API
