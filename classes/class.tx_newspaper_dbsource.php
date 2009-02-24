2d33a3304f87e2641a5255ab8305abdf508764f3
Lene Preuss <lene.preuss@gmail.com>
2009-02-24 19:15:07 +0000

    renamed files to match classes

--- classes/class.tx_newspaper_dbsource.php
+++ classes/class.tx_newspaper_dbsource.php
@@ -7,8 +7,8 @@
  */
 
 require_once(PATH_typo3conf . 'ext/newspaper/interfaces/interface.tx_newspaper_source.php');
-require_once(PATH_typo3conf . 'ext/newspaper/interfaces/interface.tx_newspaper_article.php');
-require_once(PATH_typo3conf . 'ext/newspaper/interfaces/interface.tx_newspaper_extra.php');
+require_once(PATH_typo3conf . 'ext/newspaper/interfaces/interface.tx_newspaper_articleiface.php');
+require_once(PATH_typo3conf . 'ext/newspaper/interfaces/interface.tx_newspaper_extraiface.php');
 require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_sourcebehavior.php');
 
 /// A Source which reads articles from the taz redaktionssystem
