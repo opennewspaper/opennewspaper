2b33e17c981fba084ad7890da2ce9e2a2495f502
Lene Preuss <lene.preuss@gmail.com>
2009-02-24 16:00:14 +0000

    debug--

--- tests/class.Extra_testcase.php
+++ tests/class.Extra_testcase.php
@@ -175,7 +175,6 @@ class test_Extra_testcase extends tx_phpunit_testcase {
 				tx_newspaper_Extra_Factory::getExtra2ArticleTable(),
 				'uid_local = ' . $article_uid . ' AND uid_foreign = ' . intval($extra_supertable_uid)
 			);
-			t3lib_div::debug(tx_newspaper::$query);
 			tx_newspaper::deleteRows(
 				tx_newspaper_Extra_Factory::getExtraTable(),
 				'uid = ' . $extra_supertable_uid
