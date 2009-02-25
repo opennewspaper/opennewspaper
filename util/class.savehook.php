02c24fb2b591ac92edf1c4d742d68f016eea52ca
Oliver Schröder <oliver@schroederbros.de>
2009-02-25 09:34:31 +0000

    removed bypass table name = class name not active for all classes

--- util/class.savehook.php
+++ util/class.savehook.php
@@ -10,9 +10,6 @@ class user_savehook_newspaper {
 #t3lib_div::devlog('sh post id', 'newspaper', 0, $id);
 #t3lib_div::devlog('sh post fields', 'newspaper', 0, $fieldArray);
 
-/// \todo: remove by-pass after article class and table have the same name
-$class_bypass = ($table == 'tx_newspaper_article')? 'tx_newspaper_ArticleImpl' : $table;
-
 
 		/// check if a page zone type with is_article flag set is allowed
 		$pzt = new tx_newspaper_PageZoneType(); 
@@ -42,8 +39,8 @@ $class_bypass = ($table == 'tx_newspaper_article')? 'tx_newspaper_ArticleImpl' :
 
 
 		/// check if a newspaper record is saved and make sure it's stored in the appropriate sysfolder
-		if (class_exists($class_bypass)) { ///<newspaper specification: table name = class name
-			$np_obj = new $class_bypass();
+		if (class_exists($table)) { ///<newspaper specification: table name = class name
+			$np_obj = new $table();
 			if (in_array("tx_newspaper_InSysFolder", class_implements($np_obj))) { 
 				/// tx_newspaper_InSysFolder is implemented, so record is to be stored in a special sysfolder
 				$sf = tx_newspaper_Sysfolder::getInstance();
