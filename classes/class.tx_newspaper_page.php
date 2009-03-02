d7a8145f9692be8a3df6e1c3dc759d3f566c2c81
Lene Preuss <lene.preuss@gmail.com>
2009-03-02 12:37:29 +0000

    removed premature optimization

--- classes/class.tx_newspaper_page.php
+++ classes/class.tx_newspaper_page.php
@@ -50,19 +50,17 @@ class tx_newspaper_Page implements tx_newspaper_InSysFolder {
 	 *  \param $condition SQL WHERE condition to further specify the page
 	 */
 	public function __construct(tx_newspaper_Section $parent = null, tx_newspaper_PageType $type = null) {
-		if (TYPO3_MODE == 'FE') {
-			$this->parentSection = $parent;
-			$this->pagetype = $type;
+		$this->parentSection = $parent;
+		$this->pagetype = $type;
 	
-			/// Configure Smarty rendering engine
-			$this->smarty = new tx_newspaper_Smarty();
-			$this->smarty->setTemplateSearchPath(
-				array(
-					'template_sets/' . strtolower($this->pagetype->getAttribute('type_name')),
-					'template_sets'
-				)
-			);
-		}
+		/// Configure Smarty rendering engine
+		$this->smarty = new tx_newspaper_Smarty();
+		$this->smarty->setTemplateSearchPath(
+			array(
+				'template_sets/' . strtolower($this->pagetype->getAttribute('type_name')),
+				'template_sets'
+			)
+		);
  	}
  	
  	function getAttribute($attribute) {
@@ -125,6 +123,7 @@ class tx_newspaper_Page implements tx_newspaper_InSysFolder {
  		if (!$template) {
  			$template = $this;
  		}
+ 		t3lib_div::debug($this->smarty);
  		$this->smarty->assign('section', $this->parentSection->getAttribute('section_name'));
  		$this->smarty->assign('page_type', $this->pagetype->getAttribute('type_name'));
  		
