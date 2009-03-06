ddc00189f8aa71821f08b3cbfcc798f0396b058b
Lene Preuss <lene.preuss@gmail.com>
2009-03-06 12:20:59 +0000

    debug++

--- classes/class.tx_newspaper_section.php
+++ classes/class.tx_newspaper_section.php
@@ -78,7 +78,7 @@
 			'uid', 'tx_newspaper_page',
 			'section = ' . $this->getAttribute('uid') 
  		);
- 		t3lib_div::debug($row);
+
  		$subpages = array();
  		foreach ($row as $record) {
  			$subpages[] = new tx_newspaper_Page((int)$record['uid']);
