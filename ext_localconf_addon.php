235542e8feb799a40fb0ad096a6a29628f9aca71
Oliver Schröder <oliver@schroederbros.de>
2009-03-18 14:44:12 +0000

    moved file addtionalBackendItems.php to newspaper/res/be

--- ext_localconf_addon.php
+++ ext_localconf_addon.php
@@ -9,7 +9,7 @@
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'EXT:newspaper/util/class.savehook.php:tx_newspaper_SaveHook';
 
 	// register hook to add javascript and css to BE (loaded to top)
-	$GLOBALS['TYPO3_CONF_VARS']['typo3/backend.php']['additionalBackendItems'][] = PATH_typo3conf . 'ext/newspaper/util/additionalBackendItems.php';
+	$GLOBALS['TYPO3_CONF_VARS']['typo3/backend.php']['additionalBackendItems'][] = PATH_typo3conf . 'ext/newspaper/res/be/additionalBackendItems.php';
 
 	// include newspaper classes and interfaces
 	require_once(PATH_typo3conf . 'ext/newspaper/tx_newspaper_include.php');
