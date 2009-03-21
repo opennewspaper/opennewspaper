502bb6651d3715f47efcf4fb6a07289454421dcf
Oliver Schröder <oliver@schroederbros.de>
2009-03-21 13:55:15 +0000

    module moderation list added - this time for real

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "newspaper"
 #
-# Auto generated 21-03-2009 14:32
+# Auto generated 21-03-2009 14:52
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
