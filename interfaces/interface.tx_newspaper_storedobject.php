e45587015a1033ae58eee81f8e45adea3bdbf4f4
Lene Preuss <lene.preuss@gmail.com>
2009-03-31 14:17:59 +0000

    added interface StoredObject to complete InSysFolder

---
+++ interfaces/interface.tx_newspaper_storedobject.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *  \file interface.tx_newspaper_extra.php
+ *
+ *  \author Helge Preuss <helge.preuss@gmail.com>
+ *  \date Feb 09, 2009
+ */
+
+/// A persistent object which is stored in the DB
+/** Almost all business objects belonging to the newspaper extension are stored
+ *  in Typo3 SysFolders. To find out which SysFolder an object belongs to, it
+ *  must implement the method getModuleName().
+ * 
+ *  Of course, every stored object resides in a DB table and has a UID. Getters
+ *  for DB table and UID and a setter for the UID must be provided.
+ *
+ *  Additional functionality is supplied for 
+ */
+ interface tx_newspaper_StoredObject {
+
+	/** \param $attribute_name Name of the requested attribute
+	 *	\return Value of the requested attribute
+	 */
+	public function getAttribute($attribute_name);
+	/** \param $attribute_name Name of the requested attribute
+	 *	\param $value New value of the requested attribute
+	 */
+	public function setAttribute($attribute_name, $value);
+
+	/// Write or overwrite Extra data in DB
+	/** Of course, a StoredObject needs a store() procedure ;-)
+	 *
+	 *  \return UID of stored record
+	 */
+	public function store();
+
+	/// \return UID of the object
+	public function getUid();
+
+	/// \param $uid new UID of the object
+	public function setUid($uid);
+	
+	/// \return Name of the database table the object's data are stored in
+	public function getTable();
+
+	/// \return (Internationalized) short description if the object type
+	/** This is (probably) used only in the BE, where the user needs to know
+	 *	which kind of object she is handling.
+	 */
+	public function getTitle();
+
+	/// \return String the name of the newspaper sysfolder
+	/** Needed to fill field "tx_newspaper_module" in table pages to find/create
+	 *  a storage folder for an Extra etc.
+	 *  Specification: np_* (max. 255 char); should not contain "phpunit"
+	 */
+	public static function getModuleName(); 
+
+}
+?>
