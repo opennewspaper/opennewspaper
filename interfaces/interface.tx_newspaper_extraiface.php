5b35836d85168492c2e9d71de1726adb59b5831c
Lene Preuss <lene.preuss@gmail.com>
2009-03-31 15:50:30 +0000

    renamed interface InSysFolder to StoredObject

--- interfaces/interface.tx_newspaper_extraiface.php
+++ interfaces/interface.tx_newspaper_extraiface.php
@@ -6,7 +6,7 @@
  *  \date Oct 27, 2008
  */
 
-require_once(PATH_typo3conf . 'ext/newspaper/interfaces/interface.tx_newspaper_insysfolder.php');
+require_once(PATH_typo3conf . 'ext/newspaper/interfaces/interface.tx_newspaper_storedobject.php');
 
 /// An Extra for Article s or Ressort s
 /** An Extra is, so to speak, a plugin for a displayable object.
