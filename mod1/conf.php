02158d3f570519228a96ebacb831a3e5dd06d823
Oliver Schröder <oliver@schroederbros.de>
2009-04-07 12:51:48 +0000

    back_path testing

--- mod1/conf.php
+++ mod1/conf.php
@@ -2,7 +2,7 @@
 
 	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
 define('TYPO3_MOD_PATH', '../typo3conf/ext/newspaper/mod1/');
-$BACK_PATH='../../../../typo3/';
+$BACK_PATH = PATH_typo3; // '../../../../typo3/';
 $MCONF['name']='txnewspaperMmain_txnewspaperM1';
 
 	
