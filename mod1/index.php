fb374525bd9509f333602df94d9b7472452a73e6
Oliver Schröder <oliver@schroederbros.de>
2009-04-08 11:30:08 +0000

    tx_newspaper_PageZone_Factory::getInstance()->createNew() call added to processActicatePageZoneType()

--- mod1/index.php
+++ mod1/index.php
@@ -191,10 +191,10 @@ t3lib_div::devlog('newspaper parseparam', 'newspaper', 0, $param);
 		require_once(t3lib_extMgm::extPath('newspaper'). 'classes/class.tx_newspaper_be.php');	
 		$param = $this->splitParams();
 t3lib_div::devlog('papzt param', 'newspaper', 0, $param);
-//		tx_newspaper_PageZone_Factory::getInstance()->createNew(
-//			new tx_newspaper_Page $page, 
-//			new tx_newspaper_PageZoneType $type
-//		);
+		tx_newspaper_PageZone_Factory::getInstance()->createNew(
+			new tx_newspaper_Page(intval($param['page'])), 
+			new tx_newspaper_PageZoneType(intval($param['pagezonetype']))
+		);
 		
 		$PA['row']['uid'] = $param['page']; // simulate call from be
 		$PA['AJAX_CALL'] = true;
