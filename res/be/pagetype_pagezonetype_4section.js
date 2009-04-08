69b9a84e2a0a118f48b31522d78aba1ca3ff25e6
Oliver Schröder <oliver@schroederbros.de>
2009-04-08 17:31:42 +0000

    added deletePage() and deletePageZone()

--- res/be/pagetype_pagezonetype_4section.js
+++ res/be/pagetype_pagezonetype_4section.js
@@ -54,6 +54,29 @@ path = path.substring(0, path.lastIndexOf("/") - 5); // -5 -> cut of "typo3"
 		);
 		document.getElementById('pagetype_pagezone').innerHTML = '<img src="' + path + 'typo3/gfx/spinner.gif"/>';
 	}
+
+	
+	function deletePage(section_id, pagetype_id, message) {
+		
+		if (message == undefined) {
+			alert('Illegal function call');
+			return;
+		}
+		
+		// user must confirm the he knows what he's doing
+		if (!confirm(message)) return; 
+		
+		/// ajax call: activate page type 
+		var request = new Ajax.Request(
+			path + "typo3conf/ext/newspaper/mod1/index.php",
+				{
+					method: 'get',
+					parameters: "delete_page&param=[section]" + section_id + "|[pagetype]" + pagetype_id + "&no_cache=" + new Date().getTime(),
+					onSuccess: updatePageTypePageZoneType
+				}
+		);
+		document.getElementById('pagetype_pagezone').innerHTML = '<img src="' + path + 'typo3/gfx/spinner.gif"/>';
+	}
 	
 
 	function activatePageZoneType(section_id, page_id, pagezone_type, message) {
@@ -79,6 +102,30 @@ path = path.substring(0, path.lastIndexOf("/") - 5); // -5 -> cut of "typo3"
 	}
 	
 	
+	function deletePageZone(section_id, page_id, pagezone_type, message) {
+		
+		if (message == undefined) {
+			alert('Illegal function call');
+			return;
+		}
+		
+		// user must confirm the he knows what he's doing
+		if (!confirm(message)) return; 
+		
+		/// ajax call: edit active page (zones) 
+		var request = new Ajax.Request(
+			path + "typo3conf/ext/newspaper/mod1/index.php",
+				{
+					method: 'get',
+					parameters: "delete_pagezone&param=[section]" + section_id + "|[page]" + page_id + "|[pagezonetype]" + pagezone_type + "&no_cache=" + new Date().getTime(),
+					onSuccess: updatePageTypePageZoneType
+				}
+		);
+		document.getElementById('pagetype_pagezone').innerHTML = '<img src="' + path + 'typo3/gfx/spinner.gif"/>';
+	}
+
+
+
 	
 	
 	
