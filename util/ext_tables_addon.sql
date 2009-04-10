4d667b6cf88b45f5d742836cd44acd7fe292fc17
Oliver Schröder <oliver@schroederbros.de>
2009-04-10 15:58:48 +0000

    added modifications for section, pages, pagezone_page and article

--- util/ext_tables_addon.sql
+++ util/ext_tables_addon.sql
@@ -1,2 +1,20 @@
 
+# Modifications for table 'tx_newspaper_section'
+CREATE TABLE tx_newspaper_section (
+	template_set tinytext NOT NULL
+);
 
+# Modifications for table 'tx_newspaper_page'
+CREATE TABLE tx_newspaper_page (
+	template_set tinytext NOT NULL
+);
+
+# Modifications for table 'tx_newspaper_pagezone_page'
+CREATE TABLE tx_newspaper_pagezone_page (
+	template_set tinytext NOT NULL
+);
+
+# Modifications for table 'tx_newspaper_article'
+CREATE TABLE tx_newspaper_article (
+	template_set tinytext NOT NULL
+);
