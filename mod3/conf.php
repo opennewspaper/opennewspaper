fdc458474a18203e3ea48b4079655dc4f4c53212
Oliver Schröder <oliver@schroederbros.de>
2009-04-14 15:56:23 +0000

    mod3/ added

---
+++ mod3/conf.php
@@ -0,0 +1,11 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
+$MCONF['name'] = 'txnewspaperMmain_txnewspaperM3';
+$MCONF['script'] = '_DISPATCH';
+	
+$MCONF['access'] = 'user,group';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:newspaper/mod3/locallang_mod.xml';
+?>
