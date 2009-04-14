ec81006c6dee78346f13915ae8194c1a202dc624
Oliver Schröder <oliver@schroederbros.de>
2009-04-14 15:54:57 +0000

    kickstarter

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "newspaper"
 #
-# Auto generated 14-04-2009 17:48
+# Auto generated 14-04-2009 17:54
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
