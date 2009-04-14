da79fc6f90affbf9babcdeb33ec9d7cc24464de6
Oliver Schröder <oliver@schroederbros.de>
2009-04-14 15:28:59 +0000

    test tca / range

--- tca_addon.php
+++ tca_addon.php
@@ -52,19 +52,19 @@ $TCA['tx_newspaper_extra_image']['columns']['extra_field']['config']['noTableWra
 // fix ranges artificially imposed by kickstarter
 /// \todo: check, if upper value can remain empty for "no upper limit"?"
 $TCA["tx_newspaper_page"]["columns"]["get_value"]["config"]["range"] = array (
-	"upper" => "1000000000",
+//	"upper" => "1000000000",
 	"lower" => "1"
 );
 $TCA["tx_newspaper_extra"]["columns"]["extra_uid"]["config"]["range"] = array (
-	"upper" => "1000000000",
+//	"upper" => "1000000000",
 	"lower" => "1"
 );
 $TCA["tx_newspaper_pagezone"]["columns"]["pagezone_uid"]["config"]["range"] = array (
-	"upper" => "1000000000",
+//	"upper" => "1000000000",
 	"lower" => "1"
 );
 $TCA["tx_newspaper_articlelist"]["columns"]["list_uid"]["config"]["range"] = array (
-	"upper" => "1000000000",
+//	"upper" => "1000000000",
 	"lower" => "1"
 );
 
