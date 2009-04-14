dba94a9b9b3ab7a364f04fd8da23ec0ecc60800a
Lene Preuss <lene.preuss@gmail.com>
2009-04-14 19:30:37 +0000

    typo

--- tests/class.Article_testcase.php
+++ tests/class.Article_testcase.php
@@ -110,8 +110,8 @@ class test_Article_testcase extends tx_phpunit_testcase {
 		if (is_array($extras)) foreach ($extras as $extra) {
 			$this->assertTrue($extra instanceof tx_newspaper_Extra);
 			$this->assertTrue($extra->getAttribute('uid') > 0);
-			$this->assertTrue($extra->getAttribute('extra_uid') == $extra->getExtraUid(), 
-							  "Attribute 'extra_uid' (" . $extra->getAttribute('extra_uid') . ") != getExtraUid() (" . $extra->getUid() . ")");
+			$this->assertTrue($extra->getAttribute('extra_uid') == $extra->getUid(), 
+							  "Attribute 'extra_uid' (" . $extra->getAttribute('extra_uid') . ") != getUid() (" . $extra->getUid() . ")");
 /*			test for PID disabled because $sf->getPid() is not consistent yet
  			$sf = tx_newspaper_Sysfolder::getInstance();
 			$this->assertTrue($extra->getAttribute('pid') == $sf->getPid($extra),
