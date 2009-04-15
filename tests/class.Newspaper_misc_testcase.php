bcca6e1f64bc35f6acdce51fa62b991ab861fef9
Lene Preuss <lene.preuss@gmail.com>
2009-04-15 09:42:59 +0000

    test whetehr template sets are returned correctly

--- tests/class.Newspaper_misc_testcase.php
+++ tests/class.Newspaper_misc_testcase.php
@@ -45,14 +45,19 @@ class test_Newspaper_misc_testcase extends tx_phpunit_testcase {
 	//	smarty
 	
 	public function test_getAvailableTemplateSets() {
-		$basepath = PATH_site . '/fileadmin/templates/newspaper/';
+		$basepath = PATH_site . 'fileadmin/templates/newspaper/';
 		
 		$template_sets_to_test = array('default', 'test_templateset');
 		foreach ($template_sets_to_test as $ts) {
-#			if (!file_exists($basepath . $ts)) mkdir ($basepath . $ts);
-			t3lib_div::debug($basepath . 'template_sets/' . $ts);
+			if (!file_exists($basepath . 'template_sets/' . $ts)) mkdir ($basepath . 'template_sets/' . $ts);
 		}
 		
+		$available_template_sets = tx_newspaper_Smarty::getAvailableTemplateSets();
+		foreach ($template_sets_to_test as $ts) {
+			if (!in_array($ts, $available_template_sets)) {
+				$this->fail("$ts should be in available template_sets, but isn't: " . print_r($available_template_sets, 1));
+			}
+		}
 				
 	}
 }
