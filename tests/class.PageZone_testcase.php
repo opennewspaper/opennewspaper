239f76f69254d430b2378fe1a2308289314258b9
Lene Preuss <lene.preuss@gmail.com>
2009-04-24 13:07:24 +0000

    create test date on your own

--- tests/class.PageZone_testcase.php
+++ tests/class.PageZone_testcase.php
@@ -11,8 +11,33 @@ require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_pagezone
 class test_PageZone_testcase extends tx_phpunit_testcase {
 
 	function setUp() {
-		$this->pagezone = tx_newspaper_PageZone_Factory::getInstance()->create($this->uid);
+		$this->uid = tx_newspaper::insertRows($this->pagezone_page_table, $this->pagezone_page_data);		
+		
+		$this->pagezone = new tx_newspaper_PageZone_Page($this->uid);
+		
+		$this->createExtras();
+		
 		$this->source = new tx_newspaper_DBSource();
+	}
+	
+	function tearDown() {
+		
+		$this->removeExtras();
+		
+		//	delete pagezone_papge
+		tx_newspaper::deleteRows($this->pagezone_page_table, 'uid = ' . $this->uid);
+
+		//	delete page zone entry for pagezone_page
+		tx_newspaper::deleteRows(
+			$this->pagezone_table,
+			'pagezone_table = \'' . $this->pagezone_page_table . '\' AND pagezone_uid = ' . $this->uid
+		);
+		
+		//	delete extra entry for pagezone_page
+		tx_newspaper::deleteRows(
+			$this->extra_table, 
+			'extra_table = \'' . $this->pagezone_page_table . '\' AND extra_uid = ' . $this->uid
+		);
 	}
 
 	public function test_createPageZone() {
@@ -104,10 +129,124 @@ class test_PageZone_testcase extends tx_phpunit_testcase {
 		// ...
 		t3lib_div::debug("finish me!");
 	}
-		
+
+	////////////////////////////////////////////////////////////////////////////
+
+	private function createExtras() {
+		foreach ($this->extra_data as $index => $extra) {
+			$extra_uid = tx_newspaper::insertRows($this->concrete_extra_table, $extra);
+	    	
+	    	$abstract_uid = tx_newspaper_Extra::createExtraRecord($extra_uid, $this->concrete_extra_table);
+	    	
+	    	///	link extra to article
+			tx_newspaper::insertRows(
+				$this->extra2pagezone_table,
+				array(
+					'uid_local' => $this->uid,
+					'uid_foreign' => $abstract_uid
+				));
+	    	
+	    	/// set position of extra
+	    	$row = array('position' => $this->extra_pos[$index]);
+			tx_newspaper::updateRows($this->extra_table, 'uid = ' . $abstract_uid, $row);
+		}	
+	}
+	
+	private function removeExtras() {
+		$rows = tx_newspaper::selectRows('uid_foreign', $this->extra2pagezone_table, 'uid_local = ' . $this->uid);
+		foreach ($rows as $row) {
+			$abstract_uid = $row['uid_foreign'];
+			$extra = tx_newspaper::selectOneRow('extra_uid, extra_table', $this->extra_table, 'uid = ' . $abstract_uid);
+			$concrete_uid = $extra['extra_uid'];
+			$this->assertEquals($extra['extra_table'], $this->concrete_extra_table);
+			
+			tx_newspaper::deleteRows($this->extra_table, array($abstract_uid));
+			tx_newspaper::deleteRows($this->extra2pagezone_table, 
+									 "uid_foreign = $abstract_uid AND uid_local = " . $this->uid);
+			tx_newspaper::deleteRows($this->concrete_extra_table, array($concrete_uid));
+		}
+	}
+	
+
+
 	private $bad_uid = 2000000000;			///< pagezone that does not exist
 	private $pagezone = null;				///< the object
 	private $source = null;
 	private $uid = 1;
+	
+	private $extra_table = 'tx_newspaper_extra';
+	private $concrete_extra_table = 'tx_newspaper_extra_image';
+	private $extra2pagezone_table = 'tx_newspaper_pagezone_page_extras_mm';
+	private $pagezone_table = 'tx_newspaper_pagezone';
+	private $pagezone_page_table = 'tx_newspaper_pagezone_page';
+
+	private $pagezone_page_data = array(
+		'pid'		=> '2476',
+		'tstamp'	=> '1233326263',
+		'crdate'	=> '1232376462', 		  	
+		'cruser_id'	=> '1',
+		'sorting'	=> '256',
+		'deleted'	=> '0',
+		'pagezonetype_id' => '2',
+		'pagezone_id' => 'X',
+		'extras'	=> '3',
+		'template_set' => '',
+		'inherits_from' => '0'
+	);
+	
+	private $extra_data = array(
+		array(
+			'pid' => 2573,
+			'tstamp' => 1234806796,
+			'crdate' => 1232647355,
+			'cruser_id' => 1,
+			'deleted' => 0,
+			'hidden' => 0,
+			'starttime' => 0,
+			'endtime' => 0,
+			'fe_group' => 0,
+			'extra_field' => "",	
+			'title' => "Image 3",
+			'image' => "E3_033009T.jpg",	
+			'caption' => "Caption for image 3",	
+			'template_set' => "",	
+		),
+		array(
+			'pid' => 2573,
+			'tstamp' => 1234806796,
+			'crdate' => 1232647355,
+			'cruser_id' => 1,
+			'deleted' => 0,
+			'hidden' => 0,
+			'starttime' => 0,
+			'endtime' => 0,
+			'fe_group' => 0,
+			'extra_field' => "",	
+			'title' => "Image 4",	
+			'image' => "120px-GentooFreeBSD-logo.svg_02.png",	
+			'caption' => "Daemonic Gentoo",	
+			'template_set' => "",	
+		),
+		array(
+			'pid' => 2573,
+			'tstamp' => 1234806796,
+			'crdate' => 1232647355,
+			'cruser_id' => 1,
+			'deleted' => 0,
+			'hidden' => 0,
+			'starttime' => 0,
+			'endtime' => 0,
+			'fe_group' => 0,
+			'extra_field' => "extra_field[5]",	
+			'title' => "title[5]",	
+			'image' => "lolcatsdotcomoh5o6d9hdjcawys6.jpg",	
+			'caption' => "caption[5]",	
+			'template_set' => "",	
+		),
+	);
+	
+	private $extra_pos = array(
+		1024, 2048, 4096
+	);
 }
 ?>
