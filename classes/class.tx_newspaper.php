799370a4c9851310d8e072f636ae307decad217f
Lene Preuss <lene.preuss@gmail.com>
2009-04-28 14:49:40 +0000

    debug++

--- classes/class.tx_newspaper.php
+++ classes/class.tx_newspaper.php
@@ -192,7 +192,8 @@ t3lib_div::devlog('tx_newspaper->renderList pa', 'newspaper', 0, $PA);
 					$new_id => $row
 				)
 			);
-			
+			t3lib_div::debug($datamap);
+			die();
 			/// Process the datamap, inserting a new record into $table
 			$tce = t3lib_div::makeInstance('t3lib_TCEmain');
 			$tce->start($datamap, null);
