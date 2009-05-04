fb032f7e995a58c1175f6dfcf05dc44485cd463b
Oliver Schröder <oliver@schroederbros.de>
2009-05-04 13:45:33 +0000

    moved addAdditionalScriptToBackend() to tx_newspaper_BE

--- res/be/additionalBackendItems.php
+++ res/be/additionalBackendItems.php
@@ -4,8 +4,8 @@
 		die ('Access denied.');
 
 	if (TYPO3_MODE == 'BE') {
-		require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_extra_be.php');
-		tx_newspaper_ExtraBE::addAdditionalScriptToBackend(); // add additional script (if needed) to top of backend
+		require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_be.php');
+		tx_newspaper_BE::addAdditionalScriptToBackend(); // add additional script (if needed) to top of backend
 	}
 
 ?>
