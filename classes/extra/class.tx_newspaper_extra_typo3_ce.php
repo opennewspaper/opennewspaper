64c6201b66cb9252fbcae72d4430e70aa88c1f75
Lene Preuss <lene.preuss@gmail.com>
2009-05-07 18:00:53 +0000

    debug++

--- classes/extra/class.tx_newspaper_extra_typo3_ce.php
+++ classes/extra/class.tx_newspaper_extra_typo3_ce.php
@@ -36,7 +36,7 @@ class tx_newspaper_Extra_Typo3_CE extends tx_newspaper_Extra {
 			'dontCheckPid' => 1
 		);
 		$cObj = t3lib_div::makeInstance('tslib_cObj'); 
-		return $cObj->RECORDS($tt_content_conf); 		
+		return 'tx_newspaper_Extra_Typo3_CE::render(): ' . $cObj->RECORDS($tt_content_conf); 		
 	}
 
 //TODO: getLLL
