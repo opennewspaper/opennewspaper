c465d2fddace9ff0c336cfa93776b37b296b14b1
Lene Preuss <lene.preuss@gmail.com>
2009-05-07 12:27:12 +0000

    auto-fixed changes from kickstarter

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "newspaper"
 #
-# Auto generated 07-05-2009 13:41
+# Auto generated 07-05-2009 14:26
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
