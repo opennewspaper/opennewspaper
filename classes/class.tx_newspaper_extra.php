4b6e076ce7a0844770d3a44911eddde4eac68f61
Lene Preuss <lene.preuss@gmail.com>
2009-05-08 17:00:04 +0000

    debug++

--- classes/class.tx_newspaper_extra.php
+++ classes/class.tx_newspaper_extra.php
@@ -104,7 +104,7 @@ abstract class tx_newspaper_Extra implements tx_newspaper_ExtraIface {
 	 		return $this->extra_attributes[$attribute];
  		}
 
-        throw new tx_newspaper_WrongAttributeException($attribute);
+        throw new tx_newspaper_WrongAttributeException($attribute . print_r($this->attributes, 1).print_r($this->extra_attributes, 1));
 	}
 
 	/** No tx_newspaper_WrongAttributeException here. We want to be able to set
