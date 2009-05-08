527915d6428076784c20e744abd2881d2db44c6b
Lene Preuss <lene.preuss@gmail.com>
2009-05-08 11:39:00 +0000

    debug++

--- classes/class.tx_newspaper_pagezone.php
+++ classes/class.tx_newspaper_pagezone.php
@@ -392,6 +392,7 @@ abstract class tx_newspaper_PageZone implements tx_newspaper_ExtraIface {
 	 */ 
 	public function insertExtraAfter(tx_newspaper_Extra $insert_extra,
 									 $origin_uid = 0, $recursive = true) {
+		t3lib_div::devlog('insertExtraAfter('.$insert_extra.', '.$origin_uid.', '.intval($recursive).')', 'newspaper', '', $this);
 		$insert_extra->setAttribute('position', $this->getInsertPosition($origin_uid));
 		
 		/// Write Extra to DB
