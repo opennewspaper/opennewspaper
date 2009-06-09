ad94aab81ab48914b11429af5c6b1bfef00269ac
Lene Preuss <lene.preuss@gmail.com>
2009-06-09 11:29:59 +0000

    added mod5

---
+++ mod5/conf.php
@@ -0,0 +1,11 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
+$MCONF['name'] = 'web_txnewspaperM5';
+$MCONF['script'] = '_DISPATCH';
+	
+$MCONF['access'] = 'user,group';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:newspaper/mod5/locallang_mod.xml';
+?>
