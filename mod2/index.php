a8643c70170599412ce5d049efe1d5e1fe03ef0f
Oliver Schröder <oliver@schroederbros.de>
2009-06-11 13:28:01 +0000

    show mod to non-admin users

--- mod2/index.php
+++ mod2/index.php
@@ -73,6 +73,9 @@ class  tx_newspaper_module2 extends t3lib_SCbase {
 	function main()	{
 		global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
 
+		// a valid page for permissions check is needed - use newspaper root folder
+		$this->id = tx_newspaper_Sysfolder::getInstance()->getPidRootfolder(); 
+
 		// Access check!
 		// The page will show only if there is a valid page and if this page may be viewed by the user
 		$this->pageinfo = t3lib_BEfunc::readPageAccess($this->id,$this->perms_clause);
