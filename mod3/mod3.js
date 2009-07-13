1cca561682efa97bfe239989d02e0b5e5744fd17
Oliver Schröder <oliver@schroederbros.de>
2009-07-13 09:09:20 +0000

    moved js and css from mod3.tmpl to mod3*.js and mod3.css files

---
+++ mod3/mod3.js
@@ -0,0 +1,182 @@
+<script language="javascript">
+
+	function extra_save_para(extra_uid, value) {ldelim}
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_save_para=1&extra_uid=" + extra_uid + "&value=" + value + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+	
+	function toggle_show_levels_above(checked) {ldelim}
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "toggle_show_levels_above=1&checked=" + checked + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+
+	function extra_insert_after_dummy(origin_uid, pagezone_uid) {ldelim}
+/// \todo: remove after testing
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_insert_after_dummy=1&origin_uid=" + origin_uid + "&pz_uid=" + pagezone_uid + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+
+	function extra_move_after(origin_uid, pz_uid, extra_uid) {ldelim}
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_move_after=1&origin_uid=" + origin_uid + "&pz_uid=" + pz_uid + "&extra_uid=" + extra_uid + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+	function extra_delete(pz_uid, extra_uid, message) {ldelim}
+		// user must confirm that he knows what he's doing
+		if (!confirm(message)) return;
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_delete=1&pz_uid=" + pz_uid + "&extra_uid=" + extra_uid + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+	
+	function extra_set_show(extra_uid, show) {ldelim}
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_set_show=1&extra_uid=" + extra_uid + "&show=" + show + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+	function extra_set_pass_down(pz_uid, extra_uid, pass_down) {ldelim}
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_set_pass_down=1&pz_uid=" + pz_uid + "&extra_uid=" + extra_uid + "&pass_down=" + pass_down + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+	function page_type_change(pt_uid) {ldelim}
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_page_type_change=1&pt_uid=" + pt_uid + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+	function pagezone_type_change(pzt_uid) {ldelim}
+		var request = new top.Ajax.Request(
+ 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
+ 			{ldelim}
+				method: 'get',
+				parameters: "extra_pagezone_type_change=1&pzt_uid=" + pzt_uid + "&no_cache=" + new Date().getTime(),
+				onCreate: processing,
+				onSuccess: reload
+			{rdelim}
+		);
+	{rdelim}
+
+
+	function subModalExtraInsertAfter(origin_uid, pz_uid, paragraph) {ldelim}
+		var width = Math.min(700, top.getViewportWidth() - 100); 
+		var height = top.getViewportHeight() - 50;
+/// \todo:  param for close still missing
+		top.showPopWin(
+			top.path + "typo3conf/ext/newspaper/mod3/index.php?chose_extra=1&origin_uid=" + origin_uid + "&pz_uid=" + pz_uid + "&paragraph=" + paragraph + "&returnUrl=" + top.path + "typo3conf/ext/newspaper/mod3/close.html",
+			width, 
+			height, 
+			null, 
+			true
+		);
+	{rdelim}
+	function extra_insert_after(origin_uid, pz_uid, paragraph) {ldelim}
+/// \todo: add be_mode
+		subModalExtraInsertAfter(origin_uid, pz_uid, paragraph);
+	{rdelim}
+
+
+	function subModalExtraEdit(table, uid) {ldelim}
+		var width = Math.min(700, top.getViewportWidth() - 100); 
+		var height = top.getViewportHeight() - 50;
+		top.showPopWin(
+			top.path + "typo3/alt_doc.php?returnUrl=" + top.path + "typo3conf/ext/newspaper/mod3/close.html&edit[" + table + "][" + uid + "]=edit",
+			width, 
+			height, 
+			null, 
+			true
+		);
+	{rdelim}
+	function extra_edit(table, uid) {ldelim}
+/// \todo: add be_mode
+		subModalExtraEdit(table, uid);
+	{rdelim}
+
+
+	function processing() {ldelim}
+
+		img = document.createElement('img');
+		img_src = document.createAttribute('src');
+		img_src.nodeValue = top.path + 'typo3/gfx/spinner.gif';
+		img.setAttributeNode(img_src);
+
+		layer_style = document.createAttribute('style');
+		layer_style.nodeValue = 'color: red; position: absolute; z-index: 99; text-align: center; background: rgba(208, 208, 208, 0.8) none repeat scroll 0 0; top: 0; bottom: 0; left: 0; right: 0; padding-top: 33%;';
+
+		layer = document.createElement('div');
+		layer.setAttributeNode(layer_style);
+
+		layer.appendChild(img);
+		self.document.getElementsByTagName('body')[0].appendChild(layer);
+	{rdelim}
+	
+	function reload(data) {ldelim}
+//alert(data);
+		if (data.responseText == '') {ldelim}
+			// empty string, so no error message
+/// \todo: currently no_cache param are ADDED, must be REPLACED
+			self.location.href = self.location.href + "&no_cache=" + new Date().getTime();
+		{rdelim} else {ldelim}
+			// display error message
+			self.document.getElementsByTagName('body')[0].innerHTML = data.responseText;
+		{rdelim}
+	{rdelim}
+
+</script>
