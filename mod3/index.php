1c832ef4f0250426ada411d3e6b76aa7ba67c028
Lene Preuss <lene.preuss@gmail.com>
2009-07-17 12:28:43 +0000

    debug++

--- mod3/index.php
+++ mod3/index.php
@@ -41,7 +41,7 @@ require_once(PATH_t3lib . 'class.t3lib_scbase.php');
 $BE_USER->modAccess($MCONF,1);	// This checks permissions and exits if the users has no permission for entry.
 	// DEFAULT initialization of a module [END]
 
-define('DEBUG_POSITION', false);
+define('DEBUG_POSITION', true);
 
 /**
  * Module 'Placement' for the 'newspaper' extension.
