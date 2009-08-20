c5a070b30d78ffb0153dd4328400be2fa4b8b80b
Lene Preuss <lene.preuss@gmail.com>
2009-08-20 12:37:05 +0000

    debug--

--- classes/class.tx_newspaper_extra.php
+++ classes/class.tx_newspaper_extra.php
@@ -291,8 +291,6 @@ abstract class tx_newspaper_Extra implements tx_newspaper_ExtraIface {
 	
 	/// \return array with (registered) Extra objects
 	static public function getRegisteredExtras() {
-		foreach(self::$registeredExtra as $extra) t3lib_div::devlog('registeredExtra', 'newspaper', 1, get_class($extra));
-			
 		return self::$registeredExtra;
 	}
 	
