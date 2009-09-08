b735fe0107dfd3474ea80d7db8c2ecfd757ea296
Lene Preuss <lene.preuss@gmail.com>
2009-09-08 11:31:51 +0000

    added mod6, Control Tags

---
+++ mod6/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', 'ext/newspaper/mod6/');
+$BACK_PATH='../../../';
+$MCONF['name']='txnewspaperM6';
+
+	
+$MCONF['access']='user,group';
+$MCONF['script']='index.php';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:newspaper/mod6/locallang_mod.xml';
+?>
