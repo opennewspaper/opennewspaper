8447e2362255da0fb584f267488917ee7f5faead
Lene Preuss <lene.preuss@gmail.com>
2009-09-25 15:10:27 +0000

    auto-fixed changes from kickstarter

--- tca.php
+++ tca.php
@@ -2532,4 +2532,5 @@ $TCA["tx_newspaper_extra_combolinkbox"] = array (
 	)
 );
 require_once(PATH_typo3conf . 'ext/newspaper/tca_addon.php');
+require_once(PATH_typo3conf . 'ext/newspaper_taz/tca_addon.php');
 ?>
