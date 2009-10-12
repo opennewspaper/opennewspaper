e78c0817421466e5a2573d26f119a214c6680248
Lene Preuss <lene.preuss@gmail.com>
2009-10-12 14:19:37 +0000

    auto-fixed changes from kickstarter

--- ext_tables.sql
+++ ext_tables.sql
@@ -1303,3 +1303,28 @@ CREATE TABLE tx_newspaper_article (
 CREATE TABLE tx_newspaper_articlelist_semiautomatic_articles_mm (
 	offset int(11) DEFAULT '0' NOT NULL
 );
+
+# Modifications for table 'tx_newspaper_section'
+CREATE TABLE tx_newspaper_section (
+	template_set tinytext NOT NULL
+);
+
+# Modifications for table 'tx_newspaper_page'
+CREATE TABLE tx_newspaper_page (
+	template_set tinytext NOT NULL
+);
+
+# Modifications for table 'tx_newspaper_pagezone_page'
+CREATE TABLE tx_newspaper_pagezone_page (
+	template_set tinytext NOT NULL
+);
+
+# Modifications for table 'tx_newspaper_article'
+CREATE TABLE tx_newspaper_article (
+	template_set tinytext NOT NULL
+);
+
+# Modifications for table 'tx_newspaper_articlelist_semiautomatic_articles_mm'
+CREATE TABLE tx_newspaper_articlelist_semiautomatic_articles_mm (
+	offset int(11) DEFAULT '0' NOT NULL
+);
