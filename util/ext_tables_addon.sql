f283159461d9934e699f1110bb82921ced3e66bb
Lene Preuss <lene.preuss@gmail.com>
2009-10-15 10:23:27 +0000

    create FULLTEXT indices on searchable tables

--- util/ext_tables_addon.sql
+++ util/ext_tables_addon.sql
@@ -23,3 +23,24 @@ CREATE TABLE tx_newspaper_article (
 CREATE TABLE tx_newspaper_articlelist_semiautomatic_articles_mm (
 	offset int(11) DEFAULT '0' NOT NULL
 );
+
+# Fulltext indices for articles
+CREATE TABLE `tx_newspaper_article` (
+  FULLTEXT KEY `title` (`title`, `kicker`, `title_list`, `kicker_list`)
+  FULLTEXT KEY `text` (`teaser`, `teaser_list`, `text`, `author`)
+);
+
+# Fulltext index for Extra: Image
+CREATE TABLE `tx_newspaper_extra_image` (
+  FULLTEXT KEY `title` (`title`,`kicker`,`caption`)
+);
+
+# Fulltext index for Extra: Textbox
+CREATE TABLE `tx_newspaper_extra_textbox` (
+  FULLTEXT KEY `title` (`title`,`text`)
+);
+
+# Fulltext index for Extra: Image
+CREATE TABLE `tx_newspaper_extra_bio` (
+  FULLTEXT KEY `title` (`author_name`, `bio_text`)
+);
