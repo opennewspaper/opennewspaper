28e83d1e293f93d21554cf3a3fe5a148af6f6276
Oliver Schröder <oliver@schroederbros.de>
2009-10-26 13:09:48 +0000

    corrected TYPO3_MOD_PATH

--- mod7/conf.php
+++ mod7/conf.php
@@ -1,7 +1,7 @@
 <?php
 
 	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
-define('TYPO3_MOD_PATH', 'ext/newspaper/mod7/');
+define('TYPO3_MOD_PATH', '../typo3conf/ext/newspaper/mod7/');
 $BACK_PATH = '../../../../typo3/';
 $MCONF['name']='txnewspaperMmain_txnewspaperM7';
 
