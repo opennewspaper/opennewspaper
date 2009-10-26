f7206bd21ef6ba03c51ad0aed18aa37630e23f0f
Oliver Schröder <oliver@schroederbros.de>
2009-10-26 10:00:19 +0000

    copied from segfault .... svn :-(

---
+++ mod7/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', 'ext/newspaper/mod7/');
+$BACK_PATH='../../../';
+$MCONF['name']='web_txnewspaperM7';
+
+	
+$MCONF['access']='user,group';
+$MCONF['script']='index.php';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:newspaper/mod7/locallang_mod.xml';
+?>
