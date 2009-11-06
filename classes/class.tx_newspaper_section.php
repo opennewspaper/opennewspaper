cf62ac4f9c47ed033d38c8d41f5aa317f8172b10
Lene Preuss <lene.preuss@gmail.com>
2009-11-06 14:56:43 +0000

    extract excluded section UIDs from TSConfig

--- classes/class.tx_newspaper_section.php
+++ classes/class.tx_newspaper_section.php
@@ -366,10 +366,9 @@ class tx_newspaper_Section implements tx_newspaper_StoredObject {
 	 *  \return tx_newspaper_Section objects in the DB.
 	 */
 	public static function getAllSections($sort_by = 'sorting') {
-		global $TYPO3_CONF_VARS;
-		t3lib_div::devlog('BE', 'newspaper', 0, $TYPO3_CONF_VARS['BE']);
 		$TSConfig = t3lib_BEfunc::getPagesTSconfig(0);
-		t3lib_div::devlog('TSConfig', 'newspaper', 0, $TSConfig);
+		$excluded = $TSConfig['newspaper']['excluded_sections'];
+		t3lib_div::devlog('excluded', 'newspaper', 0, $excluded);
 		
 		$pid = tx_newspaper_Sysfolder::getInstance()->getPid(new tx_newspaper_Section());
 		$row = tx_newspaper::selectRows(
