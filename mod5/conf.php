6dcdf492647bf67a60aae700bb1c79eb56911301
Oliver Schröder <oliver@schroederbros.de>
2009-11-10 10:10:28 +0000

    conf changed (using kickstarter 0.3.8 version now)

--- mod5/conf.php
+++ mod5/conf.php
@@ -1,10 +1,13 @@
 <?php
 
-	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
+// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/newspaper/mod5/');
+$BACK_PATH = '../../../../typo3/';
 $MCONF['name'] = 'txnewspaperMmain_txnewspaperM5';
-$MCONF['script'] = '_DISPATCH';
+
 	
 $MCONF['access'] = 'user,group';
+$MCONF['script'] = 'index.php';
 
 $MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
 $MLANG['default']['ll_ref'] = 'LLL:EXT:newspaper/mod5/locallang_mod.xml';
