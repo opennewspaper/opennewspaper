8902704237a0861171e42998c07bb0189b603109
Lene Preuss <lene.preuss@gmail.com>
2009-11-20 13:59:56 +0000

    debug++

--- classes/class.tx_newspaper.php
+++ classes/class.tx_newspaper.php
@@ -814,9 +814,16 @@ Time: ' . date('Y-m-d H:i:s') . ', Timestamp: ' . time() . ', be_user: ' .  $GLO
 		global $BACK_PATH;
 	
 		t3lib_div::devlog('$PA', 'newspaper', 0, $PA);	
+		
+		$out = '';
+		
+		$out .= '$PA = ' . str_replace(' ', '&nbsp;', str_replace("\n", "<br />\n", print_r($PA, 1)));
+		
 		$row = tx_newspaper::selectOneRow('list_table, list_uid', 'tx_newspaper_articlelist', 'uid = ' . intval($PA['row']['uid']));
 		$table = $row['list_table'];
 		$uid = $row['list_uid'];
+
+		$out .= '$row = ' . str_replace(' ', '&nbsp;', str_replace("\n", "<br />\n", print_r($row, 1)));
 		
 		$tce = t3lib_div::makeInstance('t3lib_TCEforms');
 		$tce->backPath = $BACK_PATH;
