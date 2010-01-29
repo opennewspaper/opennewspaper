8e25c6c49701b926fd66758de1fde78f8b61e0d8
Oliver Schröder <oliver@schroederbros.de>
2010-01-29 15:34:59 +0000

    initial upload with dummy function rendeBackend()

---
+++ classes/class.tx_newspaper_workflowlog.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Created on 29.01.2010
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+ 
+class tx_newspaper_WorkflowLog {   
+
+	public static function renderBackend($table, $uid) {
+		return 'Hello newspaper!';
+	}
+ 
+
+}
+ 
+?>
