d9b1931833a0c4df5e4a9a2bd0e92cc4e81d2d6a
Lene Preuss <lene.preuss@gmail.com>
2010-02-03 12:26:12 +0000

    typo

--- classes/class.tx_newspaper_pagezone.php
+++ classes/class.tx_newspaper_pagezone.php
@@ -1034,12 +1034,7 @@ abstract class tx_newspaper_PageZone implements tx_newspaper_ExtraIface {
      *        inheritance mode has been set to false
      */
     public function getExtras($hidden_too = false) {
-        t3lib_div::devlog('getExtras('.intval($hidden_too).')', 'newspaper', 0, array(
-            'pagezone_uid' => $this->getUid(),
-            '$origin_uid' => $origin_uid, 
-            '$hidden_too' => intval($hidden_too),
-            'getExtras()' => $this->getExtras($hidden_too))
-        );
+        t3lib_div::devlog('getExtras('.intval($hidden_too).')', 'newspaper', 0);
     	if (!$this->extras) {
             $this->readExtras($this->getUid(), $hidden_too);
         }
