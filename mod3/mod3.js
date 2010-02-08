163d8f6822d455cfb0bbf6f349c943ffaa8d0c38
Oliver Schröder <oliver@schroederbros.de>
2010-02-08 16:08:56 +0000

    using {literal} -> code more readable because {ldelim}/{rdelim} isn't needed anymore

--- mod3/mod3.js
+++ mod3/mod3.js
@@ -1,3 +1,4 @@
+{literal}
 <script language="javascript">
 
 
@@ -10,72 +11,72 @@
 
 
 // add javascript and css if not available (backend called in popup WITHOUT typo3 frameset where these scripts are loaded to)
-t3BackendObject = getTypo3BackendObject();
-if (t3BackendObject != top) {ldelim}
+var t3BackendObject = getTypo3BackendObject();
+if (t3BackendObject != top) {
 	loadJsCssFile(t3BackendObject.path + "typo3conf/ext/newspaper/contrib/subModal/common.js", "js");
 	loadJsCssFile(t3BackendObject.path + "typo3conf/ext/newspaper/contrib/subModal/subModal.js", "js");
 	loadJsCssFile(t3BackendObject.path + "typo3conf/ext/newspaper/res/be/extra/util.js", "js");
 	loadJsCssFile(t3BackendObject.path + "typo3conf/ext/newspaper/res/be/util.js", "js");
 	loadJsCssFile(t3BackendObject.path + "typo3conf/ext/newspaper/contrib/subModal/subModal.css", "css");
-{rdelim}
+}
 
 //http://www.javascriptkit.com/javatutors/loadjavascriptcss.shtml
-function loadJsCssFile(filename, filetype) {ldelim}
-	if (filetype == "js") {ldelim} //if filename is a external JavaScript file
+function loadJsCssFile(filename, filetype) {
+	if (filetype == "js") { //if filename is a external JavaScript file
 		var fileref = document.createElement('script');
 		fileref.setAttribute("type", "text/javascript");
 		fileref.setAttribute("src", filename);
-	{rdelim} else if (filetype == "css") {ldelim} //if filename is an external CSS file
+	} else if (filetype == "css") { //if filename is an external CSS file
 		var fileref = document.createElement("link");
 		fileref.setAttribute("rel", "stylesheet");
 		fileref.setAttribute("type", "text/css");
 		fileref.setAttribute("href", filename);
-	{rdelim}
-	if (typeof fileref != "undefined") {ldelim}
+	}
+	if (typeof fileref != "undefined") {
 		document.getElementsByTagName("head")[0].appendChild(fileref);
-	{rdelim}
-{rdelim}
+	}
+}
 
 
 
 /// utility functions //////////////////////////////////////////////////////////
 
 	// returns object containing getViewportWidth() etc. functions (or returns false if not available)
-	function getTypo3BackendObject() {ldelim}
-		if (typeof top.getViewportWidth == 'function') {ldelim}
+	function getTypo3BackendObject() {
+		if (typeof top.getViewportWidth == 'function') {
 			return top; // called in "normal" typo3 backend
-		{rdelim}
-		if (typeof opener.top.getViewportWidth == 'function') {ldelim}
+		}
+		if (typeof opener.top.getViewportWidth == 'function') {
 			return opener.top; // called in standalone popup
-		{rdelim}
+		}
 		return false; // no reference could be found
-	{rdelim}
+	}
 	
 
 	/// returns the reload type for ajax call depending on the flag "is_concrete_article"
-	function get_onSuccess_function(is_concrete_article) {ldelim}
+	function get_onSuccess_function(is_concrete_article) {
 		if (is_concrete_article)
 			return 'response_in_article';
 		else
 			return 'reload_page';
-	{rdelim}
+	}
 	/// returns the "processing spinner" type for ajax call depending on the flag "is_concrete_article"
-	function get_onCreate_function(is_concrete_article) {ldelim}
+	function get_onCreate_function(is_concrete_article) {
 		if (is_concrete_article)
 			return 'processing_in_article';
 		else
 			return 'processing_page';
-	{rdelim}
+	}
 
 /// \todo: use this function in all ajax calls
-	function modify_no_cache_param(url) {ldelim}
+	function modify_no_cache_param(url) {
 		/// \todo: currently no_cache param are ADDED, must be REPLACED
 		return url + "&no_cache=" + new Date().getTime();
-	{rdelim}
+	}
 
 	
 	/// "processing spinner" over whole page
-	function processing_page() {ldelim}
+	function processing_page() {
 		img = document.createElement('img');
 		img_src = document.createAttribute('src');
 		img_src.nodeValue = top.path + 'typo3/gfx/spinner.gif';
@@ -86,46 +87,46 @@ function loadJsCssFile(filename, filetype) {ldelim}
 		layer.setAttributeNode(layer_style);
 		layer.appendChild(img);
 		self.document.getElementsByTagName('body')[0].appendChild(layer);
-	{rdelim}
+	}
 	
-	function processing_in_article() {ldelim}
+	function processing_in_article() {
 		document.getElementById('extras').innerHTML = '<img src="' + top.path + 'typo3/gfx/spinner.gif"/>';
-	{rdelim}
+	}
 
 	
 	/// direct reload (no popup/modalbox involved ...)
-	function reload_page(data) {ldelim}
-		if (data.responseText == '') {ldelim}
+	function reload_page(data) {
+		if (data.responseText == '') {
 			// empty string, so no error message
 			self.location.href = modify_no_cache_param(self.location.href);
-		{rdelim} else {ldelim}
+		} else {
 			// display error message
 			self.document.getElementsByTagName('body')[0].innerHTML = data.responseText;
-		{rdelim}
-	{rdelim}
+		}
+	}
 
 	/// just display the ajax response as extra list in concrete article
-	function response_in_article(data) {ldelim}
+	function response_in_article(data) {
 		document.getElementById('extras').innerHTML = data.responseText;
-	{rdelim}
+	}
 	
 	/// the list of extras has to be reloaded from the server (needed for modal box or saveField ajax calls)
-	function reload_in_article(pz_uid) {ldelim}
-		if (t3BackendObject == top) {ldelim}
+	function reload_in_article(pz_uid) {
+		if (t3BackendObject == top) {
 			ref = top.content.list_frame
-		{rdelim} else {ldelim}
+		} else {
 			ref = top;
-		{rdelim}
+		}
 		var request = new top.Ajax.Request(
 			t3BackendObject.path + "typo3conf/ext/newspaper/mod3/index.php",
-			{ldelim}
+			{
 				method: 'get',
 				parameters: "reload_extra_in_concrete_article=1&pz_uid=" + pz_uid + "&no_cache=" + new Date().getTime(),
 				onCreate: ref.processing_in_article,
 				onSuccess: response_in_article
-			{rdelim}
+			}
 		);
-	{rdelim}	
+	}	
 		
 	
 	
@@ -134,17 +135,17 @@ function loadJsCssFile(filename, filetype) {ldelim}
 /// functions for placement module only ////////////////////////////////////////
 	
 	/// toggle checkbox "Show levels above"
-	function toggle_show_levels_above(checked) {ldelim}
+	function toggle_show_levels_above(checked) {
 		var request = new top.Ajax.Request(
 				top.path + "typo3conf/ext/newspaper/mod3/index.php",
-				{ldelim}
+				{
 				method: 'get',
 				parameters: "toggle_show_levels_above=1&checked=" + checked + "&no_cache=" + new Date().getTime(),
 				onCreate: processing,
 				onSuccess: reload
-			{rdelim}
+			}
 		);
-	{rdelim}	
+	}	
 	
 	
 
@@ -159,17 +160,17 @@ function loadJsCssFile(filename, filetype) {ldelim}
 /// functions for placement (default) article //////////////////////////////////	
 		
 	/// AJAX call: create extra on article, started by shortcut link
-	function extra_shortcut_create(article_uid, extra_class, extra_uid) {ldelim}
+	function extra_shortcut_create(article_uid, extra_class, extra_uid) {
 		var request = new top.Ajax.Request(
 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
-			{ldelim}
+			{
 				method: 'get',
 				parameters: "extra_shortcut_create=1&article_uid=" + article_uid + "&extra_class=" + extra_class + "&extra_uid=" + extra_uid + "&no_cache=" + new Date().getTime(),
 				onCreate: eval(get_onCreate_function(1)),
 				onSuccess: eval(get_onSuccess_function(1))
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 		
 		
 		
@@ -178,58 +179,58 @@ function loadJsCssFile(filename, filetype) {ldelim}
 /// functions for placement article AND concrete article ///////////////////////
 	
 	/// AJAX call: delete extra on pagezone_page or article
-	function extra_delete(pz_uid, extra_uid, message, is_concrete_article) {ldelim}
+	function extra_delete(pz_uid, extra_uid, message, is_concrete_article) {
 		if (!confirm(message)) return; // user must confirm that he knows what he's doing
 		var request = new top.Ajax.Request(
 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
-			{ldelim}
+			{
 				method: 'get',
 				parameters: "extra_delete=1&pz_uid=" + pz_uid + "&extra_uid=" + extra_uid + "&no_cache=" + new Date().getTime(),
 				onCreate: eval(get_onCreate_function(is_concrete_article)),
 				onSuccess: eval(get_onSuccess_function(is_concrete_article))
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 	
 	/// AJAX call: move extra on pagezone_page or article
-	function extra_move_after(origin_uid, pz_uid, extra_uid, is_concrete_article) {ldelim}
+	function extra_move_after(origin_uid, pz_uid, extra_uid, is_concrete_article) {
 //alert(top.path + "typo3conf/ext/newspaper/mod3/index.php");
 	var request = new top.Ajax.Request(
 				top.path + "typo3conf/ext/newspaper/mod3/index.php",
-				{ldelim}
+				{
 				method: 'get',
 				parameters: "extra_move_after=1&origin_uid=" + origin_uid + "&pz_uid=" + pz_uid + "&extra_uid=" + extra_uid + "&no_cache=" + new Date().getTime(),
 				onCreate: eval(get_onCreate_function(is_concrete_article)),
 				onSuccess: eval(get_onSuccess_function(is_concrete_article))
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 	
 	/// prepare AJAX call in modal box: edit extra on pagezone_page or article
-	function extra_edit(table, uid, pz_uid, is_concrete_article) {ldelim}
+	function extra_edit(table, uid, pz_uid, is_concrete_article) {
 /// \todo: add be_mode
 			subModalExtraEdit(table, uid, pz_uid, is_concrete_article);
-	{rdelim}
+	}
 
 	/// prepare AJAX call in modal box: insert extra on pagezone_page or article
-	function extra_insert_after(origin_uid, pz_uid, paragraph, new_at_top, is_concrete_article) {ldelim}
+	function extra_insert_after(origin_uid, pz_uid, paragraph, new_at_top, is_concrete_article) {
 /// \todo: add be_mode
 		subModalExtraInsertAfter(origin_uid, pz_uid, paragraph, new_at_top, is_concrete_article);
-	{rdelim}	
+	}	
 	
 	
 	/// store data in field (if field is changed a undo/store option is added to field; one field editable at a time)
-	function extra_save_field(pz_uid, extra_uid, value, type, is_concrete_article) {ldelim}
+	function extra_save_field(pz_uid, extra_uid, value, type, is_concrete_article) {
 		var request = new top.Ajax.Request(
 				top.path + "typo3conf/ext/newspaper/mod3/index.php",
-				{ldelim}
+				{
 				method: 'get',
 				parameters: "extra_save_field=1&pz_uid=" + pz_uid + "&extra_uid=" + extra_uid + "&value=" + value + "&type=" + type + "&no_cache=" + new Date().getTime(),
 				onCreate: eval(get_onCreate_function(is_concrete_article)),
-				onSuccess: response_in_article
-			{rdelim}
+				onSuccess: eval(get_onSuccess_function(is_concrete_article))
+			}
 		);
-	{rdelim}
+	}
 	
 	
 
@@ -247,7 +248,7 @@ function loadJsCssFile(filename, filetype) {ldelim}
 /// modal box functions
 	
 	// new at top = show input field for paragraph
-	function subModalExtraInsertAfter(origin_uid, pz_uid, paragraph, new_at_top, is_concrete_article) {ldelim}
+	function subModalExtraInsertAfter(origin_uid, pz_uid, paragraph, new_at_top, is_concrete_article) {
 		var width = Math.min(700, top.getViewportWidth() - 100); 
 		var height = top.getViewportHeight() - 50;
 		var closehtml = (is_concrete_article)? escape(t3BackendObject.path + "typo3conf/ext/newspaper/mod3/close_reload_in_concrete_article.html?pz_uid=" + pz_uid) : t3BackendObject.path + "typo3conf/ext/newspaper/mod3/close.html"; 
@@ -258,9 +259,9 @@ function loadJsCssFile(filename, filetype) {ldelim}
 			null, 
 			true
 		);
-	{rdelim}
+	}
 	
-	function subModalExtraEdit(table, uid, pz_uid, is_concrete_article) {ldelim}
+	function subModalExtraEdit(table, uid, pz_uid, is_concrete_article) {
 		var width = Math.min(700, top.getViewportWidth() - 100); 
 		var height = top.getViewportHeight() - 50;
 		var closehtml = (is_concrete_article)? escape(t3BackendObject.path + "typo3conf/ext/newspaper/mod3/close_reload_in_concrete_article.html?pz_uid=" + pz_uid) : t3BackendObject.path + "typo3conf/ext/newspaper/mod3/close.html";
@@ -271,7 +272,7 @@ function loadJsCssFile(filename, filetype) {ldelim}
 			null, 
 			true
 		);
-	{rdelim}
+	}
 
 	
 	
@@ -312,84 +313,84 @@ function loadJsCssFile(filename, filetype) {ldelim}
 
 
 
-	function extra_insert_after_dummy(origin_uid, pagezone_uid) {ldelim}
+	function extra_insert_after_dummy(origin_uid, pagezone_uid) {
 /// \todo: remove after testing
 		var request = new top.Ajax.Request(
  			top.path + "typo3conf/ext/newspaper/mod3/index.php",
- 			{ldelim}
+ 			{
 				method: 'get',
 				parameters: "extra_insert_after_dummy=1&origin_uid=" + origin_uid + "&pz_uid=" + pagezone_uid + "&no_cache=" + new Date().getTime(),
 				onCreate: processing,
 				onSuccess: reload
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 
 
-	function extra_set_show(extra_uid, show) {ldelim}
+	function extra_set_show(extra_uid, show) {
 		var request = new top.Ajax.Request(
  			top.path + "typo3conf/ext/newspaper/mod3/index.php",
- 			{ldelim}
+ 			{
 				method: 'get',
 				parameters: "extra_set_show=1&extra_uid=" + extra_uid + "&show=" + show + "&no_cache=" + new Date().getTime(),
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 
-	function extra_set_pass_down(pz_uid, extra_uid, pass_down) {ldelim}
+	function extra_set_pass_down(pz_uid, extra_uid, pass_down) {
 		var request = new top.Ajax.Request(
  			top.path + "typo3conf/ext/newspaper/mod3/index.php",
- 			{ldelim}
+ 			{
 				method: 'get',
 				parameters: "extra_set_pass_down=1&pz_uid=" + pz_uid + "&extra_uid=" + extra_uid + "&pass_down=" + pass_down + "&no_cache=" + new Date().getTime(),
 				onCreate: processing,
 				onSuccess: reload
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 
-	function page_type_change(pt_uid) {ldelim}
+	function page_type_change(pt_uid) {
 		var request = new top.Ajax.Request(
  			top.path + "typo3conf/ext/newspaper/mod3/index.php",
- 			{ldelim}
+ 			{
 				method: 'get',
 				parameters: "extra_page_type_change=1&pt_uid=" + pt_uid + "&no_cache=" + new Date().getTime(),
 				onCreate: processing,
 				onSuccess: reload
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 
-	function pagezone_type_change(pzt_uid) {ldelim}
+	function pagezone_type_change(pzt_uid) {
 		var request = new top.Ajax.Request(
  			top.path + "typo3conf/ext/newspaper/mod3/index.php",
- 			{ldelim}
+ 			{
 				method: 'get',
 				parameters: "extra_pagezone_type_change=1&pzt_uid=" + pzt_uid + "&no_cache=" + new Date().getTime(),
 				onCreate: processing,
 				onSuccess: reload
-			{rdelim}
+			}
 		);
-	{rdelim}
+	}
 
 	
-	function inheritancesource_change(pz_uid, value) {ldelim}
+	function inheritancesource_change(pz_uid, value) {
 	var request = new top.Ajax.Request(
 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
-			{ldelim}
+			{
 			method: 'get',
 			parameters: "inheritancesource_change=1&pz_uid=" + pz_uid + "&value=" + value + "&no_cache=" + new Date().getTime(),
 			onCreate: processing,
 			onSuccess: reload
-		{rdelim}
+		}
 	);
-{rdelim}
+}
 
 	
 	
 
 /// \to do: remove after all calls are switched to page/article version ????
-	function processing() {ldelim}
+	function processing() {
 
 		img = document.createElement('img');
 		img_src = document.createAttribute('src');
@@ -404,19 +405,19 @@ function loadJsCssFile(filename, filetype) {ldelim}
 
 		layer.appendChild(img);
 		self.document.getElementsByTagName('body')[0].appendChild(layer);
-	{rdelim}
+	}
 	
 	// direct reload (no popup/modalbox involved ...)
-	function reload(data) {ldelim}
-		if (data.responseText == '') {ldelim}
+	function reload(data) {
+		if (data.responseText == '') {
 			// empty string, so no error message
 /// \todo: currently no_cache param are ADDED, must be REPLACED
 			self.location.href = self.location.href + "&no_cache=" + new Date().getTime();
-		{rdelim} else {ldelim}
+		} else {
 			// display error message
 			self.document.getElementsByTagName('body')[0].innerHTML = data.responseText;
-		{rdelim}
-	{rdelim}
+		}
+	}
 
 	
 	
@@ -432,61 +433,61 @@ function loadJsCssFile(filename, filetype) {ldelim}
 	document.change_notes = false; // if set to false, a note might be changed
 	document.def_notes = new Array(); // stores the current value for all notes being displayed
 	
-	function changeField(extra_uid, type) {ldelim}
-		if (eval("document.change_" + type)) {ldelim}
+	function changeField(extra_uid, type) {
+		if (eval("document.change_" + type)) {
 			alert('todo: message ... erst speichern ... ' + extra_uid + ',' + eval("document.def_" + type + "[extra_uid]"));
 			document.getElementById(type + '_' + extra_uid).value = eval("document.def_" + type + "[extra_uid]"); // undo
 			return false;
-		{rdelim}
+		}
 
 		// \todo: type check ...
 		
 		document.getElementById(type + '_td_' + extra_uid).style.backgroundColor = 'red';
 		document.getElementById('save_' + type + '_' + extra_uid).style.display = 'inline';
 		
-		switch(type) {ldelim}
+		switch(type) {
 			case 'para':
 				document.change_para = true;
 			break;
 			case 'notes':
 				document.change_notes = true;
 			break;
-		{rdelim}
+		}
 		
-	{rdelim}
+	}
 
-	function undoField(extra_uid, type) {ldelim}
+	function undoField(extra_uid, type) {
 		document.getElementById(type + '_td_' + extra_uid).style.backgroundColor = '';
 		document.getElementById('save_' + type + '_' + extra_uid).style.display = 'none';
 
 		document.getElementById(type + '_' + extra_uid).value = eval("document.def_" + type + "[extra_uid]");
 
-		switch(type) {ldelim}
+		switch(type) {
 			case 'para':
 				document.change_para = false;
 			break;
 			case 'notes':
 				document.change_notes = false;
 			break;
-		{rdelim}
+		}
 		
-	{rdelim}
+	}
 
-	function saveField(pz_uid, extra_uid, type, is_concrete_article) {ldelim}
+	function saveField(pz_uid, extra_uid, type, is_concrete_article) {
 		value = document.getElementById(type + '_' + extra_uid).value;
 		extra_save_field(pz_uid, extra_uid, value, type, is_concrete_article);
 		
 		// store this value as new default (not needed if page is reloaded ...)
-		switch(type) {ldelim}
+		switch(type) {
 			case 'para':
 				document.def_para[extra_uid] = value;
 			break;
 			case 'notes':
 				document.def_notes[extra_uid] = value;
 			break;
-		{rdelim}
+		}
 		
-	{rdelim}
+	}
 	
 	
 	
@@ -494,18 +495,19 @@ function loadJsCssFile(filename, filetype) {ldelim}
 	
 	
 /// template set dropdown handling: ATTENTION: copy of this function in res/be/pagetype_pagezonetype_4section.js
-	function storeTemplateSet(table, uid, value) {ldelim}
+	function storeTemplateSet(table, uid, value) {
 		uid = parseInt(uid);
 		var request = new top.Ajax.Request(
 			top.path + "typo3conf/ext/newspaper/mod3/index.php",
-			{ldelim}
+			{
 				method: 'get',
 				parameters: "templateset_dropdown_store=1&table=" + table + "&uid=" + uid + "&value=" + value + "&no_cache=" + new Date().getTime()
-			{rdelim}
+			}
 		);
-	{rdelim}				
+	}				
 
 	
 
 	
 </script>
+{/literal}
