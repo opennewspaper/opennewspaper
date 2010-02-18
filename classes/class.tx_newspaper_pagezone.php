92172c9f73f9c8c8c2cf13ecf4c33664c3e16949
Lene Preuss <lene.preuss@gmail.com>
2010-02-18 17:29:26 +0000

    manually setting the origin uid confuses getInsertPosition()

--- classes/class.tx_newspaper_pagezone.php
+++ classes/class.tx_newspaper_pagezone.php
@@ -486,7 +486,7 @@ abstract class tx_newspaper_PageZone implements tx_newspaper_ExtraIface {
 				$copied_extra = clone $insert_extra;
 				$copied_extra->setAttribute('origin_uid', $insert_extra->getOriginUid());
 				
-				$inheriting_pagezone->insertExtraAfter($copied_extra, $insert_extra->getOriginUid(), false);
+				$inheriting_pagezone->insertExtraAfter($copied_extra, $origin_uid /* $insert_extra->getOriginUid() */, false);
 				t3lib_div::devlog('copied extra', 'newspaper', 0, $copied_extra->__toString());
 		
 			}
