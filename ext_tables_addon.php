eb937c012a5a3d3d3b9de3872ce7f1c4e2217a76
Ramon Zöllner <ramon@ramonzoellner.de>
2010-03-02 15:40:06 +0000

    add mod8

--- ext_tables_addon.php
+++ ext_tables_addon.php
@@ -83,6 +83,7 @@ if (TYPO3_MODE == 'BE') {
 	t3lib_extMgm::addModule('txnewspaperMmain','txnewspaperM1','',t3lib_extMgm::extPath($_EXTKEY).'mod1/'); // AJAX stuff
 	t3lib_extMgm::addModule('txnewspaperMmain','txnewspaperM2','',t3lib_extMgm::extPath($_EXTKEY).'mod2/'); // moderation list (workflow)
 	t3lib_extMgm::addModule('txnewspaperMmain','txnewspaperM3','',t3lib_extMgm::extPath($_EXTKEY).'mod3/'); // placement
+    t3lib_extMgm::addModule('txnewspaperMmain','txnewspaperM8','',t3lib_extMgm::extPath($_EXTKEY).'mod8/'); // tag admin
 	t3lib_extMgm::addModule('txnewspaperMmain','txnewspaperM6','',t3lib_extMgm::extPath($_EXTKEY).'mod6/'); // control tags
 	t3lib_extMgm::addModule('txnewspaperMmain','txnewspaperM4','',t3lib_extMgm::extPath($_EXTKEY).'mod4/'); // admin tools
 	t3lib_extMgm::addModule('txnewspaperMmain','txnewspaperM7','',t3lib_extMgm::extPath($_EXTKEY).'mod7/'); // placing articles in article lists
