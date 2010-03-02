49aeaadf555482b72e1b2146569b908ceb4f1f3b
Ramon Zöllner <ramon@ramonzoellner.de>
2010-03-02 15:42:37 +0000

    Merge branch 'tag_modul'

--- mod8/conf.php
+++ mod8/conf.php
@@ -1,14 +1,14 @@
 <?php
 
-	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
-define('TYPO3_MOD_PATH', 'ext/newspaper/mod8/');
-$BACK_PATH='../../../';
-$MCONF['name']='web_txnewspaperM8';
+// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/newspaper/mod8/');
+$BACK_PATH = '../../../../typo3/';
+$MCONF['name'] = 'txnewspaperMmain_txnewspaperM8';
 
-	
-$MCONF['access']='user,group';
-$MCONF['script']='index.php';
+
+$MCONF['access'] = 'user,group';
+$MCONF['script'] = 'index.php';
 
 $MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
-$MLANG['default']['ll_ref']='LLL:EXT:newspaper/mod8/locallang_mod.xml';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:newspaper/mod8/locallang_mod.xml';
 ?>
