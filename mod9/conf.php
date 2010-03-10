9d1e64e0ab436573a61f2e5ac981b7687e3ad93b
Oliver Schröder <oliver@schroederbros.de>
2010-03-10 16:20:40 +0000

    mod9 added

---
+++ mod9/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', 'ext/newspaper/mod9/');
+$BACK_PATH='../../../';
+$MCONF['name']='txnewspaperMmain_txnewspaperM9';
+
+	
+$MCONF['access']='user,group';
+$MCONF['script']='index.php';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:newspaper/mod9/locallang_mod.xml';
+?>
