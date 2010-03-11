43632149c488d52d8b6d21df71575880e8175af1
Oliver Schröder <oliver@schroederbros.de>
2010-03-11 10:00:20 +0000

    module configured correctly (finally ...)

--- mod9/conf.php
+++ mod9/conf.php
@@ -1,14 +1,16 @@
 <?php
 
-	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
-define('TYPO3_MOD_PATH', 'ext/newspaper/mod9/');
-$BACK_PATH='../../../';
-$MCONF['name']='txnewspaperMmain_txnewspaperM9';
+	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
+$MCONF['name'] = 'txnewspaperMmain_txnewspaperM9';
+$MCONF['script'] = '_DISPATCH';
 
+define('TYPO3_MOD_PATH', '../typo3conf/ext/newspaper/mod9/');
+$BACK_PATH = '../../../../typo3/';
 	
-$MCONF['access']='user,group';
-$MCONF['script']='index.php';
+$MCONF['access'] = 'user,group';
+$MCONF['navFrameScript']='class.tx_newspaper_sectiontree_mod9.php';
 
 $MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
-$MLANG['default']['ll_ref']='LLL:EXT:newspaper/mod9/locallang_mod.xml';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:newspaper/mod9/locallang_mod.xml';
+
 ?>
--- mod9/index.php
+++ mod9/index.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2010 Helge Preuss, Oliver Schröder, Samuel Talleux <helge.preuss@gmail.com, typo3@schroederbros.de, samuel@talleux.de>
+*  (c) 2009 Helge Preuss, Oliver Schroeder, Samuel Talleux <helge.preuss@gmail.com, typo3@schroederbros.de, samuel@talleux.de>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -21,63 +21,47 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
 
 
-	// DEFAULT initialization of a module [BEGIN]
-unset($MCONF);
+/// \todo: @oliver: major clean up needed!
+
+
+//unset($MCONF);
 require_once('conf.php');
-require_once($BACK_PATH.'init.php');
-require_once($BACK_PATH.'template.php');
+require_once($BACK_PATH . 'init.php');
+require_once($BACK_PATH . 'template.php');
+
+
+/// Class to generate a BE module with 100% width
+class fullWidthDoc extends template {
+	var $divClass = 'typo3-fullWidthDoc';	///< Sets width to 100%
+}
+
+
+
+
+//var_dump(debug_backtrace());
+//debug($_REQUEST, '_reuqest');
 
 $LANG->includeLLFile('EXT:newspaper/mod9/locallang.xml');
-require_once(PATH_t3lib.'class.t3lib_scbase.php');
+require_once(PATH_t3lib . 'class.t3lib_scbase.php');
 $BE_USER->modAccess($MCONF,1);	// This checks permissions and exits if the users has no permission for entry.
 	// DEFAULT initialization of a module [END]
 
-
+define('DEBUG_OUTPUT', true); // show position etc.
 
 /**
- * Module 'Article lists' for the 'newspaper' extension.
+ * Module 'Placement' for the 'newspaper' extension.
  *
- * @author	Helge Preuss, Oliver Schröder, Samuel Talleux <helge.preuss@gmail.com, typo3@schroederbros.de, samuel@talleux.de>
- * @package	TYPO3
- * @subpackage	tx_newspaper
+ * @author	Helge Preuss, Oliver Schroeder, Samuel Talleux <helge.preuss@gmail.com, typo3@schroederbros.de, samuel@talleux.de>
  */
 class  tx_newspaper_module9 extends t3lib_SCbase {
-				var $pageinfo;
-
-				/**
-				 * Initializes the Module
-				 * @return	void
-				 */
-				function init()	{
-					global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
-
-					parent::init();
-
-					/*
-					if (t3lib_div::_GP('clear_all_cache'))	{
-						$this->include_once[] = PATH_t3lib.'class.t3lib_tcemain.php';
-					}
-					*/
-				}
-
-				/**
-				 * Adds items to the ->MOD_MENU array. Used for the function menu selector.
-				 *
-				 * @return	void
-				 */
-				function menuConfig()	{
-					global $LANG;
-					$this->MOD_MENU = Array (
-						'function' => Array (
-							'1' => $LANG->getLL('function1'),
-							'2' => $LANG->getLL('function2'),
-							'3' => $LANG->getLL('function3'),
-						)
-					);
-					parent::menuConfig();
-				}
+	var $pageinfo;
 
 				/**
 				 * Main function of the module. Write the content to $this->content
@@ -90,15 +74,18 @@ class  tx_newspaper_module9 extends t3lib_SCbase {
 
 					// Access check!
 					// The page will show only if there is a valid page and if this page may be viewed by the user
-					$this->pageinfo = t3lib_BEfunc::readPageAccess($this->id,$this->perms_clause);
-					$access = is_array($this->pageinfo) ? 1 : 0;
+					
+					
+//					$this->pageinfo = t3lib_BEfunc::readPageAccess($this->id,$this->perms_clause);
+//					$access = is_array($this->pageinfo) ? 1 : 0;
+					$access = 1; /// \todo: maybe we should implement a more sophisticated version of this ;-)
 
 					if (($this->id && $access) || ($BE_USER->user['admin'] && !$this->id))	{
 
 							// Draw the header.
-						$this->doc = t3lib_div::makeInstance('mediumDoc');
+						$this->doc = t3lib_div::makeInstance('fullWidthDoc');
 						$this->doc->backPath = $BACK_PATH;
-						$this->doc->form='<form action="" method="POST">';
+						$this->doc->form='<form action="" method="post" enctype="multipart/form-data">';
 
 							// JavaScript
 						$this->doc->JScode = '
@@ -116,13 +103,13 @@ class  tx_newspaper_module9 extends t3lib_SCbase {
 							</script>
 						';
 
-						$headerSection = $this->doc->getHeader('pages',$this->pageinfo,$this->pageinfo['_thePath']).'<br />'.$LANG->sL('LLL:EXT:lang/locallang_core.xml:labels.path').': '.t3lib_div::fixed_lgd_pre($this->pageinfo['_thePath'],50);
+						$headerSection = $this->doc->getHeader('pages',$this->pageinfo,$this->pageinfo['_thePath']).'<br />' . $LANG->sL('LLL:EXT:lang/locallang_core.xml:labels.path').': '.t3lib_div::fixed_lgd_pre($this->pageinfo['_thePath'],50);
 
 						$this->content.=$this->doc->startPage($LANG->getLL('title'));
 						$this->content.=$this->doc->header($LANG->getLL('title'));
-						$this->content.=$this->doc->spacer(5);
-						$this->content.=$this->doc->section('',$this->doc->funcMenu($headerSection,t3lib_BEfunc::getFuncMenu($this->id,'SET[function]',$this->MOD_SETTINGS['function'],$this->MOD_MENU['function'])));
-						$this->content.=$this->doc->divider(5);
+//						$this->content.=$this->doc->spacer(5);
+//						$this->content.=$this->doc->section('',$this->doc->funcMenu($headerSection,t3lib_BEfunc::getFuncMenu($this->id,'SET[function]',$this->MOD_SETTINGS['function'],$this->MOD_MENU['function'])));
+//						$this->content.=$this->doc->divider(5);
 
 
 						// Render content:
@@ -130,9 +117,9 @@ class  tx_newspaper_module9 extends t3lib_SCbase {
 
 
 						// ShortCut
-						if ($BE_USER->mayMakeShortcut())	{
-							$this->content.=$this->doc->spacer(20).$this->doc->section('',$this->doc->makeShortcutIcon('id',implode(',',array_keys($this->MOD_MENU)),$this->MCONF['name']));
-						}
+//						if ($BE_USER->mayMakeShortcut())	{
+//							$this->content.=$this->doc->spacer(20).$this->doc->section('',$this->doc->makeShortcutIcon('id',implode(',',array_keys($this->MOD_MENU)),$this->MCONF['name']));
+//						}
 
 						$this->content.=$this->doc->spacer(10);
 					} else {
@@ -146,6 +133,7 @@ class  tx_newspaper_module9 extends t3lib_SCbase {
 						$this->content.=$this->doc->spacer(5);
 						$this->content.=$this->doc->spacer(10);
 					}
+				
 				}
 
 				/**
@@ -164,29 +152,45 @@ class  tx_newspaper_module9 extends t3lib_SCbase {
 				 *
 				 * @return	void
 				 */
-				function moduleContent()	{
-					switch((string)$this->MOD_SETTINGS['function'])	{
-						case 1:
-							$content='<div align="center"><strong>Hello World!</strong></div><br />
-								The "Kickstarter" has made this module automatically, it contains a default framework for a backend module but apart from that it does nothing useful until you open the script '.substr(t3lib_extMgm::extPath('newspaper'),strlen(PATH_site)).$pathSuffix.'index.php and edit it!
-								<hr />
-								<br />This is the GET/POST vars sent to the script:<br />'.
-								'GET:'.t3lib_div::view_array($_GET).'<br />'.
-								'POST:'.t3lib_div::view_array($_POST).'<br />'.
-								'';
-							$this->content.=$this->doc->section('Message #1:',$content,0,1);
-						break;
-						case 2:
-							$content='<div align=center><strong>Menu item #2...</strong></div>';
-							$this->content.=$this->doc->section('Message #2:',$content,0,1);
-						break;
-						case 3:
-							$content='<div align=center><strong>Menu item #3...</strong></div>';
-							$this->content.=$this->doc->section('Message #3:',$content,0,1);
-						break;
-					}
+				function moduleContent() {
+					global $LANG;
+
+					$content = 'dummy';
+
+					$this->content .= $this->doc->section('', $content, 0, 1);
+				
 				}
-			}
+
+
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Initializes the Module
+	 * @return	void
+	 */
+	function init()	{
+		global $BE_USER,$LANG,$BACK_PATH,$TCA_DESCR,$TCA,$CLIENT,$TYPO3_CONF_VARS;
+	
+		parent::init();
+	
+	}
+
+	/**
+	 * Adds items to the ->MOD_MENU array. Used for the function menu selector.
+	 *
+	 * @return	void
+	 */
+	function menuConfig()	{
+		global $LANG;
+		parent::menuConfig();
+	}
+
+}
 
 
 
