07dc41b26b2cc3df675121292d7dd6b123aac218
Lene Preuss <lene.preuss@gmail.com>
2010-03-18 11:04:42 +0000

    constructor needs explicit int value to use it as UID

--- classes/class.tx_newspaper_smarty.php
+++ classes/class.tx_newspaper_smarty.php
@@ -172,7 +172,7 @@ class tx_newspaper_Smarty extends Smarty {
 		$page_type = $page->getPageType();
 		$page_type->getAttribute('uid');
 		$pagetype_uid = $page->getAttribute('pagetype_id');
-		$page_type2 = new tx_newspaper_PageType($pagetype_uid);
+		$page_type2 = new tx_newspaper_PageType(intval($pagetype_uid));
 		$page_type2->getAttribute('uid');
 		t3lib_div::devlog('setPageType', 'np', 0, 
 			array($pagetype_uid, $page_type, $page_type2, array_slice(debug_backtrace(), 0, 2)));
