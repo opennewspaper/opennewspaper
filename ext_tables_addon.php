fa3efc3fed7778f9c75da29793244ee58edda75c
Samuel Talleux <samuel@talleux.de>
2010-03-22 18:49:03 +0000

    localize and reorganize modules

--- ext_tables_addon.php
+++ ext_tables_addon.php
@@ -82,13 +82,13 @@ if (TYPO3_MODE == 'BE') {
 	t3lib_extMgm::addModule('txnewspaperMmain', '',              '', t3lib_extMgm::extPath($_EXTKEY) . 'mod_main/'); // main
 	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM5', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod5/'); // wizards
 	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM2', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod2/'); // moderation list (workflow)
-	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM3', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod3/'); // placement
-    t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM8', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod8/'); // tag admin
-	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM6', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod6/'); // control tags
 	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM9', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod9/'); // placing articles in section article lists
 	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM7', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod7/'); // placing articles in article lists (non-section) + backend for manual and semiatumatic article lists
-	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM4', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod4/'); // admin tools
+	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM3', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod3/'); // placement
+	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM6', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod6/'); // control tags
+	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM8', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod8/'); // tag admin
 	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/'); // AJAX stuff
+	t3lib_extMgm::addModule('txnewspaperMmain', 'txnewspaperM4', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod4/'); // admin tools
 
 	/// add newspaper to Plugin-in list
 	/// records are stored in sysfolders with module set to 'newspaper'
