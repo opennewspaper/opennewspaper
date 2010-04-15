e2fd1664e310d01333c4f2c1f98b58bb3c46c5ff
Oliver Schröder <oliver@schroederbros.de>
2010-04-15 11:03:20 +0000

    new_article_backend(): add section to section2 only if a default article is available

--- mod5/index.php
+++ mod5/index.php
@@ -313,6 +313,8 @@ class  tx_newspaper_module5 extends t3lib_SCbase {
 //t3lib_div::devlog('NEW ARTICLE', 'newspaper', 0);		
 		global $LANG;
 		
+		
+// \todo: remove old stuff when new stuff is approved
 		$sections = tx_newspaper_Section::getAllSections();
 		$sections_available = array(); // true = default article available, else false
 		foreach($sections as $section) {
@@ -321,8 +323,12 @@ class  tx_newspaper_module5 extends t3lib_SCbase {
 			} else {
 				$sections_available[] = false;
 			}
-		}		
-		
+		}
+
+
+
+
+
  		$smarty = new tx_newspaper_Smarty();
 		$smarty->setTemplateSearchPath(array('typo3conf/ext/newspaper/mod5/'));
 
@@ -363,13 +369,13 @@ class  tx_newspaper_module5 extends t3lib_SCbase {
 		$sub_sections = array();
 		foreach($start_sections as $key => $current_sub_section) {
 			// check if main section on level 1 can take articles. add to selectbox2 if yes (only selectbox2 sections can be chosen)
-			if ($current_sub_section->getAttribute('articles_allowed')) {
+			if ($current_sub_section->getAttribute('articles_allowed') && $current_sub_section->getDefaultArticle()) {
 				$sub_sections[$current_sub_section->getUid()][$start_section->getUid()] = $current_sub_section;
 			}
 			// add all sub section that can take articles
 			$tmp_sections = $current_sub_section->getChildSections(true);
 			foreach($tmp_sections as $tmp_section) {
-				if ($tmp_section->getAttribute('articles_allowed')) {
+				if ($tmp_section->getAttribute('articles_allowed') && $tmp_section->getDefaultArticle()) {
 					$sub_sections[$current_sub_section->getUid()][$tmp_section->getUid()] = $tmp_section;
 				}
 			}
