23afc8c6712898471bf43805bb5a43c036e8ec2a
Oliver Schröder <oliver@schroederbros.de>
2010-06-15 13:32:38 +0000

    testing: rte conf for extra textbox

--- tca.php
+++ tca.php
@@ -1675,6 +1675,17 @@ $TCA["tx_newspaper_extra_textbox"] = array (
 				"type" => "text",
 				"cols" => "30",
 				"rows" => "5",
+				"wizards" => Array(
+					"_PADDING" => 2,
+					"RTE" => array(
+						"notNewRecords" => 1,
+						"RTEonly" => 1,
+						"type" => "script",
+						"title" => "Full screen Rich Text Editing|Formatteret redigering i hele vinduet",
+						"icon" => "wizard_rte2.gif",
+						"script" => "wizard_rte.php",
+					),
+				),
 			)
 		),
 		"image" => Array (		
