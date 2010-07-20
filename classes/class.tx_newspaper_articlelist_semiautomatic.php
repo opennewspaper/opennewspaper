411ef438ef042c48672064817ecd0458cb5cec67
Lene Preuss <lene.preuss@gmail.com>
2010-07-20 10:18:17 +0000

    debug++

--- classes/class.tx_newspaper_articlelist_semiautomatic.php
+++ classes/class.tx_newspaper_articlelist_semiautomatic.php
@@ -148,7 +148,7 @@ class tx_newspaper_ArticleList_Semiautomatic extends tx_newspaper_ArticleList {
 	const default_num_articles = 20;
 	
 	/// how many articles are loaded for comparison of offsets
-	const num_raw_uids =100;
+	const num_raw_uids = 100;
 	
 	/// SQL table storing the relations between list and articles.
 	const mm_table = 'tx_newspaper_articlelist_semiautomatic_articles_mm';
@@ -292,6 +292,7 @@ class tx_newspaper_ArticleList_Semiautomatic extends tx_newspaper_ArticleList {
         $old_order[$index][1] -= $distance;
 
         t3lib_div::devlog('resortArticle()', 'newspaper', 0, array(
+            'uid' => $old_order[$index][0],
             'index'=>$index,
             'shuffle_value' => $shuffle_value,
             'new index' => $new_index,
