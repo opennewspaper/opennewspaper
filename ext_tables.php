bbc3632d3a9ef6e5a3c58edacd0dd908361b38f0
Lene Preuss <lene.preuss@gmail.com>
2010-09-07 12:00:42 +0000

    rmoved a s**tload of nonsensical lines

--- ext_tables.php
+++ ext_tables.php
@@ -14,51 +14,6 @@ t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Display Ressorts/Articles");
 if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_newspaper_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_newspaper_pi1_wizicon.php';
 
 
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
-if (TYPO3_MODE == 'BE')	{
-		
-}
-
-
 t3lib_extMgm::addToInsertRecords('tx_newspaper_extra_image');
 
 $TCA["tx_newspaper_extra_image"] = array (
