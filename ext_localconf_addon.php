ee838fb9b320b7b8f4c3c1b1df80db1e38e0ba34
Oliver Schröder <oliver@schroederbros.de>
2010-09-08 11:10:05 +0000

    added switch: hide backend fields in backend only

--- ext_localconf_addon.php
+++ ext_localconf_addon.php
@@ -31,113 +31,117 @@
 	
 	
 	
-	
-	$sysfolder = ''; // \todo, see #603
-	
-	// hide some fields
-	// hide some tables
-	t3lib_extMgm::addPageTSConfig('
-		TCEFORM.tx_newspaper_article.hidden.disabled = 1 
-		TCEFORM.tx_newspaper_article.source_id.disabled = 1
-		TCEFORM.tx_newspaper_article.source_object.disabled = 1
-		TCEFORM.tx_newspaper_article.is_template.disabled = 1
-		TCEFORM.tx_newspaper_article.pagezonetype_id.disabled = 1
-		TCEFORM.tx_newspaper_article.name.disabled = 1
-		TCEFORM.tx_newspaper_article.inherits_from.disabled = 1
-		TCEFORM.tx_newspaper_article.modification_user.disabled = 1
-		TCEFORM.tx_newspaper_articlelist.section_id.disabled = 1
-		TCEFORM.pages.tx_newspaper_module.disabled = 1
-
-		// hide filter fields for manuel articlelist (activate when article browser can use filters)
-		TCEFORM.tx_newspaper_articlelist_manual.filter_sections.disabled = 1
-		TCEFORM.tx_newspaper_articlelist_manual.filter_tags_include.disabled = 1
-		TCEFORM.tx_newspaper_articlelist_manual.filter_tags_exclude.disabled = 1
-		TCEFORM.tx_newspaper_articlelist_manual.filter_articlelist_exclude.disabled = 1
-		TCEFORM.tx_newspaper_articlelist_manual.filter_sql_table.disabled = 1
-		TCEFORM.tx_newspaper_articlelist_manual.filter_sql_where.disabled = 1
-		TCEFORM.tx_newspaper_articlelist_manual.filter_sql_order_by.disabled = 1
+	if (TYPO3_MODE == 'BE') {
 		
-		// hide starttime and endtime for abstract articlelist (\todo: remove in kickstarter; what could be a useful usage?)
-		TCEFORM.tx_newspaper_articlelist.starttime.disabled = 1
-		TCEFORM.tx_newspaper_articlelist.endtime.disabled = 1
-		TCEFORM.tx_newspaper_articlelist.hidden.disabled = 1
-
-		// hide starttime and endtime for extras
-		TCEFORM.tx_newspaper_extra_ad.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_ad.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_ad.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_articlelinks.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_articlelist.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_articlelist.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_bio.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_bio.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_bio.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_combolinkbox.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_combolinkbox.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_combolinkbox.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_container.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_container.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_container.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_controltagzone.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_controltagzone.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_controltagzone.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_displayarticles.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_displayarticles.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_externallinks.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_externallinks.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_generic.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_generic.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_generic.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_image.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_image.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_image.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_mostcommented.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_mostcommented.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_mostcommented.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_searchresults.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_searchresults.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_sectionlist.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_sectionlist.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_sectionlist.hidden.disabled = 1
-		TCEFORM.tx_newspaper_extra_textbox.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_textbox.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_typo3_ce.starttime.disabled = 1
-		TCEFORM.tx_newspaper_extra_typo3_ce.endtime.disabled = 1
-		TCEFORM.tx_newspaper_extra_typo3_ce.hidden.disabled = 1
-
-		// \todo enable after functionlaity is implemented, see #1111		
-		TCEFORM.tx_newspaper_articlelist_semiautomatic.subsequent_sections.disabled = 1
-
-		mod.web_list.hideTables = tx_newspaper_extra,tx_newspaper_page,tx_newspaper_pagezone,tx_newspaper_pagezone_page
-				
-		user.options.hideRecords.pages = ' . $sysfolder . '
-	');
+		// some backend modifications
 	
-	// hide template sets in sections, if they shouldn't be used
-	if (!tx_newspaper_be::useTemplateSetsForSections()) {
+		$sysfolder = ''; // \todo, see #603
+	
+		// hide some fields
+		// hide some tables
 		t3lib_extMgm::addPageTSConfig('
-			TCEFORM.tx_newspaper_section.template_set.disabled = 1 
+			TCEFORM.tx_newspaper_article.hidden.disabled = 1 
+			TCEFORM.tx_newspaper_article.source_id.disabled = 1
+			TCEFORM.tx_newspaper_article.source_object.disabled = 1
+			TCEFORM.tx_newspaper_article.is_template.disabled = 1
+			TCEFORM.tx_newspaper_article.pagezonetype_id.disabled = 1
+			TCEFORM.tx_newspaper_article.name.disabled = 1
+			TCEFORM.tx_newspaper_article.inherits_from.disabled = 1
+			TCEFORM.tx_newspaper_article.modification_user.disabled = 1
+			TCEFORM.tx_newspaper_articlelist.section_id.disabled = 1
+			TCEFORM.pages.tx_newspaper_module.disabled = 1
+	
+			// hide filter fields for manuel articlelist (activate when article browser can use filters)
+			TCEFORM.tx_newspaper_articlelist_manual.filter_sections.disabled = 1
+			TCEFORM.tx_newspaper_articlelist_manual.filter_tags_include.disabled = 1
+			TCEFORM.tx_newspaper_articlelist_manual.filter_tags_exclude.disabled = 1
+			TCEFORM.tx_newspaper_articlelist_manual.filter_articlelist_exclude.disabled = 1
+			TCEFORM.tx_newspaper_articlelist_manual.filter_sql_table.disabled = 1
+			TCEFORM.tx_newspaper_articlelist_manual.filter_sql_where.disabled = 1
+			TCEFORM.tx_newspaper_articlelist_manual.filter_sql_order_by.disabled = 1
+			
+			// hide starttime and endtime for abstract articlelist (\todo: remove in kickstarter; what could be a useful usage?)
+			TCEFORM.tx_newspaper_articlelist.starttime.disabled = 1
+			TCEFORM.tx_newspaper_articlelist.endtime.disabled = 1
+			TCEFORM.tx_newspaper_articlelist.hidden.disabled = 1
+	
+			// hide starttime and endtime for extras
+			TCEFORM.tx_newspaper_extra_ad.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_ad.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_ad.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_articlelinks.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_articlelist.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_articlelist.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_bio.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_bio.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_bio.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_combolinkbox.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_combolinkbox.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_combolinkbox.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_container.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_container.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_container.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_controltagzone.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_controltagzone.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_controltagzone.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_displayarticles.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_displayarticles.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_externallinks.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_externallinks.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_generic.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_generic.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_generic.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_image.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_image.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_image.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_mostcommented.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_mostcommented.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_mostcommented.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_searchresults.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_searchresults.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_sectionlist.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_sectionlist.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_sectionlist.hidden.disabled = 1
+			TCEFORM.tx_newspaper_extra_textbox.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_textbox.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_typo3_ce.starttime.disabled = 1
+			TCEFORM.tx_newspaper_extra_typo3_ce.endtime.disabled = 1
+			TCEFORM.tx_newspaper_extra_typo3_ce.hidden.disabled = 1
+	
+			// \todo enable after functionlaity is implemented, see #1111		
+			TCEFORM.tx_newspaper_articlelist_semiautomatic.subsequent_sections.disabled = 1
+	
+			mod.web_list.hideTables = tx_newspaper_extra,tx_newspaper_page,tx_newspaper_pagezone,tx_newspaper_pagezone_page
+					
+			user.options.hideRecords.pages = ' . $sysfolder . '
+		');
+		
+		// hide template sets in sections, if they shouldn't be used
+		if (!tx_newspaper_be::useTemplateSetsForSections()) {
+			t3lib_extMgm::addPageTSConfig('
+				TCEFORM.tx_newspaper_section.template_set.disabled = 1 
+			');
+		}
+		// always hide template sets in article
+		t3lib_extMgm::addPageTSConfig('
+			TCEFORM.tx_newspaper_article.template_set.disabled = 1 
+		');
+	
+	
+		
+		// set start module
+		t3lib_extMgm::addUserTSConfig('
+			setup.default.startModule = txnewspaperMmain_txnewspaperM2
+			setup.override.startModule = txnewspaperMmain_txnewspaperM2
+		');
+		
+		
+		// remove some save&new buttons
+		t3lib_extMgm::addUserTSConfig('
+			options.saveDocNew.tx_newspaper_article=0
 		');
 	}
-	// always hide template sets in article
-	t3lib_extMgm::addPageTSConfig('
-		TCEFORM.tx_newspaper_article.template_set.disabled = 1 
-	');
-
-
-	
-	// set start module
-	t3lib_extMgm::addUserTSConfig('
-		setup.default.startModule = txnewspaperMmain_txnewspaperM2
-		setup.override.startModule = txnewspaperMmain_txnewspaperM2
-	');
 	
 	
-	// remove some save&new buttons
-	t3lib_extMgm::addUserTSConfig('
-		options.saveDocNew.tx_newspaper_article=0
-	');
-	
-	
-	
+//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_recordlocking.php']['pre_release_lock'] = 'tx_newspaper_Typo3Hook->releaseLocks'; 	
 ?>
