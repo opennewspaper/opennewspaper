8318d390e155705e9753727c0554aa84653d012a
Lene Preuss <lene.preuss@gmail.com>
2010-09-21 11:05:13 +0000

    debug++

--- tests/class.ArticleList_testcase.php
+++ tests/class.ArticleList_testcase.php
@@ -29,6 +29,7 @@ class test_ArticleList_testcase extends tx_newspaper_database_testcase {
 
         $row = tx_newspaper::selectRows('uid', 'tx_newspaper_articlelist');
         $old_count = count($row);
+        print_r($row);
 
         $al = tx_newspaper_ArticleList_Factory::getInstance()->create($row['uid']);
         $al->store();
