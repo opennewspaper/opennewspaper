52263144b1be162750735bf4e270dbb380b817a1
Lene Preuss <lene.preuss@gmail.com>
2010-10-12 10:07:51 +0000

    removed crud that was left from copying code from the control tag extra

--- classes/extra/class.tx_newspaper_extra_container.php
+++ classes/extra/class.tx_newspaper_extra_container.php
@@ -8,39 +8,22 @@ require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_extra.ph
  *  - \p extras (UIDs of tx_newspaper_Extra records)
  */
 class tx_newspaper_Extra_Container extends tx_newspaper_Extra {
-	
-	///	SQL table matching tx_newspaer_Extra s to Control Tags and Tag Zones
-	const controltag_to_extra_table = 'tx_newspaper_controltag_to_extra';
-	///	SQL table n which Tag Zones are stored
-	const tag_zone_table = 'tx_newspaper_tag_zone';
-	///	SQL table in which tx_newspaper_Tag s are stored
-	const tag_table = 'tx_newspaper_tag';
-	///	SQL table associating tx_newspaper_Tag s with tx_newspaper_Article s 
-	const article_tag_mm_table = 'tx_newspaper_article_tags_mm';
 
-	///	\p tag_type field's value for Control Tags
-	const control_tag_type = 'control';
-		
 	public function __construct($uid = 0) {
 		if ($uid) {
 			parent::__construct($uid); 
 		}
 	}
-	
+
 	public function __toString() {
-		try {
-		return 'Extra: UID ' . $this->getExtraUid() . ', Control tag Extra: UID ' . $this->getUid() .
-				' (Tag zone: ' . $this->getAttribute('tag_zone') . ')';
-		} catch(Exception $e) {
-			return "ControlTagZone: Exception thrown!" . $e;
-		}	
+		return 'Container::__toString()';
 	}
 	
 	/// Assigns extras to be rendered to the smarty template and renders it.
 	/** If no Extras match, returns nothing.
 	 * 
 	 *  Smarty template:
-	 *  \include res/templates/tx_newspaper_extra_controltagzone.tmpl
+	 *  \include res/templates/tx_newspaper_extra_container.tmpl
 	 */
 	public function render($template_set = '') {
         
@@ -73,11 +56,7 @@ class tx_newspaper_Extra_Container extends tx_newspaper_Extra {
 
 	/// Displays the Tag Zone operating on.
 	public function getDescription() {
-		$tag_zone = tx_newspaper::selectOneRow(
-			'name', self::tag_zone_table, 
-			'uid = ' . $this->getAttribute('extras')
-		);
-		return $this->getTitle() . '(' . $tag_zone['name'] . ')';
+		return $this->__toString();
 	}
 
 	public static function getModuleName() {
