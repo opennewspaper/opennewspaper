5a77717fb192e65dc3e0d2ed5b722d80c369611e
Lene Preuss <lene.preuss@gmail.com>
2010-12-10 14:33:11 +0000

    added stub for testcase for dependency tree

---
+++ tests/class.DependencyTree_testcase.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once(PATH_typo3conf . 'ext/newspaper/tests/class.tx_newspaper_database_testcase.php');
+/// testsuite for class tx_newspaper_pagezone
+class test_DependencyTree_testcase extends tx_newspaper_database_testcase {
+
+    public function setUp() {
+        parent::setUp();
+        $sectionUid = tx_newspaper::insertRows('tx_newspaper_section', array('section_name' => 'dunmy'));
+        $this->dummySection = new tx_newspaper_Section($sectionUid);
+    }
+
+    public function tearDown() {
+        $this->clearDatabase();
+    }
+
+    public function test_getPages() {
+        
+        $uid = $this->fixture->getArticleUid();
+        $article = new tx_newspaper_Article($this->uid);
+        
+        $tree = tx_newspaper_DependencyTree::generateFromArticle($article);
+        
+        
+    }
+    private $dummySection;
+
+}
+
+?>
