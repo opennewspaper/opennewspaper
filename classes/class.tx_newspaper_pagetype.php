1c4bee7bae247fcf905b425571ee22236a2c023b
Lene Preuss <lene.preuss@gmail.com>
2010-12-13 15:07:10 +0000

    debug++

--- classes/class.tx_newspaper_pagetype.php
+++ classes/class.tx_newspaper_pagetype.php
@@ -165,6 +165,8 @@ class tx_newspaper_PageType implements tx_newspaper_StoredObject {
 
     static public function getArticlePageType() {
         $pagetypes = self::getAvailablePageTypes();
+        				echo '<p>'; print_r($pagetypes); echo '</p>';
+        
         foreach ($pagetypes as $pagetype) {
             if ($pagetype->getAttribute('is_article_page')) return $pagetype;
         }
