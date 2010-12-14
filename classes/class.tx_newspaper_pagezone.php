5b2e9e71c539c1f4526297eda89fd1ad27eae82d
Lene Preuss <lene.preuss@gmail.com>
2010-12-14 15:17:46 +0000

    debug--

--- classes/class.tx_newspaper_pagezone.php
+++ classes/class.tx_newspaper_pagezone.php
@@ -840,14 +840,12 @@ abstract class tx_newspaper_PageZone implements tx_newspaper_ExtraIface {
 	 *  \return UID of abstract PageZone record
 	 */ 
 	public function createPageZoneRecord() {
-		print('createPageZoneRecord()<br/>');
 		/// Check if record is already present in page zone table
 		$row = tx_newspaper::selectZeroOrOneRows(
 			'uid', 'tx_newspaper_pagezone', 
 			'pagezone_table = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($this->getTable(), $this->getTable()) .
 			' AND pagezone_uid = ' . $this->getUid()	
 		);
-		print_r($row);
 		if ($row['uid']) return $row['uid'];
 		
 		/// read typo3 fields to copy into page zone table
