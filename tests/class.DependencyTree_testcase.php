154105675b949ba79ac5f83d697068f1edef0e96
Lene Preuss <lene.preuss@gmail.com>
2011-02-11 17:40:56 +0000

    debug--

--- tests/class.DependencyTree_testcase.php
+++ tests/class.DependencyTree_testcase.php
@@ -183,11 +183,8 @@ class test_DependencyTree_testcase extends tx_newspaper_database_testcase {
         $tree->executeActionsOnPages();
 
         $this->checkIsPageArray($this->called_pages);
-echo sizeof ($this->called_pages)."<br />";
-        $this->checkIsArticlePageForSection($this->called_pages[0]);
 
         foreach ($this->called_pages as $i => $page) {
-            echo "$i " . print_r($page->getGETParameters(), 1) . "<br />";
             $this->checkIsArticlePageForSection($page);
         }
     }
