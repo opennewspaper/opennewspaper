7ba23f12f607ce882c89713109d606ed17497537
Ulf Schleth <ulf@einsnull.de>
2011-03-04 11:54:33 +0000

    fertig?

--- classes/extra/class.tx_newspaper_extra_html.php
+++ classes/extra/class.tx_newspaper_extra_html.php
@@ -1,12 +1,15 @@
 <?php
-
+ 
 require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_extra.php');
-
+ 
 /// tx_newspaper_Extra that renders 
-/** This Extra is used to place ads in an article 
- */  
+/** This Extra is used to place HTML in an article 
+ */
+
 class tx_newspaper_Extra_HTML extends tx_newspaper_Extra {
 
+	const description_length = 50; 
+	
 	/// Constructor
 	public function __construct($uid = 0) {
 		if ($uid) {
@@ -22,17 +25,19 @@ class tx_newspaper_Extra_HTML extends tx_newspaper_Extra {
 		}	
 	}
 	
-	/** Render an empty ad.
+	/** Render a piece of HTML.
 	/*  Smarty template:
-	 *  \include res/templates/tx_newspaper_extra_ad.tmpl
+	 *  \include res/templates/tx_newspaper_extra_html.tmpl
 	 */	
 	public function render($template_set = '') {
-        
+				
 		$rendered = $this->getAttribute('html');
-
-tx_newspaper::devlog("render", $rendered);
 		
-/* nur für extras mit template
+		## die folgende zeile sorgt nach entfernung der Auskommentierung  ggfs. für eine 
+		# Fehlerausgabe im Typo3-Developer-Log:
+		#  tx_newspaper::devlog("render", $rendered); 
+		
+		/** 
 		$this->prepare_render($template_set);
 		
 		$template = $this->getAttribute('template');
@@ -44,14 +49,22 @@ tx_newspaper::devlog("render", $rendered);
 			$template = $this;
 		}
 
-        $rendered = $this->smarty->fetch($template);
-     
-*/
+        $rendered = $this->smarty->fetch($template);   
+		*/
         
         return $rendered;
 	}
 
-	/// title for module
+	/** Displays the title and the beginning of the text.
+	 */
+	public function getDescription() {
+		return substr(
+		
+			'<strong>' . $this->getAttribute('title') . '</strong> ' . $this->getAttribute('notes'), 
+			0, self::description_length+2*strlen('<strong>')+1);
+	}
+	
+		/// title for module
 	public static function getModuleName() {
 		return 'np_extra_html';
 	}
