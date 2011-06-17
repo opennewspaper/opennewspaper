65bb8961503721190e545f9fada247b40008dc57
Lene Preuss <lene.preuss@gmail.com>
2011-06-17 12:27:39 +0000

    debug++ [#1573]

--- classes/class.tx_newspaper_pagezone.php
+++ classes/class.tx_newspaper_pagezone.php
@@ -1143,7 +1143,7 @@ tx_newspaper::devlog("findExtraByOriginUID($origin_uid, $hidden_too)");
      *        inheritance mode has been set to false
      */
     public function getExtras($hidden_too = false) {
-    	if (!$this->extras || $hidden_too) {
+    	if (empty($this->extras) || $hidden_too) {
             $this->readExtras($this->getUid(), $hidden_too);
         }
 
