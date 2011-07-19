feca10ea29957b4c0165760a1b0d125ccd05f075
Lene Preuss <lene.preuss@gmail.com>
2011-07-19 10:57:57 +0000

    tx_newspaper::buildTSFE() is not enough. also init ->sys_page [#1661]

--- tests/class.Article_testcase.php
+++ tests/class.Article_testcase.php
@@ -37,6 +37,8 @@ class test_Article_testcase extends tx_newspaper_database_testcase {
 	
 	public function test_render() {
         tx_newspaper::buildTSFE();
+        $GLOBALS['TSFE']->sys_page = new t3lib_pageSelect();
+        $GLOBALS['TSFE']->sys_page->init(false);
 		try {
 			$this->checkOutput($this->article->render());
 		} catch (tx_newspaper_Exception $e) {
