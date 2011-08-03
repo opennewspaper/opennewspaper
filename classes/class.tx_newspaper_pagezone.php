600ba32de4db798ca527f097554e03678963c723
Lene Preuss <lene.preuss@gmail.com>
2011-08-03 14:18:01 +0000

    debug++ [#1661]

--- classes/class.tx_newspaper_pagezone.php
+++ classes/class.tx_newspaper_pagezone.php
@@ -1128,6 +1128,7 @@ abstract class tx_newspaper_PageZone implements tx_newspaper_ExtraIface {
      */
 	final protected function findExtraByOriginUID($origin_uid, $hidden_too = false) {
 		foreach ($this->getExtras($hidden_too) as $extra) {
+            echo "origin uid: " . $extra->getOriginUid() . ", wanted: $origin_uid";
 			if ($extra->getOriginUid() == $origin_uid) return $extra;
 		}
 		return null;
