e0d0b7aee57a32aae19b3e22f9aaa685071a2d22
Lene Preuss <lene.preuss@gmail.com>
2011-12-13 15:10:54 +0000

    moved more non-interface classes out of the main directory [#1728]

--- tx_newspaper_include.php
+++ tx_newspaper_include.php
@@ -190,7 +190,7 @@ require_once(PATH_typo3conf . 'ext/newspaper/classes/private/class.tx_newspaper_
 require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_article.php');
 require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_articletype.php');
 require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_sysfolder.php');
-require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_util_mod.php'); // \todo: why is this class used in frontend? (see #1019)
+require_once(PATH_typo3conf . 'ext/newspaper/classes/private/class.tx_newspaper_util_mod.php'); // \todo: why is this class used in frontend? (see #1019)
 
 if (TYPO3_MODE == 'BE')	{
 	require_once(PATH_typo3conf . 'ext/newspaper/classes/private/class.tx_newspaper_be.php');
