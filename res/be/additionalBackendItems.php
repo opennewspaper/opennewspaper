9683a706b6766134637ca50e897ead307c769460
Lene Preuss <lene.preuss@gmail.com>
2011-12-14 04:07:58 +0000

    fixed path [#1728]

--- res/be/additionalBackendItems.php
+++ res/be/additionalBackendItems.php
@@ -4,7 +4,7 @@
 		die ('Access denied.');
 
 	if (TYPO3_MODE == 'BE') {
-		require_once(PATH_typo3conf . 'ext/newspaper/classes/class.tx_newspaper_be.php');
+		require_once(PATH_typo3conf . 'ext/newspaper/classes/private/class.tx_newspaper_be.php');
 		tx_newspaper_BE::addAdditionalScriptToBackend(); // add additional script (if needed) to top of backend
 	}
 
