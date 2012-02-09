57dc2509dfd305105aaeafd222a9e54138140ccf
Lene Preuss <lene.preuss@gmail.com>
2012-02-09 17:26:33 +0000

    created PHP file to execute the asynchronous task [#1691]

---
+++ util/execute_asynchronously.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Author: Lene Preuss <lene.preuss@gmail.com>
+ * Date:   2/9/12
+ * Time:   6:22 PM
+ */
+ 
+echo "class: " . $argv[1] . ", method: " . $argv[2] . ", args: " . $argv[3];
+
