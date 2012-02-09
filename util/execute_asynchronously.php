66dc21c4e00e77e5b20d9e51884b5b01a63d69ed
Lene Preuss <lene.preuss@gmail.com>
2012-02-09 19:07:29 +0000

    include class definitions [#1691]

--- util/execute_asynchronously.php
+++ util/execute_asynchronously.php
@@ -5,7 +5,31 @@
  * Time:   6:22 PM
  */
 
+
+$DIR = dirname(__FILE__);
+
+/*
+$dirs = explode('/', $DIR);
+$mod_path = '';
+
+$i = 0;
+while ($dirs[$i] != 'typo3conf') {
+    unset($dirs[$i]);
+    $i++;
+}
+print_r($dirs);
+
+$mod_path = implode('/', $dirs) . '/typo3';
+
+define('TYPO3_MOD_PATH', $mod_path . '/');
+
+require_once(dirname(__FILE__) . '/../../../../typo3_src/typo3/init.php');
+*/
+
+define('PATH_typo3conf', $DIR . '/../../../');
+
 require_once(dirname(__FILE__) . '/../classes/private/class.tx_newspaper_file.php');
+require_once(dirname(__FILE__) . '/../tests/class.AsynchronousTask_testcase.php');
 
 function getObjectFromObjectFile($filename) {
     $serialized_file = new tx_newspaper_File($filename);
