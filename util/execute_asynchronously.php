f2612516055df01005501d001d99eb5db7a2fdc5
Lene Preuss <lene.preuss@gmail.com>
2012-02-09 17:39:44 +0000

    include tx_newspaper_file definition and unserialize the requested object [#1691]

--- util/execute_asynchronously.php
+++ util/execute_asynchronously.php
@@ -5,13 +5,15 @@
  * Time:   6:22 PM
  */
 
+require_once(__DIR__ . '/../classes/private/class.tx_newspaper_file.php');
+
 function getObjectFromObjectFile($filename) {
     $serialized_file = new tx_newspaper_File($filename);
     $serialized_object = $serialized_file->read();
     return unserialize($serialized_object);
 }
 
-#$object = getObjectFromObjectFile($argv[1]);
+$object = getObjectFromObjectFile($argv[1]);
 
 echo "cwd" . getcwd() . ", class: " . get_class($object) . ", method: " . $argv[2] . ", args: " . $argv[3];
 
