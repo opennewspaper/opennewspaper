731eaf7df7ee589c18f420e0e866454f3ec86a36
Lene Preuss <lene.preuss@gmail.com>
2012-02-10 11:48:36 +0000

    included typo3's init.php manually in the file [#1691]

--- util/execute_asynchronously.sh
+++ util/execute_asynchronously.sh
@@ -4,4 +4,9 @@ object_file=$1
 method=$2
 args=$3
 
-nohup php execute_asynchronously.php $1 $2 $3 &
+basedir="$(dirname "$0")"
+echo nohup php execute_asynchronously.php "$1" "$2" "$3" >> /tmp/execute_asynchronously.log
+
+# nohup php execute_asynchronously.php "$1" "$2" "$3" &
+
+php "$basedir"/execute_asynchronously.php "$1" "$2" "$3"
