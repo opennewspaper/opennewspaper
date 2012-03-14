2013793a414eb3708bba622e614177ff124a7396
Lene Preuss <lene.preuss@gmail.com>
2012-03-14 14:25:46 +0000

    refactor [#1777]

--- tests/class.ArticleList_testcase.php
+++ tests/class.ArticleList_testcase.php
@@ -47,39 +47,27 @@ class test_ArticleList_testcase extends tx_newspaper_database_testcase {
 
     public function test_automaticArticleListGetArticles() {
 
-        $row = tx_newspaper::selectRows('*', 'tx_newspaper_articlelist' , "list_table = 'tx_newspaper_articlelist_semiautomatic'");
-        $this->assertGreaterThan(0, intval($row[0]['uid']), 'No automatic article list found: ' . print_r($row, 1));
+        $al = $this->createAutomaticArticleList();
 
-        $al = tx_newspaper_ArticleList_Factory::getInstance()->create($row[0]['uid']);
-        $articles_before = self::makeUIDarray($al->getArticles(2));
+        $articles_before = $this->getFirstTwoArticleUIDs($al);
 
-        $this->assertEquals(2, sizeof($articles_before), 'Need at least 2 articles in list: ' . print_r($articles_before, 1));
+        $articles_swapped = self::getFirstTwoEntriesSwapped($articles_before);
 
-        $articles_swapped = array();
-        $articles_swapped[0] = $articles_before[1];
-        $articles_swapped[1] = $articles_before[0];
-
-        $uids = array(
-            array($articles_swapped[0], -1),
-            array($articles_swapped[1],  0),
+        $al->assembleFromUIDs(
+            array(
+                array($articles_swapped[0], -1),
+                array($articles_swapped[1],  0),
+            )
         );
 
-        $al->assembleFromUIDs($uids);
+        $articles_after = $this->getFirstTwoArticleUIDs($al);
 
-        $articles_after = self::makeUIDarray($al->getArticles(2));
-        $this->assertEquals(
-            sizeof($articles_swapped), sizeof($articles_after),
-            'Size of articles in list after assembleFromUids() (' . sizeof($articles_after) .
-            ') does not match size of UID array (' . sizeof($articles_swapped) . ')'
-        );
-
-        $mm = tx_newspaper::selectRows('*', 'tx_newspaper_articlelist_semiautomatic_articles_mm');
         for ($i = 0; $i < sizeof($articles_swapped); $i++) {
             $this->assertTrue(
                 $articles_swapped[$i] == $articles_after[$i],
                 "article $i is not equal after assembleFromUids(): " .
                         print_r($articles_swapped, 1) . " != " . print_r($articles_after, 1) .
-                        'MM table: ' . print_r($mm, 1)
+                        'MM table: ' . print_r(tx_newspaper::selectRows('*', 'tx_newspaper_articlelist_semiautomatic_articles_mm'), 1)
             );
         }
 
@@ -92,6 +80,23 @@ class test_ArticleList_testcase extends tx_newspaper_database_testcase {
         self::$num_article_lists = count($row);
         $latest = array_pop($row);
         return intval($latest['uid']);
+    }
+
+    private function createAutomaticArticleList() {
+        $row = tx_newspaper::selectRows('*', 'tx_newspaper_articlelist', "list_table = 'tx_newspaper_articlelist_semiautomatic'");
+        $this->assertGreaterThan(0, intval($row[0]['uid']), 'No automatic article list found: ' . print_r($row, 1));
+
+        return tx_newspaper_ArticleList_Factory::getInstance()->create($row[0]['uid']);
+    }
+
+    private function getFirstTwoArticleUIDs(tx_newspaper_ArticleList $al) {
+        $articles_before = self::makeUIDarray($al->getArticles(2));
+        $this->assertEquals(2, sizeof($articles_before), 'Need at least 2 articles in list: ' . print_r($articles_before, 1));
+        return $articles_before;
+    }
+
+    private static function getFirstTwoEntriesSwapped(array $array) {
+        return array($array[1], $array[0]);
     }
 
     private static function makeUIDarray(array $articles) {
