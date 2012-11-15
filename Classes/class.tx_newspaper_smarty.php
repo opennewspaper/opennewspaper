6c6821a18f0390d984e41abec331d6a8fd654d87
Oliver Schröder <oliver@schroederbros.de>
2012-11-15 14:37:01 +0000

    Removed bug from last commit (#1925)

--- Classes/class.tx_newspaper_smarty.php
+++ Classes/class.tx_newspaper_smarty.php
@@ -217,11 +217,6 @@ self::debug_search_path && tx_newspaper::devlog('setPageZoneType ' . $pagezone_t
 
     private function setBasePath() {
 
-        // Make sure TSFE is available
-        if (!$GLOBALS['TSFE']) {
-            tx_newspaper::buildTSFE();
-        }
-
         // @todo: Check if $GLOBALS['TSFE']->id would work ($GLOBALS['TSFE']->page['uid'] not working for Windows installations)
         $pageUid = $GLOBALS['TSFE']->page['uid']? $GLOBALS['TSFE']->page['uid'] : $GLOBALS['TSFE']->id;
         $TSConfig = t3lib_BEfunc::getPagesTSconfig($pageUid);
