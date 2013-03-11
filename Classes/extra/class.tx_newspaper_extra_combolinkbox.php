0a0fa87999e1dc7c23e75f8ff79d628d6f6a5144
Lene Preuss <lene.preuss@gmail.com>
2013-03-11 12:04:38 +0000

    zugriff auf die einzelnen linkkategorien public machen [#2052]

--- Classes/extra/class.tx_newspaper_extra_combolinkbox.php
+++ Classes/extra/class.tx_newspaper_extra_combolinkbox.php
@@ -21,87 +21,87 @@ require_once(PATH_typo3conf . 'ext/newspaper/Classes/extra/class.tx_newspaper_ex
  */
 class tx_newspaper_Extra_ComboLinkBox extends tx_newspaper_Extra {
 
-	const article_table = 'tx_newspaper_article';
+    const article_table = 'tx_newspaper_article';
 
-	public function __construct($uid = 0) {
-		if ($uid) {
-			parent::__construct($uid);
-		}
-	}
+    public function __construct($uid = 0) {
+        if ($uid) {
+            parent::__construct($uid);
+        }
+    }
 
-	public function __toString() {
-		try {
-		return 'Extra: UID ' . $this->getExtraUid() . ', Combo link box Extra: UID ' . $this->getUid();
-		} catch(Exception $e) {
-			return "ComboLinkBox: Exception thrown!" . $e;
-		}
-	}
+    public function __toString() {
+        try {
+        return 'Extra: UID ' . $this->getExtraUid() . ', Combo link box Extra: UID ' . $this->getUid();
+        } catch(Exception $e) {
+            return "ComboLinkBox: Exception thrown!" . $e;
+        }
+    }
 
-	/// Assigns Articles and Links to the smarty template and renders it.
-	/** Smarty template:
-	 *  \include res/templates/tx_newspaper_extra_combolinkbox.tmpl
-	 */
-	public function render($template_set = '') {
+    /// Assigns Articles and Links to the smarty template and renders it.
+    /** Smarty template:
+     *  \include res/templates/tx_newspaper_extra_combolinkbox.tmpl
+     */
+    public function render($template_set = '') {
 
         tx_newspaper_ExecutionTimer::start();
 
-		$this->getAttribute('uid');
+        $this->getAttribute('uid');
 
-		$this->prepare_render($template_set);
+        $this->prepare_render($template_set);
 
-		if ($this->getAttribute('show_related_articles') &&
-			intval(t3lib_div::_GP(tx_newspaper::GET_article()))) {
+        if ($this->getAttribute('show_related_articles') &&
+            intval(t3lib_div::_GP(tx_newspaper::GET_article()))) {
 
-			$this->smarty->assign('related_articles', $this->getRelatedArticles());
-		}
+            $this->smarty->assign('related_articles', $this->getRelatedArticles());
+        }
 
-		if ($this->getAttribute('manually_selected_articles')) {
-			$this->smarty->assign('manually_selected_articles', $this->getManuallySelectedArticles());
-		}
+        if ($this->getAttribute('manually_selected_articles')) {
+            $this->smarty->assign('manually_selected_articles', $this->getManuallySelectedArticles());
+        }
 
-		if ($this->getAttribute('internal_links')) {
-			$this->smarty->assign('internal_links', $this->getInternalLinks());
-		}
+        if ($this->getAttribute('internal_links')) {
+            $this->smarty->assign('internal_links', $this->getInternalLinks());
+        }
 
-		if ($this->getAttribute('external_links')) {
-			$this->smarty->assign('external_links', $this->getExternalLinks());
-		}
+        if ($this->getAttribute('external_links')) {
+            $this->smarty->assign('external_links', $this->getExternalLinks());
+        }
 
         $rendered = $this->smarty->fetch($this->getSmartyTemplate());
 
         tx_newspaper_ExecutionTimer::logExecutionTime();
 
         return $rendered;
-	}
+    }
 
-	/// Displays the Tag Zone operating on.
-	public function getDescription() {
-		return "<p><strong>" . $this->getAttribute('title') . "</strong></p>" . "<p>" . $this->getAttribute('short_description') . "</p>";
-	}
+    /// Displays the Tag Zone operating on.
+    public function getDescription() {
+        return "<p><strong>" . $this->getAttribute('title') . "</strong></p>" . "<p>" . $this->getAttribute('short_description') . "</p>";
+    }
 
-	public static function getModuleName() {
-		return 'np_combo_link_box';
-	}
+    public static function getModuleName() {
+        return 'np_combo_link_box';
+    }
 
-	public static function dependsOnArticle() { return true; }
+    public static function dependsOnArticle() { return true; }
 
-	////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
-	private function getRelatedArticles() {
+    public function getRelatedArticles() {
         $current_article = new tx_newspaper_Article(t3lib_div::_GP(tx_newspaper::GET_article()));
 
         return $current_article->getRelatedArticles();
-	}
+    }
 
-	private function getManuallySelectedArticles() {
+    public function getManuallySelectedArticles() {
         $articles = array();
         foreach ($this->getManuallySelectedArticleUids() as $article_uid) {
             $articles[] = new tx_newspaper_Article(intval(trim($article_uid)));
         }
         return $articles;
-	}
+    }
 
-    private function getManuallySelectedArticleUids() {
+    public function getManuallySelectedArticleUids() {
         $rows = tx_newspaper::selectRows(
             'uid',
             'tx_newspaper_article',
@@ -112,12 +112,12 @@ class tx_newspaper_Extra_ComboLinkBox extends tx_newspaper_Extra {
         return array_map('array_pop', $rows);
     }
 
-    private function getInternalLinks() {
+    public function getInternalLinks() {
         return self::getLinks($this->getAttribute('internal_links'));
     }
 
-    private function getExternalLinks() {
-    	return self::getLinks($this->getAttribute('external_links'));
+    public function getExternalLinks() {
+        return self::getLinks($this->getAttribute('external_links'));
     }
 
     private static function getLinks($links_csv) {
