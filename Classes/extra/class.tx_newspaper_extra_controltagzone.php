26a814ff43ec9e734f0b5c801cf3e28b09129d10
Lene Preuss <lene.preuss@gmail.com>
2013-04-25 16:51:25 +0000

    getExtraWhichContains($extra) for tag zone extra [#2012]

--- Classes/extra/class.tx_newspaper_extra_controltagzone.php
+++ Classes/extra/class.tx_newspaper_extra_controltagzone.php
@@ -23,172 +23,194 @@ require_once(PATH_typo3conf . 'ext/newspaper/Classes/class.tx_newspaper_extra.ph
  *  - \p tag_type (string)
  *  - \p default_extra (UIDs of tx_newspaper_Extra records)
  */
-class tx_newspaper_Extra_ControlTagZone extends tx_newspaper_Extra {
+class tx_newspaper_Extra_ControlTagZone extends tx_newspaper_Extra implements tx_newspaper_ContainerExtra {
 
-	///	SQL table matching tx_newspaer_Extra s to Control Tags and Tag Zones
-	const controltag_to_extra_table = 'tx_newspaper_controltag_to_extra';
-	///	SQL table n which Tag Zones are stored
-	const tag_zone_table = 'tx_newspaper_tag_zone';
-	///	SQL table in which tx_newspaper_Tag s are stored
-	const tag_table = 'tx_newspaper_tag';
-	///	SQL table associating tx_newspaper_Tag s with tx_newspaper_Article s
-	const article_tag_mm_table = 'tx_newspaper_article_tags_mm';
+    ///    SQL table matching tx_newspaer_Extra s to Control Tags and Tag Zones
+    const controltag_to_extra_table = 'tx_newspaper_controltag_to_extra';
+    ///    SQL table n which Tag Zones are stored
+    const tag_zone_table = 'tx_newspaper_tag_zone';
+    ///    SQL table in which tx_newspaper_Tag s are stored
+    const tag_table = 'tx_newspaper_tag';
+    ///    SQL table associating tx_newspaper_Tag s with tx_newspaper_Article s
+    const article_tag_mm_table = 'tx_newspaper_article_tags_mm';
 
-	public function __construct($uid = 0) {
-		if ($uid) {
-			parent::__construct($uid);
-		}
-	}
+    public function __construct($uid = 0) {
+        if ($uid) {
+            parent::__construct($uid);
+        }
+    }
 
-	public function __toString() {
-		try {
-		return 'Extra: UID ' . $this->getExtraUid() . ', Control tag Extra: UID ' . $this->getUid() .
-				' (Tag zone: ' . $this->getAttribute('tag_zone') . ')';
-		} catch(Exception $e) {
-			return "ControlTagZone: Exception thrown!" . $e;
-		}
-	}
+    public function __toString() {
+        try {
+        return 'Extra: UID ' . $this->getExtraUid() . ', Control tag Extra: UID ' . $this->getUid() .
+                ' (Tag zone: ' . $this->getAttribute('tag_zone') . ')';
+        } catch(Exception $e) {
+            return "ControlTagZone: Exception thrown!" . $e;
+        }
+    }
 
-	/// Assigns extras to be rendered to the smarty template and renders it.
-	/** If no Extras match, returns nothing.
-	 *
-	 *  Smarty template:
-	 *  \include res/templates/tx_newspaper_extra_controltagzone.tmpl
-	 */
-	public function render($template_set = '') {
+    /// Assigns extras to be rendered to the smarty template and renders it.
+    /** If no Extras match, returns nothing.
+     *
+     *  Smarty template:
+     *  \include res/templates/tx_newspaper_extra_controltagzone.tmpl
+     */
+    public function render($template_set = '') {
 
-		$control_tags = $this->getControlTags();
-		$extras = $this->getExtras($control_tags);
+        $extras = $this->getExtras($this->getControlTags());
 
-		if (!$extras) return;
+        if (!$extras) return;
 
-		$rendered_extras = array();
-		foreach ($extras as $tag => $extra) {
-			$extra->assignSmartyVar(array('dossier_link' => self::getDossierLink($tag)));
-			$rendered_extras[] = $extra->render();
-		}
+        $rendered_extras = array();
+        foreach ($extras as $tag => $extra) {
+            $extra->assignSmartyVar(array('dossier_link' => self::getDossierLink($tag)));
+            $rendered_extras[] = $extra->render();
+        }
 
-		$this->prepare_render($template_set);
+        $this->prepare_render($template_set);
 
-		$this->smarty->assign('extras', $rendered_extras);
+        $this->smarty->assign('extras', $rendered_extras);
 
         $rendered = $this->smarty->fetch($this->getSmartyTemplate());
 
         return $rendered;
-	}
+    }
 
-	/// Displays the Tag Zone operating on.
-	public function getDescription() {
-		try {
-			$tag_zone = tx_newspaper::selectOneRow(
-				'name', self::tag_zone_table,
-				'uid = ' . $this->getAttribute('tag_zone')
-			);
-			return ($this->getAttribute('short_description')? $this->getAttribute('short_description') . '<br />' : '') .
-				' (' . $tag_zone['name'] . ')';
-		} catch (tx_newspaper_DBException $e) {
-			return ($this->getAttribute('short_description')? $this->getAttribute('short_description') . '<br />' : '') . ' (' .
-				   tx_newspaper::getTranslation('message_no_controltag_selected') .
-				   ')';
-		}
+    /// Displays the Tag Zone operating on.
+    public function getDescription() {
+        try {
+            $tag_zone = tx_newspaper::selectOneRow(
+                'name', self::tag_zone_table,
+                'uid = ' . $this->getAttribute('tag_zone')
+            );
+            return ($this->getAttribute('short_description')? $this->getAttribute('short_description') . '<br />' : '') .
+                ' (' . $tag_zone['name'] . ')';
+        } catch (tx_newspaper_DBException $e) {
+            return ($this->getAttribute('short_description')? $this->getAttribute('short_description') . '<br />' : '') . ' (' .
+                   tx_newspaper::getTranslation('message_no_controltag_selected') .
+                   ')';
+        }
 
-	}
+    }
 
-	public static function getModuleName() {
-		return 'np_control_tag_extra';
-	}
+    public function contains(tx_newspaper_Extra $extra) {
+        return in_array($extra, $this->getExtras($this->getControlTags()));
+    }
 
-	public static function dependsOnArticle() { return false; }
+    public static function getExtraWhichContains(tx_newspaper_Extra $extra) {
 
-	////////////////////////////////////////////////////////////////////////////
+        $tag_zones = tx_newspaper_DB::getInstance()->selectRows(
+            'tag_zone, tag_type', self::controltag_to_extra_table, 'extra = ' . $extra->getUid()
+        );
+        if (empty($tag_zones)) return null;
 
-	/// Find out which control tags are currently active
-	/** Reads the Control Tags associated with the currently displayed Article
-	 *  from the article_tag_mm_table.
-	 *  \return UIDs of control tags for the currently displayed Article
-	 */
-	private function getControlTags() {
-		$tag_uids = array();
+        foreach ($tag_zones as $tag_zone) {
+            $uids = tx_newspaper_DB::getInstance()->selectRows(
+                'uid', 'tx_newspaper_extra_controltagzone',
+                'tag_zone = "' . $tag_zone['tag_zone'] .'" AND tag_type = "' . $tag_zone['tag_type'] . '"'
+            );
+            $uid = intval($uids['uid']);
+            if ($uid) return new tx_newspaper_Extra_ControlTagZone($uid);
+        }
 
-		if (intval(t3lib_div::_GP(tx_newspaper::GET_article()))) {
-			$article = new tx_newspaper_article(t3lib_div::_GP(tx_newspaper::GET_article()));
-			$tags = tx_newspaper::selectRows(
-				self::tag_table . '.uid',
-				self::tag_table .
-					' JOIN ' . self::article_tag_mm_table .
-					' ON ' . self::tag_table . '.uid = ' . self::article_tag_mm_table . '.uid_foreign',
-				self::article_tag_mm_table . '.uid_local = ' . $article->getUid() .
-				' AND ' . self::tag_table . '.tag_type = \'' . tx_newspaper_tag::getControlTagType() .'\''
-			);
+        return new tx_newspaper_Extra_Container($uid);
+    }
 
-			foreach ($tags as $tag) $tag_uids[] = $tag['uid'];
-		} else {
+    public static function getModuleName() {
+        return 'np_control_tag_extra';
+    }
+
+    public static function dependsOnArticle() { return false; }
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    /// Find out which control tags are currently active
+    /** Reads the Control Tags associated with the currently displayed Article
+     *  from the article_tag_mm_table.
+     *  \return UIDs of control tags for the currently displayed Article
+     */
+    private function getControlTags() {
+        $tag_uids = array();
+
+        if (intval(t3lib_div::_GP(tx_newspaper::GET_article()))) {
+            $article = new tx_newspaper_article(t3lib_div::_GP(tx_newspaper::GET_article()));
+            $tags = tx_newspaper::selectRows(
+                self::tag_table . '.uid',
+                self::tag_table .
+                    ' JOIN ' . self::article_tag_mm_table .
+                    ' ON ' . self::tag_table . '.uid = ' . self::article_tag_mm_table . '.uid_foreign',
+                self::article_tag_mm_table . '.uid_local = ' . $article->getUid() .
+                ' AND ' . self::tag_table . '.tag_type = \'' . tx_newspaper_tag::getControlTagType() .'\''
+            );
+
+            foreach ($tags as $tag) $tag_uids[] = $tag['uid'];
+        } else {
             if (intval(t3lib_div::_GP('dossier'))) {
                 $tag_uids[] = intval(t3lib_div::_GP('dossier'));
             }
         }
 
-		return $tag_uids;
-	}
+        return $tag_uids;
+    }
 
-	///	Returns the Extras displayed for the Tag Zone of the object
-	/** \param $control_tags Control tags present
-	 *  \return Array of Extras which have been set up for the Tag Zone of the
-	 * 		tx_newspaper_Extra_ControlTagZone object and any of the control tags
-	 * 		in \p $control_tags. Extras for the first matching tag are returned,
-	 * 		the following tags are ignored.
-	 */
-	private function getExtras(array $control_tags) {
-		$extra = array();
+    /// Returns the Extras displayed for the Tag Zone of the object
+    /** @param $control_tags Control tags present
+     *  @return tx_newspaper_Extra[] Array of Extras which have been set up for the Tag
+     *      Zone of the tx_newspaper_Extra_ControlTagZone object and any of the control
+     *      tags in \p $control_tags. Extras for the first matching tag are returned,
+     *      the following tags are ignored.
+     */
+    private function getExtras(array $control_tags) {
+        $extra = array();
 
-		///	Check if an Extra is assigned for the current tag zone for any control tag
-		foreach ($control_tags as $control_tag) {
-			$extras_data = tx_newspaper::selectRows(
-				'extra', self::controltag_to_extra_table,
-				'tag = ' . $control_tag .
-				' AND tag_zone = ' . $this->getAttribute('tag_zone'),
-				'',
-				'sorting'
-			);
+        ///    Check if an Extra is assigned for the current tag zone for any control tag
+        foreach ($control_tags as $control_tag) {
+            $extras_data = tx_newspaper::selectRows(
+                'extra', self::controltag_to_extra_table,
+                'tag = ' . $control_tag .
+                ' AND tag_zone = ' . $this->getAttribute('tag_zone'),
+                '',
+                'sorting'
+            );
 
-			if ($extras_data) {
-				foreach ($extras_data as $extra_data) {
+            if ($extras_data) {
+                foreach ($extras_data as $extra_data) {
                     $extra[$control_tag] = tx_newspaper_Extra_Factory::getInstance()->create($extra_data['extra']);
-				}
-			}
-		}
+                }
+            }
+        }
 
-		///	Check if default Extra(s) are set
-		if (!$extra) {
-			if ($this->getAttribute('default_extra')) {
-				foreach (explode(',', $this->getAttribute('default_extra')) as $extra_uid) {
-					$extra[] = tx_newspaper_Extra_Factory::getInstance()->create($extra_uid);
-				}
-			}
-		}
-		return $extra;
-	}
+        ///    Check if default Extra(s) are set
+        if (!$extra) {
+            if ($this->getAttribute('default_extra')) {
+                foreach (explode(',', $this->getAttribute('default_extra')) as $extra_uid) {
+                    $extra[] = tx_newspaper_Extra_Factory::getInstance()->create($extra_uid);
+                }
+            }
+        }
+        return $extra;
+    }
 
-	/// Returns the link to the dossier page for the referenced tag
-	/** \param $tag $uid of the tag for which the dossier is assembled
-	 *  \return Link to the Typo3 page containing the dossier
-	 *  \throw tx_newspaper_IllegalUsageException if no dossier page is defined
-	 *   	in TSConfig
-	 *  \todo make GET parameter 'dossier' configurable
-	 */
-	static public function getDossierLink($tag) {
+    /// Returns the link to the dossier page for the referenced tag
+    /** \param $tag $uid of the tag for which the dossier is assembled
+     *  \return Link to the Typo3 page containing the dossier
+     *  \throw tx_newspaper_IllegalUsageException if no dossier page is defined
+     *       in TSConfig
+     *  \todo make GET parameter 'dossier' configurable
+     */
+    static public function getDossierLink($tag) {
 
         if ($tag instanceof tx_newspaper_Tag) $tag = $tag->getUid();
 
         $dossier_page = tx_newspaper::getDossierPageID();
 
-		$url = tx_newspaper::typolink_url(
-			array(
-				'id' => $dossier_page,
-				tx_newspaper::getDossierGETParameter() => $tag
-			));
-		return $url;
-	}
+        $url = tx_newspaper::typolink_url(
+            array(
+                'id' => $dossier_page,
+                tx_newspaper::getDossierGETParameter() => $tag
+            ));
+        return $url;
+    }
 
 }
 
