dd8cf1847ef46f3c5c2fa70f4f44eb81e021ba18
Lene Preuss <lene.preuss@gmail.com>
2013-10-17 13:21:27 +0200

    added a unit test stub for SectionModuleController::changeParent()

---
+++ tests/class.SectionModuleController_testcase.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Created on Oct 17, 2013
+ *
+ * Author: Lene Preuss <lene.preuss@gmail.com>
+ */
+
+require_once(PATH_typo3conf . 'ext/newspaper/Classes/Controller/SectionModuleController.php');
+require_once(PATH_typo3conf . 'ext/newspaper/tests/class.tx_newspaper_database_testcase.php');
+
+class test_SectionModuleController_testcase extends tx_newspaper_database_testcase {
+
+    function setUp() {
+    }
+    
+    function tearDown() {
+    }
+
+    public function test_changeParent() {
+    }
+    
+}
+?>
