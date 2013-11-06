106107a95d29f3fe2bb39f7bb1ff441ce276db4d
Lene Preuss <lene.preuss@gmail.com>
2013-11-06 12:31:08 +0100

    stub for test for chageParent() in SectionModuleController [#2134]

--- tests/class.SectionModuleController_testcase.php
+++ tests/class.SectionModuleController_testcase.php
@@ -11,14 +11,30 @@ require_once(PATH_typo3conf . 'ext/newspaper/tests/class.tx_newspaper_database_t
 class test_SectionModuleController_testcase extends tx_newspaper_database_testcase {
 
     function setUp() {
+        parent::setUp();
+        $this->controller = new Tx_newspaper_Controller_SectionModuleController();
     }
     
     function tearDown() {
     }
 
     public function test_changeParent() {
+
+        // change parent of grandchild section to parent section, see if extras change and all
+
+        // insert extra on child section that must be gone from the grandchild section after changing parent
+
+        // check all origin uids on grandchild section, must be != extra uids
+
+
+        $method = new ReflectionMethod($this->controller, 'changeParent');
+        $method->setAccessible(true);
+        $method->invoke($this->controller, new tx_newspaper_Section($this->fixture->getParentSectionUid()));
+
         $this->skipTest("Not yet implemented");
     }
-    
+
+    /** @var Tx_newspaper_Controller_SectionModuleController */
+    private $controller;
 }
 ?>
