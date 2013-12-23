cc5ae26fdd92e903f74ec5eff283db5f5289d52e
Lene Preuss <lene.preuss@gmail.com>
2013-12-23 12:22:15 +0100

    - refactoring - print debug info to temp file which cannot be printed to screen in new PHPunit versions [#2134]

--- tests/class.SectionModuleController_testcase.php
+++ tests/class.SectionModuleController_testcase.php
@@ -8,6 +8,71 @@
 require_once(PATH_typo3conf . 'ext/newspaper/Classes/Controller/SectionModuleController.php');
 require_once(PATH_typo3conf . 'ext/newspaper/tests/class.tx_newspaper_database_testcase.php');
 
+class InheritanceStructure {
+
+    public function __construct(tx_newspaper_fixture $fixture) {
+        $this->sections = $fixture->getAllSections();
+
+        $this->parent_section = $this->getSectionByName('Daddy Section');
+        $this->child_section = $this->getSectionByName('Son Section');
+        $this->grandchild_section = $this->getSectionByName('Grandchild Section');
+
+        $this->parent_zone = $fixture->getRandomPageZoneForPlacement($this->parent_section); #
+        $this->child_zone =  $fixture->getRandomPageZoneForPlacement($this->child_section);
+        $this->grandchild_zone =  $fixture->getRandomPageZoneForPlacement($this->grandchild_section);
+
+    }
+
+    public function __toString() {
+        $ret = "";
+        foreach ($this->sections as $section) {
+            $ret .= $section->getSectionName() . "\n";
+            foreach ($section->getActivePages() as $page) {
+                $ret .= "    " . $page->getPageType()->getAttribute('type_name') . "\n";
+                foreach ($page->getPageZones() as $zone) {
+                    $ret .= "        " . $zone->printableName() . "\n";
+                    if ($zone->getParentForPlacement() instanceof tx_newspaper_PageZone) {
+                        $ret .= "            parent: " . $zone->getParentForPlacement()->printableName() . "\n";
+                    }
+                }
+            }
+        }
+        return $ret;
+    }
+
+    public function &parentS() { return $this->parent_section; }
+    public function &childS() { return $this->child_section; }
+    public function &grandchildS() { return $this->grandchild_section; }
+
+    public function &parentZ() { return $this->parent_zone; }
+    public function &childZ() { return $this->child_zone; }
+    public function &grandchildZ() { return $this->grandchild_zone; }
+
+    private function &getSectionByName($name) {
+        foreach($this->sections as $section) {
+tx_newspaper_File::w($section->getSectionName());
+            if (strpos($section->getSectionName(), $name) !== false) return $section;
+        }
+        throw new tx_newspaper_InconsistencyException("Section $name not found");
+    }
+
+    /** @var tx_newspaper_Section[] */
+    private $sections = array();
+    /** @var tx_newspaper_Section */
+    private $parent_section = null;
+    /** @var tx_newspaper_Section */
+    private $child_section = null;
+    /** @var tx_newspaper_Section */
+    private $grandchild_section = null;
+
+    /** @var tx_newspaper_PageZone */
+    private $parent_zone = null;
+    /** @var tx_newspaper_PageZone */
+    private $child_zone = null;
+    /** @var tx_newspaper_PageZone */
+    private $grandchild_zone = null;
+}
+
 class test_SectionModuleController_testcase extends tx_newspaper_database_testcase {
 
 
@@ -21,59 +86,45 @@ class test_SectionModuleController_testcase extends tx_newspaper_database_testca
 
     public function test_changeParent() {
 
-        $parent_section = $this->fixture->getParentSection();
-        $sections = $this->fixture->getAllSections();
-        /** @var tx_newspaper_Section $grandchild_section */
-        $grandchild_section = array_pop($sections);
-        $child_section = array_pop($sections);
-
-        $parent_zone = $this->fixture->getRandomPageZoneForPlacement($parent_section);
-        $child_zone =  $this->fixture->getRandomPageZoneForPlacement($child_section);
-
+        $s = new InheritanceStructure($this->fixture);
         $inheriting_extra = $this->fixture->createExtraToInherit('tx_newspaper_Extra_Generic');
-
+tx_newspaper_File::w("structure first: $s");
         // insert extra on child section that must be gone from the grandchild section after changing parent
-        $child_zone->insertExtraAfter($inheriting_extra);
-        $grandchild_zone =  $this->fixture->getRandomPageZoneForPlacement($grandchild_section);
+        $s->childZ()->insertExtraAfter($inheriting_extra);
+        $s->childZ()->store();
+        $s->grandchildZ()->store();
 
-        $this->assertTrue(
-            $child_zone->doesContainExtra($inheriting_extra),
-            'child section does not contain extra after inserting'
-        );
-        $this->assertTrue(
-            $grandchild_zone->doesContainExtra($inheriting_extra),
-            'grandchild section does not contain extra after inserting '
-        );
-        $this->assertFalse(
-            $parent_zone->doesContainExtra($inheriting_extra),
-            'parent section contains extra after inserting'
-        );
-
-        $inherited_extra = array_pop($grandchild_zone->getExtrasOf('tx_newspaper_Extra_Generic'));
-        $this->assertNotEquals(
-            $inherited_extra->getExtraUid(), $inherited_extra->getOriginUid(),
-            "inherited extra has own uid as origin uid"
-        );
+        $this->checkInheritanceStructureIsInOrder($s, $inheriting_extra);
 
         // change parent of grandchild section to parent section, see if extras change and all
-        $this->callChangeParent($grandchild_section, $parent_section);
+        $this->callChangeParent($s->grandchildS(), $s->parentS());
+
+        // reinstantiate everything to make sure the changes have taken place
+        $s = new InheritanceStructure($this->fixture);
+        $inheriting_extra = tx_newspaper_Extra_Factory::getInstance()->create($inheriting_extra->getExtraUid());
+tx_newspaper_File::w("structure now: $s");
 
         $this->assertEquals(
-            $parent_section->getUid(), $grandchild_section->getParentSection()->getUid(),
-            "grandchild's parent section: " . $grandchild_section->getParentSection()->getUid() . ", parent section: " . $parent_section->getUid()
+            $s->parentS()->getUid(), $s->grandchildS()->getParentSection()->getUid(),
+            "grandchild's parent section: " . $s->grandchildS()->getParentSection()->getUid() . ", parent section: " . $s->parentS()->getUid()
         );
 
-        $parent_zone =  $this->fixture->getRandomPageZoneForPlacement($parent_section);
-        $grandchild_zone =  $this->fixture->getRandomPageZoneForPlacement($grandchild_section);
+tx_newspaper_File::w(print_r(array_map(function(tx_newspaper_PageZone $p) { return $p->printableName(); }, $s->grandchildZ()->getInheritanceHierarchyUp()), 1));
+
+        $this->assertFalse(
+            $s->grandchildZ()->doesContainExtra($inheriting_extra),
+            "grandchild zone apparently still contains extra inherited from child zone " . $s->grandchildZ()->getAttribute('inherits_from')
+        );
+
         $this->assertEquals(
-            $parent_zone->getAbstractUid(), $grandchild_zone->getParentForPlacement()->getAbstractUid(),
-            "grandchild's parent zone: " . $grandchild_zone->getParentForPlacement()->getAbstractUid() . ", parent zone: " . $parent_zone->getAbstractUid()
+            $s->parentZ()->getAbstractUid(), $s->grandchildZ()->getParentForPlacement()->getAbstractUid(),
+            "grandchild's parent zone: " . $s->grandchildZ()->getParentForPlacement() . ", parent zone: " . $s->parentZ()
         );
 
         // check all origin uids on grandchild section, must be != extra uids
-        foreach ($parent_zone->getExtras() as $inherited_extra) {
+        foreach ($s->parentZ()->getExtras() as $inherited_extra) {
             $this->assertTrue(
-                $grandchild_zone->doesContainExtra($inherited_extra),
+                $s->grandchildZ()->doesContainExtra($inherited_extra),
                 "inherited extra $inherited_extra not on inheriting zone"
             );
         }
@@ -104,10 +155,40 @@ class test_SectionModuleController_testcase extends tx_newspaper_database_testca
         $method = new ReflectionMethod($this->controller, 'changeParent');
         $method->setAccessible(true);
         $method->invokeArgs($this->controller, array(&$section));
+
+        $section->store();
     }
 
     /** @var Tx_newspaper_Controller_SectionModuleController */
     private $controller;
+
+    /**
+     * @param $grandchild_section
+     * @param $child_zone
+     * @param $inheriting_extra
+     * @param $parent_zone
+     * @return array
+     */
+    private function checkInheritanceStructureIsInOrder(InheritanceStructure &$structure, tx_newspaper_Extra $inheriting_extra) {
+        $this->assertTrue(
+            $structure->childZ()->doesContainExtra($inheriting_extra),
+            'child section does not contain extra after inserting'
+        );
+        $this->assertTrue(
+            $structure->grandchildZ()->doesContainExtra($inheriting_extra),
+            'grandchild section does not contain extra after inserting '
+        );
+        $this->assertFalse(
+            $structure->parentZ()->doesContainExtra($inheriting_extra),
+            'parent section contains extra after inserting'
+        );
+
+        $inherited_extra = array_pop($structure->grandchildZ()->getExtrasOf('tx_newspaper_Extra_Generic'));
+        $this->assertNotEquals(
+            $inherited_extra->getExtraUid(), $inherited_extra->getOriginUid(),
+            "inherited extra has own uid as origin uid"
+        );
+    }
 
 }
 ?>
